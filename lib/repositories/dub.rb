be25948ffa6cf42e194c9c02937a05ad47c94183
Andrew Nesbitt <andrewnez@gmail.com>
2014-10-06 10:24:04 +0100

    Added dub

---
+++ lib/repositories/dub.rb
@@ -0,0 +1,11 @@
+module Repositories
+  class Dub
+    def self.project_names
+      HTTParty.get("http://code.dlang.org/packages/index.json").parsed_response.sort
+    end
+
+    def self.project(name)
+      HTTParty.get("http://code.dlang.org/packages/#{name}.json").parsed_response
+    end
+  end
+end
