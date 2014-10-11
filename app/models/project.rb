58ab5a6205ccf6e7e974e2157faccb47a41292e8
Andrew Nesbitt <andrewnez@gmail.com>
2014-10-11 18:41:11 +0100

    nicer urls

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,12 @@
 class Project < ActiveRecord::Base
   validates_presence_of :name, :platform
 
+  # TODO validate homepage format
+
+  def to_param
+    "#{id}-#{name.parameterize}"
+  end
+
   ## relations
   # versions => dependencies
   # repository
