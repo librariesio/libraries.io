8f42027921e94d91321754bdb2a3791f22d17a09
Andrew Nesbitt <andrewnez@gmail.com>
2014-10-13 14:04:04 +0100

    Fixed sublime importer

--- lib/repositories/sublime.rb
+++ lib/repositories/sublime.rb
@@ -1,11 +1,11 @@
 class Repositories
-  class Sublime
+  class Sublime < Base
     def self.project_names
       HTTParty.get("https://sublime.wbond.net/channel.json").parsed_response['packages_cache'].map{|k,v| v[0]['name']}
     end
 
     def self.project(name)
-      HTTParty.get("https://sublime.wbond.net/packages/#{name}.json").parsed_response
+      HTTParty.get(URI.escape("https://sublime.wbond.net/packages/#{name}.json")).parsed_response
     end
 
     def self.mapping(project)
@@ -13,7 +13,7 @@ class Repositories
         :name => project["name"],
         :description => project["description"],
         :homepage => project["homepage"],
-        :keywords => project["labels"]
+        :keywords => project["labels"].join(',')
       }
     end
 
