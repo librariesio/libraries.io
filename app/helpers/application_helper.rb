057a060694bf0e805db4566a71497aa258643627
Andrew Nesbitt <andrewnez@gmail.com>
2015-01-11 23:08:56 +0000

    Link to package pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def package_link(name, platform)
+    case platform
+    when 'Hex'
+      "https://hex.pm/packages/#{name}"
+    when 'Dub'
+      "http://code.dlang.org/packages/#{name}"
+    when 'Emacs'
+      "http://melpa.org/#/#{name}"
+    when 'Jam'
+      "http://jamjs.org/packages/#/details/#{name}"
+    end
+  end
 end
