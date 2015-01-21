6ed79a53b93fd9b65e6601b5bc66762f765ee31a
Andrew Nesbitt <andrewnez@gmail.com>
2015-01-21 01:10:12 +0000

    to_a for some reason

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -6,7 +6,7 @@ class PlatformsController < ApplicationController
   def show
     @platform = "Repositories::#{params[:id]}".constantize
     @platform_name = @platform.to_s.demodulize
-    @licenses = Project.platform(@platform_name).popular_licenses
+    @licenses = Project.platform(@platform_name).popular_licenses.to_a
     limit = 5
     @added = Project.platform(@platform_name).limit(limit).order('created_at DESC')
     @updated = Project.platform(@platform_name).limit(limit).order('updated_at DESC')
