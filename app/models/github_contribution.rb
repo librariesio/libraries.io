8a8bd99953ec934a228759a06aafa5d6b9af8bba
Andrew Nesbitt <andrewnez@gmail.com>
2015-01-25 20:58:45 +0000

    Link to contribution pages

--- app/models/github_contribution.rb
+++ app/models/github_contribution.rb
@@ -1,4 +1,8 @@
 class GithubContribution < ActiveRecord::Base
   belongs_to :github_user
   belongs_to :github_repository
+
+  def github_url
+    "https://github.com/#{github_repository.full_name}/commits/master?author=#{github_user.login}"
+  end
 end
