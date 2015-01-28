b76d4177c6b84c18ba42e83f5805e98271b86718
Andrew Nesbitt <andrewnez@gmail.com>
2015-01-28 10:33:44 +0000

    Fixed versions page

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -3,6 +3,9 @@ class VersionsController < ApplicationController
     @project = Project.find params[:project_id]
     @version = @project.versions.find_by!(number: params[:id])
     @versions = @project.versions.order('number DESC').to_a
+    if @project.github_repository
+      @contributors = @project.github_repository.github_contributions.includes(:github_user).limit(10)
+    end
     render template: 'projects/show'
   end
 end
