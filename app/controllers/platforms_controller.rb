be368785a0e76de87e79979d9bb5a6446df2b532
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-02 17:33:36 +0000

    Show more

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -8,6 +8,7 @@ class PlatformsController < ApplicationController
     @platform_name = @platform.to_s.demodulize
     @licenses = Project.platform(@platform_name).popular_licenses.to_a
     @updated = Project.platform(@platform_name).limit(5).order('updated_at DESC')
+    @created = Project.platform(@platform_name).limit(5).order('created_at DESC')
     @popular = Project.platform(@platform_name).with_repo.limit(5).order('github_repositories.stargazers_count DESC')
     @contributors = GithubUser.top_for(@platform_name, 6)
   end
