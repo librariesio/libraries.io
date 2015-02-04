e82065e56c34210453585d1681bc5f72b364c0c3
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-04 01:31:39 +0000

    Fixed users again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = GithubUser.find(params[:id])
     @contributions = @user.github_contributions
-                          .includes(:github_repository => :project)
+                          .includes(:github_repository => :projects)
                           .order('count DESC')
   end
 end
