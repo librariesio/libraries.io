e3fa89e4c8ee0a2dafe35a3ff8f87d9bd0c67717
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-04 19:27:52 +0000

    github contribution touches

--- app/models/github_contribution.rb
+++ app/models/github_contribution.rb
@@ -1,6 +1,6 @@
 class GithubContribution < ActiveRecord::Base
-  belongs_to :github_user
-  belongs_to :github_repository
+  belongs_to :github_user, touch: true
+  belongs_to :github_repository, touch: true
 
   def github_url
     "https://github.com/#{github_repository.full_name}/commits/master?author=#{github_user.login}"
