6cd46127b1e8cf385667299153e913c8d6321885
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-05 10:08:34 +0000

    Popular projects by uniq github repos

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -9,8 +9,10 @@ class PlatformsController < ApplicationController
     @licenses = Project.platform(@platform_name).popular_licenses.limit(8).to_a
     @updated = Project.platform(@platform_name).limit(5).order('updated_at DESC')
     @created = Project.platform(@platform_name).limit(5).order('created_at DESC')
-    @popular = Project.platform(@platform_name).with_repo.limit(5).order('github_repositories.stargazers_count DESC')
     @contributors = GithubUser.top_for(@platform_name, 5)
+    @popular = Project.platform(@platform_name).with_repo.limit(20)
+      .order('github_repositories.stargazers_count DESC')
+      .to_a.uniq(&:github_repository_id).first(5)
   end
 
   def legacy
