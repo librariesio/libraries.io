ebbd41d664738f9c44bac350f2add1ab54558b8f
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-05 19:48:53 +0000

    Grids of contributors

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -9,7 +9,7 @@ class PlatformsController < ApplicationController
     @licenses = Project.platform(@platform_name).popular_licenses.limit(8).to_a
     @updated = Project.platform(@platform_name).limit(5).order('updated_at DESC')
     @created = Project.platform(@platform_name).limit(5).order('created_at DESC')
-    @contributors = GithubUser.top_for(@platform_name, 5)
+    @contributors = GithubUser.top_for(@platform_name, 24)
     @popular = Project.platform(@platform_name).with_repo.limit(30)
       .order('github_repositories.stargazers_count DESC')
       .to_a.uniq(&:github_repository_id).first(5)
