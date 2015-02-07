1406957a38c5900a67d47021b87d8737d4b544c0
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-07 18:05:44 +0000

    Show more license options on the search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
     scope = scope.platform(params[:platform]) if params[:platform].present?
     scope = scope.license(params[:license]) if params[:license].present?
 
-    @licenses = Project.popular_licenses.limit(8)
+    @licenses = Project.popular_licenses.limit(20)
     @platforms = Download.platforms.map{|p| p.to_s.demodulize }
     @projects = scope.paginate(page: params[:page])
   end
