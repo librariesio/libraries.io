e3546359ceef493bf5110dba3e02cf4a615e34b7
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-08 09:50:47 +0000

    Disable search filters for now

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
     scope = Project.search(params[:q])
-    scope = scope.platform(params[:platform]) if params[:platform].present?
-    scope = scope.license(params[:license]) if params[:license].present?
+    # scope = scope.platform(params[:platform]) if params[:platform].present?
+    # scope = scope.license(params[:license]) if params[:license].present?
 
     @licenses = Project.popular_licenses.limit(20)
     @platforms = Download.platforms.map{|p| p.to_s.demodulize }
