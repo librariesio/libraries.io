cf16400531a8817614e6a18811616acc3fe703d1
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-10 18:52:47 +0000

    Reorder the loading of rake task data

--- lib/download.rb
+++ lib/download.rb
@@ -6,11 +6,11 @@ class Download
   end
 
   def self.new_github_repos
-    Project.undownloaded_repos.order('created_at DESC').find_each(&:update_github_repo)
+    Project.undownloaded_repos.order('created_at ASC').find_each(&:update_github_repo)
   end
 
   def self.download_contributors
-    GithubRepository.order('updated_at ASC').find_each(&:download_github_contributions)
+    GithubRepository.order('created_at DESC').find_each(&:download_github_contributions)
   end
 
   def self.update_github_repos
