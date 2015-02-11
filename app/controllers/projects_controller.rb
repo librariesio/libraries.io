fd424542b8b97219a9e721934b158b94c4c0c058
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-11 09:30:56 +0000

    302 redircet plz

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,6 @@ class ProjectsController < ApplicationController
   def find_project
     @project = Project.platform(params[:platform]).where('lower(name) = ?', params[:name].downcase).first
     raise ActiveRecord::RecordNotFound if @project.nil?
-    redirect_to project_path(@project.to_param) if params[:platform] != params[:platform].downcase || params[:name] != @project.name
+    redirect_to project_path(@project.to_param), :status => :moved_permanently if params[:platform] != params[:platform].downcase || params[:name] != @project.name
   end
 end
