a9e4564216d1023439da82c9850d7254dcf76583
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-16 12:15:25 +0000

    Moved filtering to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = GithubUser.where("lower(login) = ?", params[:login].downcase).first
     raise ActiveRecord::RecordNotFound if @user.nil?
+    @repositories = @user.repositories.includes(:projects).reject{|g| g.projects.empty? }
     @contributions = @user.github_contributions
                           .includes(:github_repository => :projects)
-                          .order('count DESC').reject{|g| g.github_repository.try(:owner_name) == @user.login }
+                          .order('count DESC').reject{|g| g.github_repository.nil? || g.github_repository.owner_name == @user.login || g.github_repository.projects.empty? }
   end
 end
