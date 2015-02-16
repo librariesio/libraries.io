c2fca96854f5a970b8de0a791eadec757653cc19
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-16 10:25:33 +0000

    Show contributions on stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,5 +4,6 @@ class StatsController < ApplicationController
     @new_versions = Version.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_repos = GithubRepository.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_users = GithubUser.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    @new_contributions = GithubContribution.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
   end
 end
