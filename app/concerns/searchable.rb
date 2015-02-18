f6d6113bf65917bafe2a051f568f8b208d76f33c
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-18 09:52:35 +0000

    remove projects from es index after being deleted

--- app/concerns/searchable.rb
+++ app/concerns/searchable.rb
@@ -23,8 +23,16 @@ module Searchable
       end
     end
 
-    after_commit on: [:create, :update] do
+    after_commit on: [:create] do
       __elasticsearch__.index_document
+    end
+
+    after_commit on: [:update] do
+      __elasticsearch__.update_document
+    end
+
+    after_commit on: [:destroy] do
+      __elasticsearch__.delete_document
     end
 
     def as_indexed_json(options = {})
