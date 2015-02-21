52e1b95521fdc2dd0e662d30f0fd52f3c23ac13d
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-21 16:18:53 +0000

    Removed more slow contributor sections

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -20,22 +20,4 @@ class GithubUser < ActiveRecord::Base
   def repositories
     GithubRepository.where('full_name ILIKE ?', "#{login}/%")
   end
-
-  def self.top_for(platform, limit = 5)
-    GithubContribution.where(platform: platform).select('sum(count) as count, github_user_id')
-      .group('github_user_id')
-      .order('count DESC')
-      .limit(limit)
-      .includes(:github_user)
-      .map(&:github_user)
-  end
-
-  def self.top(limit = 5)
-    GithubContribution.select('sum(count) as count, github_user_id')
-      .group('github_user_id')
-      .order('count DESC')
-      .limit(limit)
-      .includes(:github_user)
-      .map(&:github_user)
-  end
 end
