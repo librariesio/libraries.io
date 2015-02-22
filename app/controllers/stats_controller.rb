13923ce67bb97c32cba1f8dd781e24dc26816d66
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-22 21:12:07 +0000

    Add users and subscriptions to stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,10 +1,13 @@
 class StatsController < ApplicationController
   def index
-    @new_projects = Project.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
-    @new_versions = Version.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
-    @new_repos = GithubRepository.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
-    @new_users = GithubUser.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
-    @new_contributions = GithubContribution.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
-    @new_dependencies = Dependency.where('created_at > ?', 1.week.ago).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    period = 2.days.ago
+    @new_projects = Project.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    @new_versions = Version.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    @new_repos = GithubRepository.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    @new_github_users = GithubUser.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    @new_contributions = GithubContribution.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    @new_dependencies = Dependency.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    @new_users = User.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
+    @new_subscriptions = Subscription.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
   end
 end
