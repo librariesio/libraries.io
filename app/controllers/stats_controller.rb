8fdbb6d340b8498afc6ee852402012eb39898b26
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-01 23:36:30 +0000

    Show less stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,11 +4,8 @@ class StatsController < ApplicationController
   def index
     period = 3.days.ago
     @new_projects = Project.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
-    @new_versions = Version.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_repos = GithubRepository.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_github_users = GithubUser.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
-    @new_contributions = GithubContribution.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
-    @new_dependencies = Dependency.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_users = User.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_subscriptions = Subscription.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_readmes = Readme.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
