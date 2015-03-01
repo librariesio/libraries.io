d67fb77ac1374efe0c273e81c6d8eaddbce2f3f5
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-01 20:04:12 +0000

    Handle missing github repos better

--- app/controllers/github_repositories_controller.rb
+++ app/controllers/github_repositories_controller.rb
@@ -1,6 +1,7 @@
 class GithubRepositoriesController < ApplicationController
   def show
     @github_repository = GithubRepository.find_by_full_name([params[:owner], params[:name]].join('/'))
+    raise ActiveRecord::RecordNotFound if @github_repository.nil?
     @contributors = @github_repository.github_contributions.order('count DESC').limit(20).includes(:github_user)
     @projects = @github_repository.projects.includes(:versions)
   end
