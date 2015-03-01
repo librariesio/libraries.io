085bb64df3ab59188c36c58b2af689c8c67ed7a0
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-01 23:02:10 +0000

    Cache parts of the language pages

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -7,9 +7,6 @@ class LanguagesController < ApplicationController
     find_language
     @updated = Project.search('*', filters: { language: @language }, sort: 'updated_at').records.includes(:versions).first(5)
     @created = Project.search('*', filters: { language: @language }, sort: 'created_at').records.includes(:versions).first(5)
-    @popular = Project.popular(filters: { language: @language }).first(5)
-    @licenses = Project.popular_licenses(filters: { language: @language }).first(8)
-    @platforms = Project.popular_platforms(filters: { language: @language }).first(10)
   end
 
   def find_language
