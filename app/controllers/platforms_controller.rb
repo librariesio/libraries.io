91e41b6c0790374c4e983e69f13d82f28cc8f700
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-03-03 00:48:15 +0000

    Coloured favicons

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -5,10 +5,12 @@ class PlatformsController < ApplicationController
 
   def show
     find_platform
-    @licenses = Project.popular_licenses(filters: {platform: @platform_name}).first(10)
-    @updated = Project.search('*', filters: {platform: @platform_name}, sort: 'updated_at').records.includes(:versions).first(5)
-    @created = Project.search('*', filters: {platform: @platform_name}, sort: 'created_at').records.includes(:versions).first(5)
-    @popular = Project.popular(filters: { platform: @platform_name }).first(5)
-    @languages = Project.popular_languages(filters: {platform: @platform_name}).first(10)
+    @licenses = []#Project.popular_licenses(filters: {platform: @platform_name}).first(10)
+    @updated = []#Project.search('*', filters: {platform: @platform_name}, sort: 'updated_at').records.includes(:versions).first(5)
+    @created = []#Project.search('*', filters: {platform: @platform_name}, sort: 'created_at').records.includes(:versions).first(5)
+    @popular = []#Project.popular(filters: { platform: @platform_name }).first(5)
+    @languages = []#Project.popular_languages(filters: {platform: @platform_name}).first(10)
+
+    @color = @platform.color
   end
 end
