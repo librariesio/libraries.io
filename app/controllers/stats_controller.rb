da5da43aade285648038b5dac26c5cb64bbbd82d
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-06 23:30:46 +0000

    Normalize the ends and beginnings of days

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,7 +2,7 @@ class StatsController < ApplicationController
   newrelic_ignore
 
   def index
-    period = 7.days.ago
+    period = 7.days.ago.beginning_of_day
     @new_projects = Project.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_repos = GithubRepository.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
     @new_github_users = GithubUser.where('created_at > ?', period).group("date(created_at)").count.sort_by{|k,v| k }.reverse
