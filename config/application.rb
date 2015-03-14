268cadab07ae4742e738f8eda70522517f5d96aa
Baris Balic <leafybasil@gmail.com>
2015-03-14 15:45:50 +0000

    Handle errors by routing to an explicit controller.

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module Libraries
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.exceptions_app = routes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
 
+  get '/404', to: 'errors#not_found'
+  get '/422', to: 'errors#unprocessable'
+  get '/500', to: 'errors#internal'
+
   resources :licenses, constraints: { :id => /.*/ }
   resources :languages
   resources :subscriptions
