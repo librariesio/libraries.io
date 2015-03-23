5ccecdb0737e86dc76a770f93a014a5874b6d6b6
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-23 22:27:16 +0000

    eager load projects versions for api

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -7,7 +7,7 @@ class Api::ProjectsController < Api::ApplicationController
   private
 
   def find_project
-    @project = Project.platform(params[:platform]).where('lower(name) = ?', params[:name].downcase).includes({:github_repository => :readme}).first
+    @project = Project.platform(params[:platform]).where('lower(name) = ?', params[:name].downcase).includes(:versions, :github_repository).first
     raise ActiveRecord::RecordNotFound if @project.nil?
     redirect_to project_path(@project.to_param), :status => :moved_permanently if params[:platform] != params[:platform].downcase || params[:name] != @project.name
     @color = @project.color
