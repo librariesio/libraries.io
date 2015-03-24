708005be46be8ee2cfeeec7c05d5f8e762bd4eca
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-24 22:13:01 +0000

    subscriptions don’t have to belong to a user now

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,5 @@
 class Subscription < ActiveRecord::Base
-  validates_presence_of :project, :user
+  validates_presence_of :project
   validates_uniqueness_of :project, scope: :user_id
   belongs_to :project
   belongs_to :user
