5496c4be5d2a666ef733a3159c97d89343ddb3ed
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-29 12:22:58 +0100

    Allow a manifest to load details about its repo

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -1,4 +1,8 @@
 class Manifest < ActiveRecord::Base
   belongs_to :user
   has_many :subscriptions, dependent: :destroy
+
+  def repository
+    user.github_client.repo(repository_id) if repository_id.present?
+  end
 end
