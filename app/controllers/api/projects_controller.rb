4d69dda0c5bce521612f1239db3fb0331603ccc2
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-30 14:27:41 +0100

    Remove find project method from projects api controller

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,6 +1,7 @@
 class Api::ProjectsController < Api::ApplicationController
   def show
-    find_project
+    @project = Project.platform(params[:platform]).where('lower(name) = ?', params[:name].downcase).includes(:versions, :github_repository).first
+    raise ActiveRecord::RecordNotFound if @project.nil?
     render json: @project.as_json(:include => [:versions, :github_repository])
   end
 
@@ -12,12 +13,5 @@ class Api::ProjectsController < Api::ApplicationController
 
   def searchcode
     render json: Project.where('updated_at > ?', 1.day.ago).pluck(:repository_url).compact.reject(&:blank?)
-  end
-
-  private
-
-  def find_project
-    @project = Project.platform(params[:platform]).where('lower(name) = ?', params[:name].downcase).includes(:versions, :github_repository).first
-    raise ActiveRecord::RecordNotFound if @project.nil?
   end
 end
