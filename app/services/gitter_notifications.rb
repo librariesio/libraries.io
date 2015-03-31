2c2b55987dc31d19b44396470aae96bbdab43a8f
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-31 09:59:07 +0100

    Only send message to gitter in production

--- app/services/gitter_notifications.rb
+++ app/services/gitter_notifications.rb
@@ -1,12 +1,14 @@
 class GitterNotifications
   class << self
     def new_project(project_name, platform)
+      return unless Rails.env.production?
       uri = URI.parse("https://webhooks.gitter.im/e/5c404ff65201c8e9bed8")
       message = "[#{platform}] **[#{project_name}](http://libraries.io/#{platform}/#{project_name})** has been added"
       Net::HTTP.post_form(uri, {"message" => message})
     end
 
     def new_version(project_name, platform, version)
+      return unless Rails.env.production?
       uri = URI.parse("https://webhooks.gitter.im/e/5c404ff65201c8e9bed8")
       message = "[#{platform}] **[#{project_name}](http://libraries.io/#{platform}/#{project_name}/#{version})** #{version} has been added"
       Net::HTTP.post_form(uri, {"message" => message})
