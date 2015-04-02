ac40f1b2a857a065d0c5065a24705857d0b34c2d
Andrew Nesbitt <andrewnez@gmail.com>
2015-04-02 16:32:17 +0100

    Use Typhoeus for posting to gitter
    cc @barisbalic

--- app/services/gitter_notifications.rb
+++ app/services/gitter_notifications.rb
@@ -1,17 +1,16 @@
 class GitterNotifications
   class << self
     def new_project(project_name, platform)
-      return unless Rails.env.production?
-      uri = URI.parse("https://webhooks.gitter.im/e/5c404ff65201c8e9bed8")
-      message = "[#{platform}] **[#{project_name}](http://libraries.io/#{platform}/#{project_name})** has been added"
-      Net::HTTP.post_form(uri, {"message" => message})
+      post_to_gitter "[#{platform}] **[#{project_name}](http://libraries.io/#{platform}/#{project_name})** has been added"
     end
 
     def new_version(project_name, platform, version)
+      post_to_gitter("[#{platform}] **[#{project_name}](http://libraries.io/#{platform}/#{project_name}/#{version})** #{version} has been added")
+    end
+
+    def post_to_gitter(message)
       return unless Rails.env.production?
-      uri = URI.parse("https://webhooks.gitter.im/e/5c404ff65201c8e9bed8")
-      message = "[#{platform}] **[#{project_name}](http://libraries.io/#{platform}/#{project_name}/#{version})** #{version} has been added"
-      Net::HTTP.post_form(uri, {"message" => message})
+      Typhoeus::Request.new("https://webhooks.gitter.im/e/5c404ff65201c8e9bed8", method: :post, body: { 'message' => message }).run
     end
   end
 end
