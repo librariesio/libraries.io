bb0a200663c0704a87b01917c5358c3847d4042b
Andrew Nesbitt <andrewnez@gmail.com>
2015-04-02 19:45:46 +0100

    Fix that fuckup!

--- db/migrate/20150402181050_add_updated_at_index_to_projects.rb
+++ db/migrate/20150402181050_add_updated_at_index_to_projects.rb
@@ -1,6 +1,5 @@
 class AddUpdatedAtIndexToProjects < ActiveRecord::Migration
   def change
-    # add_index :projects, :updated_at
-    # add_index :projects, :created_at
+    add_index :projects, :updated_at
   end
 end
