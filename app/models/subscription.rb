d3d2924ab03779cc37e53abf975d574728021185
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-10 23:19:57 +0100

    Fix repo subscription validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ActiveRecord::Base
   validates_presence_of :project
-  validates_uniqueness_of :project, scope: :user_id
+  validates_uniqueness_of :project, scope: :user_id, allow_nil: true
   belongs_to :project
   belongs_to :user
   belongs_to :repository_subscription
