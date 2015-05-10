78e8757bf7c93f3f741965ae40b51696bf585416
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-10 16:10:20 +0100

    Fixed Manifest#latest scope

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -2,7 +2,7 @@ class Manifest < ActiveRecord::Base
   belongs_to :github_repository
   has_many :repository_dependencies, dependent: :destroy
 
-  scope :latest, -> { select("DISTINCT(manifests.id), manifests.*").order("manifests.created_at DESC") }
+  scope :latest, -> { order("manifests.path, created_at DESC").select("DISTINCT on (manifests.path) *") }
 
   def github_link
     github_repository.blob_url + path
