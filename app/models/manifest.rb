9fba12dfa4e2052a175326182bf1d0627b9014a1
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-10 14:51:25 +0100

    Only show latest manifests

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -2,6 +2,8 @@ class Manifest < ActiveRecord::Base
   belongs_to :github_repository
   has_many :repository_dependencies, dependent: :destroy
 
+  scope :latest, -> { select("DISTINCT(manifests.id), manifests.*").order("manifests.created_at DESC") }
+
   def github_link
     github_repository.blob_url + path
   end
