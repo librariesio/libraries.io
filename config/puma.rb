d80fb8622b2dfb0f5f56888317b16532c15e3f14
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-17 20:01:07 +0100

    Puma redo redis connection on boot

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,8 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
+
+  uri = ENV['REDISCLOUD_URL'] || 'redis://localhost:6379/'
+  REDIS = Redis.new(url: uri, driver: :hiredis)
 end
