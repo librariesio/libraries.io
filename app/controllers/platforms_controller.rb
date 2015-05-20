a60d97612a824632dd2a5a79498b0c351ef0fe02
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-20 08:55:54 +0100

    Most watched section on platform, language and license pages

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -7,6 +7,7 @@ class PlatformsController < ApplicationController
     find_platform
     @updated = Project.search('*', filters: {platform: @platform_name}, sort: 'latest_release_published_at').records.includes(:github_repository).first(5)
     @created = Project.search('*', filters: {platform: @platform_name}, sort: 'created_at').records.includes(:github_repository).first(5)
+    @watched = Project.platform(@platform_name).most_watched.limit(5)
 
     @color = @platform.color
   end
