84bb1319af6a9bf22d9e7d3de93e83028c332d3a
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-21 12:12:16 +0100

    Password protect sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end if Rails.env.production?
   mount Sidekiq::Web => '/sidekiq'
 
   namespace :api do
