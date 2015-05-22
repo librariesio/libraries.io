d7a45eafc5751aeed836713916176b8a029042bc
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-22 21:40:01 +0100

    Dependency linking task

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -4,7 +4,14 @@ class Dependency < ActiveRecord::Base
 
   validates_presence_of :project_name, :version_id, :requirements, :platform
 
+  scope :without_project_id, -> { where(project_id: nil) }
+
   def find_project_id
     Project.platform(version.project.platform).where('lower(name) = ?', project_name.downcase).first.try(:id)
+  end
+
+  def update_project_id
+    proj_id = find_project_id
+    update_attribute(:project_id, proj_id) if proj_id.present?
   end
 end
--- lib/tasks/projects.rake
+++ lib/tasks/projects.rake
@@ -13,4 +13,8 @@ namespace :projects do
     ids = Project.order('updated_at ASC').limit(10_000).pluck(:id).to_a
     Project.includes([{:github_repository => [:readme, :github_tags]}, :versions, :github_contributions]).where(id: ids).find_each(&:update_source_rank)
   end
+
+  task link_dependencies: :environment do
+    Dependency.without_project_id.find_each(&:update_project_id)
+  end
 end
