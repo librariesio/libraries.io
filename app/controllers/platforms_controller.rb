79e5996527e337b9b4456a9b67c1e9e542502271
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-27 20:35:18 +0100

    Revert "Make sure new and updated look different"
    This reverts commit f89c97bd019c07b178c5dd7a78d8b91c8e8603c8.

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -5,8 +5,8 @@ class PlatformsController < ApplicationController
 
   def show
     find_platform
-    @updated = Project.search('*', filters: {platform: @platform_name}, sort: 'latest_release_published_at').records.includes(:github_repository).many_versions.first(5)
-    @created = Project.search('*', filters: {platform: @platform_name}, sort: 'created_at').records.includes(:github_repository).few_versions.first(5)
+    @updated = Project.search('*', filters: {platform: @platform_name}, sort: 'latest_release_published_at').records.includes(:github_repository).first(5)
+    @created = Project.search('*', filters: {platform: @platform_name}, sort: 'created_at').records.includes(:github_repository).first(5)
     @watched = Project.platform(@platform_name).most_watched.limit(4)
 
     @color = @platform.color
