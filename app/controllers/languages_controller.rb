8c0165fa9d8cb1253796d79f7069a26c658e8acb
Andrew Nesbitt <andrewnez@gmail.com>
2015-05-31 09:35:22 +0100

    Fix updated sorting

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -7,7 +7,7 @@ class LanguagesController < ApplicationController
     find_language
 
     @created = Project.language(@language).few_versions.order('projects.created_at DESC').limit(5).includes(:github_repository)
-    @updated = Project.language(@language).many_versions.order('projects.updated_at DESC').limit(5).includes(:github_repository)
+    @updated = Project.language(@language).many_versions.order('projects.latest_release_published_at DESC').limit(5).includes(:github_repository)
     @color = Languages::Language[@language].try(:color)
     @watched = Project.language(@language).most_watched.limit(4)
   end
--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -7,7 +7,7 @@ class PlatformsController < ApplicationController
     find_platform
 
     @created = Project.platform(@platform_name).few_versions.order('projects.created_at DESC').limit(5).includes(:github_repository)
-    @updated = Project.platform(@platform_name).many_versions.order('projects.updated_at DESC').limit(5).includes(:github_repository)
+    @updated = Project.platform(@platform_name).many_versions.order('projects.latest_release_published_at DESC').limit(5).includes(:github_repository)
     @watched = Project.platform(@platform_name).most_watched.limit(4)
 
     @color = @platform.color
