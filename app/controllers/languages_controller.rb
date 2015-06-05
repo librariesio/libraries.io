06df89b35a4bc15def197f542faadf87058e5987
Andrew Nesbitt <andrewnez@gmail.com>
2015-06-05 20:35:04 +0100

    Disable the language pages for a minute

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -6,11 +6,11 @@ class LanguagesController < ApplicationController
   def show
     find_language
 
-    @created = Project.language(@language).few_versions.order('projects.created_at DESC').limit(5).includes(:github_repository)
-    @updated = Project.language(@language).many_versions.order('projects.latest_release_published_at DESC').limit(5).includes(:github_repository)
+    @created = []#Project.language(@language).few_versions.order('projects.created_at DESC').limit(5).includes(:github_repository)
+    @updated = []#Project.language(@language).many_versions.order('projects.latest_release_published_at DESC').limit(5).includes(:github_repository)
     @color = Languages::Language[@language].try(:color)
-    @watched = Project.language(@language).most_watched.limit(5)
-    @popular = Project.language(@language).order('projects.rank DESC').limit(5).includes(:github_repository)
+    @watched = []#Project.language(@language).most_watched.limit(5)
+    @popular = []#Project.language(@language).order('projects.rank DESC').limit(5).includes(:github_repository)
   end
 
   def find_language
