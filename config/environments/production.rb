569befb75dcc7b89ac1b4dcb9a0e521d24cdaa6c
Andrew Nesbitt <andrewnez@gmail.com>
2015-06-16 12:52:49 +0100

    Define an asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = config.action_mailer.asset_host  = 'https://libraries.io'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
