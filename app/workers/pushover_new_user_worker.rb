ae736d62f5333de36437815dcdb6b44b8dd200f5
Andrew Nesbitt <andrewnez@gmail.com>
2015-06-22 12:21:10 +0100

    Only send pushover alert for interesting users

--- app/workers/pushover_new_user_worker.rb
+++ app/workers/pushover_new_user_worker.rb
@@ -3,12 +3,16 @@ class PushoverNewUserWorker
 
   def perform(user_id)
     user = User.find(user_id)
-    Pushover.notification({
-      title: "@#{user.nickname} just signed up",
-      message: "#{user.github_client.repos.length} repos and #{user.github_client.user(user.nickname).followers} followers",
-      sound: 'bugle',
-      url: user.github_url,
-      url_title: 'View on Github'
-    })
+    repos = user.github_client.repos.length
+    followers = user.github_client.user(user.nickname).followers
+    if repos > 0 || followers > 0
+      Pushover.notification({
+        title: "@#{user.nickname} just signed up",
+        message: "#{repos} repos and #{followers} followers",
+        sound: 'bugle',
+        url: user.github_url,
+        url_title: 'View on Github'
+      })
+    end
   end
 end
