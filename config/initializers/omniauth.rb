f4ec8eac676c18ff05743de5d4346194c4da4074
Andrew Nesbitt <andrewnez@gmail.com>
2015-09-01 09:26:17 +0100

    Change/improve github oauth scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -14,8 +14,8 @@ end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email"
-  provider :github_public, ENV['GITHUB_PUBLIC_KEY'], ENV['GITHUB_PUBLIC_SECRET'], scope: "user:email,public_repo", request_path: '/auth/github_public', callback_path: '/auth/github_public/callback'
-  provider :github_private, ENV['GITHUB_PRIVATE_KEY'], ENV['GITHUB_PRIVATE_SECRET'], scope: "user:email,repo", request_path: '/auth/github_private', callback_path: '/auth/github_private/callback'
+  provider :github_public, ENV['GITHUB_PUBLIC_KEY'], ENV['GITHUB_PUBLIC_SECRET'], scope: "user:email,write:repo_hook,read:org", request_path: '/auth/github_public', callback_path: '/auth/github_public/callback'
+  provider :github_private, ENV['GITHUB_PRIVATE_KEY'], ENV['GITHUB_PRIVATE_SECRET'], scope: "user:email,repo,read:org", request_path: '/auth/github_private', callback_path: '/auth/github_private/callback'
 end
 
 Rails.application.config.default_provider = :github
