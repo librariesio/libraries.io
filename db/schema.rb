7ce30a0a49ba3c73282ddb146fe4ac19b4d1e254
Andrew Nesbitt <andrewnez@gmail.com>
2015-09-05 17:56:51 +0100

    Add unique index to version number/project_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904092811) do
+ActiveRecord::Schema.define(version: 20150905165001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -279,7 +279,7 @@ ActiveRecord::Schema.define(version: 20150904092811) do
   end
 
   add_index "versions", ["created_at"], name: "index_versions_on_created_at", using: :btree
-  add_index "versions", ["project_id", "number"], name: "index_versions_on_project_id_and_number", using: :btree
+  add_index "versions", ["project_id", "number"], name: "index_versions_on_project_id_and_number", unique: true, using: :btree
   add_index "versions", ["project_id"], name: "index_versions_on_project_id", using: :btree
 
 end
