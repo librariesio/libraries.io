a7ece1a6b4ae5659aed8b6ace331d6d3f5572d8f
Andrew Nesbitt <andrewnez@gmail.com>
2015-09-08 11:50:34 +0100

    Slightly different path loading

--- config/initializers/libraries.rb
+++ config/initializers/libraries.rb
@@ -1,2 +1,1 @@
-require_relative("#{Rails.root}/lib/repositories/base.rb")
-Dir["#{Rails.root}/lib/repositories/*.rb"].each {|file| require file }
+require("#{Rails.root}/lib/repositories.rb")
--- lib/repositories.rb
+++ lib/repositories.rb
@@ -1,6 +1,8 @@
-Dir["lib/repositories/*.rb"].each {|file| require "./#{file}" }
+require("#{Rails.root}/lib/repositories/base.rb")
+Dir["#{Rails.root}/lib/repositories/*.rb"].each {|file| require file }
 class Repositories
   def self.descendants
     constants.collect {|const_name| const_get(const_name)}
   end
 end
+require("#{Rails.root}/lib/download.rb")
