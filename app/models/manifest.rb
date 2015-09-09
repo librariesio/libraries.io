fd4bfef0931b0b11d61dff3fc96e050ea256188d
Andrew Nesbitt <andrewnez@gmail.com>
2015-09-09 10:54:25 +0100

    Less ambiguous manifest query

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -2,7 +2,7 @@ class Manifest < ActiveRecord::Base
   belongs_to :github_repository
   has_many :repository_dependencies, dependent: :destroy
 
-  scope :latest, -> { order("manifests.path, created_at DESC").select("DISTINCT on (manifests.path) *") }
+  scope :latest, -> { order("manifests.path, manifests.created_at DESC").select("DISTINCT on (manifests.path) *") }
 
   def github_link
     github_repository.blob_url + path
