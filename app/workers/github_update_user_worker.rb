9d7c78f11bf6163df15538042e08d1b12346ef34
Andrew Nesbitt <andrewnez@gmail.com>
2015-09-11 13:27:04 +0100

    Don’t redownload recently updated users

--- app/workers/github_update_user_worker.rb
+++ app/workers/github_update_user_worker.rb
@@ -4,6 +4,7 @@ class GithubUpdateUserWorker
 
   def perform(login)
     user = GithubUser.find_by_login(login)
+    return if user.updated_at > 2.day.ago
     user.download_from_github
     user.download_orgs
     user.download_repos
