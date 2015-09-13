437944363ac35f3718c654adcdf9b8401d0c63ae
Andrew Nesbitt <andrewnez@gmail.com>
2015-09-13 18:08:00 +0100

    Order forks by when they were last pushed at after stars

--- app/models/github_repository.rb
+++ app/models/github_repository.rb
@@ -33,7 +33,7 @@ class GithubRepository < ActiveRecord::Base
   scope :from_org, lambda{ |org_id|  where(github_organisation_id: org_id) }
   scope :without_manifests, -> { includes(:manifests).where(manifests: {github_repository_id: nil}) }
 
-  scope :interesting, -> { where('github_repositories.stargazers_count > 0').order('github_repositories.stargazers_count DESC') }
+  scope :interesting, -> { where('github_repositories.stargazers_count > 0').order('github_repositories.stargazers_count DESC, github_repositories.pushed_at DESC') }
 
   def self.language(language)
     where('lower(github_repositories.language) = ?', language.try(:downcase))
