820ac0d935c1ef36d25040f8435ddfdb1f50e20d
Andrew Nesbitt <andrewnez@gmail.com>
2015-10-30 18:32:03 +0000

    Added github tag worker

---
+++ app/workers/github_tag_worker.rb
@@ -0,0 +1,15 @@
+class GithubTagWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :low, unique: true
+
+  def perform(repo_name, token = nil)
+    token = token || AuthToken.token
+
+    github_repository = GithubRepository.find_by_full_name(repo_name)
+    if github_repository
+      github_repository.download_tags(token)
+    else
+      GithubRepository.create_from_github(repo_name, token)
+    end
+  end
+end
