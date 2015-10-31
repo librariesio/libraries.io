92ef8bb5458b4e1e2ec99ff0fde0329dff940368
Andrew Nesbitt <andrewnez@gmail.com>
2015-10-31 13:27:28 +0000

    Sort orgs by number of repos

--- app/controllers/github_organisations_controller.rb
+++ app/controllers/github_organisations_controller.rb
@@ -7,7 +7,7 @@ class GithubOrganisationsController < ApplicationController
 
   def mozilla
     @org_logins = ['mozilla', 'mozilla-it', 'bugzilla', 'drumbeat-badge-sprint', 'hackasaurus', 'jetpack-labs', 'mdn', 'mozbrick', 'mozilla-appmaker', 'mozilla-b2g', 'mozilla-comm', 'mozilla-cordova', 'mozilla-metrics', 'mozilla-raptor', 'mozilla-services', 'mozilla-svcops', 'mozillatw', 'Mozilla-TWQA', 'mozillahispano', 'MozillaSecurity', 'MozillaWiki', 'mozillayvr', 'mozfr', 'OpenNews', 'rust-lang', 'servo', 'tabulapdf', 'webcompat']
-    @orgs = GithubOrganisation.where(login: @org_logins).includes(:github_repositories)
+    @orgs = GithubOrganisation.where(login: @org_logins).includes(:github_repositories).sort_by{|o| o.github_repositories.length }
     @repositories = GithubRepository.where(github_organisation_id: @orgs.map(&:id))
     @licenses = @repositories.group('license').count
     @languages = @repositories.group('language').count
