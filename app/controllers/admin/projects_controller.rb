1d495fe7d145e5457dd8a51437d6529b6b3db624
Andrew Nesbitt <andrewnez@gmail.com>
2015-11-08 15:48:12 +0000

    order deprecated by rank

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -30,10 +30,10 @@ class Admin::ProjectsController < Admin::ApplicationController
     @search = Project.search('deprecated', filters: {
       platform: params[:platform]
     }, sort: params[:sort], order: params[:order])
-    @projects = @search.records.where("status IS ? OR status = ''", nil).paginate(page: params[:page])
+    @projects = @search.records.where("status IS ? OR status = ''", nil).order(:rank).paginate(page: params[:page])
     if @projects.empty?
       repo_ids = GithubRepository.with_projects.where("github_repositories.description ilike '%deprecated%'").pluck(:id)
-      @projects = Project.where("status IS ? OR status = ''", nil).where(github_repository_id: repo_ids).paginate(page: params[:page])
+      @projects = Project.where("status IS ? OR status = ''", nil).where(github_repository_id: repo_ids).order(:rank).paginate(page: params[:page])
     end
   end
 
