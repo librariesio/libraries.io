6155f2bb8f6a30e8390e7096a8e7c5e86e95d7a8
Andrew Nesbitt <andrewnez@gmail.com>
2015-11-09 22:01:10 +0000

    Add update github worker

---
+++ app/workers/github_update_worker.rb
@@ -0,0 +1,14 @@
+class GithubUpdateWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :low, unique: true
+
+  def perform(repo_name, token = nil)
+    token = token || AuthToken.token
+    github_repository = GithubRepository.find_by_full_name(repo_name)
+    if github_repository
+      github_repository.update_from_github
+    else
+      GithubRepository.create_from_github(repo_name, token)
+    end
+  end
+end
