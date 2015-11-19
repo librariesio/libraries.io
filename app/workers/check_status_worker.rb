1069e85bec96e8e07f4e96300d3975b9d3be37ba
Andrew Nesbitt <andrewnez@gmail.com>
2015-11-19 14:21:11 +0000

    Handle removed rubygems as well

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -4,8 +4,12 @@ class CheckStatusWorker
 
   def perform(project_id)
     project = Project.find_by_id project_id
-    if project && project.platform == 'NPM'
+    return unless project
+    if project.platform.downcase == 'npm'
       response = Typhoeus.get("https://www.npmjs.com/package/#{project.name}")
+      project.update_attribute(:status, 'Removed') if response.response_code == 404
+    elsif project.platform.downcase == 'rubygems'
+      response = Typhoeus.get("https://rubygems.org/gems/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     end
   end
