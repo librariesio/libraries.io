26cedd90829179ecd765a9bd1b4ed7ba3f9db180
Andrew Nesbitt <andrewnez@gmail.com>
2015-11-20 00:11:27 +0000

    Derp

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -16,46 +16,46 @@ class CheckStatusWorker
       response = Typhoeus.get("https://packagist.org/packages/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 302
     when 'nuget'
-      response = Typhoeus.get("https://www.nuget.org/packages/#{name}")
+      response = Typhoeus.get("https://www.nuget.org/packages/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'wordpress'
-      response = Typhoeus.get("https://wordpress.org/plugins/#{name}")
+      response = Typhoeus.get("https://wordpress.org/plugins/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'cpan'
-      response = Typhoeus.get("https://metacpan.org/release/#{name}")
+      response = Typhoeus.get("https://metacpan.org/release/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'clojars'
-      response = Typhoeus.get("https://clojars.org/#{name}")
+      response = Typhoeus.get("https://clojars.org/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'cocoapods'
-      response = Typhoeus.get("http://cocoapods.org/pods/#{name}")
+      response = Typhoeus.get("http://cocoapods.org/pods/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'hackage'
-      response = Typhoeus.get("http://hackage.haskell.org/package/#{name}")
+      response = Typhoeus.get("http://hackage.haskell.org/package/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'cran'
-      response = Typhoeus.get("http://cran.r-project.org/web/packages/#{name}/index.html")
+      response = Typhoeus.get("http://cran.r-project.org/web/packages/#{project.name}/index.html")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'atom'
-      response = Typhoeus.get("https://atom.io/packages/#{name}")
+      response = Typhoeus.get("https://atom.io/packages/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'cargo'
-      response = Typhoeus.get("https://crates.io/crates/#{name}/#{version}")
+      response = Typhoeus.get("https://crates.io/crates/#{project.name}/#{version}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'sublime'
-      response = Typhoeus.get("https://packagecontrol.io/packages/#{name}")
+      response = Typhoeus.get("https://packagecontrol.io/packages/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'pub'
-      response = Typhoeus.get("https://pub.dartlang.org/packages/#{name}")
+      response = Typhoeus.get("https://pub.dartlang.org/packages/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'hex'
-      response = Typhoeus.get("https://hex.pm/packages/#{name}/#{version}")
+      response = Typhoeus.get("https://hex.pm/packages/#{project.name}/#{version}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'elm'
-      response = Typhoeus.get("http://package.elm-lang.org/packages/#{name}/latest")
+      response = Typhoeus.get("http://package.elm-lang.org/packages/#{project.name}/latest")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     when 'dub'
-      response = Typhoeus.get("http://code.dlang.org/packages/#{name}")
+      response = Typhoeus.get("http://code.dlang.org/packages/#{project.name}")
       project.update_attribute(:status, 'Removed') if response.response_code == 404
     end
   end
