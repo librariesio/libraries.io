0a91787bef1b6dc399a66444a1b5d00205c41067
Andrew Nesbitt <andrewnez@gmail.com>
2015-12-03 12:21:07 +0000

    Quick fix!

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -2,7 +2,7 @@ class Manifest < ActiveRecord::Base
   belongs_to :github_repository
   has_many :repository_dependencies, dependent: :delete_all
 
-  scope :latest, -> { order("manifests.path, manifests.created_at DESC").select("DISTINCT on (manifests.path) *") }
+  scope :latest, -> { order("manifests.filepath, manifests.created_at DESC").select("DISTINCT on (manifests.filepath) *") }
 
   def github_link
     github_repository.blob_url + path
