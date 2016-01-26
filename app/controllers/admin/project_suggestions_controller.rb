f3af39153dacea077aaf76e9c0a4c5fc100159b3
Andrew Nesbitt <andrewnez@gmail.com>
2016-01-26 09:50:06 +0000

    Reorder project suggestions

--- app/controllers/admin/project_suggestions_controller.rb
+++ app/controllers/admin/project_suggestions_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProjectSuggestionsController < Admin::ApplicationController
   def index
-    @project_suggestions = ProjectSuggestion.all.paginate(page: params[:page])
+    @project_suggestions = ProjectSuggestion.all.order('created_at DESC').paginate(page: params[:page])
   end
 end
