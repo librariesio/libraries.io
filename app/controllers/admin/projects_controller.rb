372352caa4e14654269ea70d2ca515a53901011a
Andrew Nesbitt <andrewnez@gmail.com>
2016-02-01 10:00:03 +0000

    more broad unmaintained search

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -50,7 +50,7 @@ class Admin::ProjectsController < Admin::ApplicationController
   end
 
   def unmaintained
-    @search = Project.search('unmaintained', filters: {
+    @search = Project.search('maintained', filters: {
       platform: params[:platform]
     }, sort: params[:sort], order: params[:order])
 
@@ -65,7 +65,7 @@ class Admin::ProjectsController < Admin::ApplicationController
     @projects = @search.records.where("status IS ? OR status = ''", nil).order('rank DESC').paginate(page: params[:page])
     @platforms = @search.records.where("status IS ? OR status = ''", nil).pluck('platform').compact.uniq
     if @projects.empty?
-      repo_ids = GithubRepository.with_projects.where("github_repositories.description ilike '%unmaintained%'").pluck(:id)
+      repo_ids = GithubRepository.with_projects.where("github_repositories.description ilike '%maintained%'").pluck(:id)
       @projects = scope.where(github_repository_id: repo_ids).order('rank DESC').paginate(page: params[:page])
       @platforms = Project.where("status IS ? OR status = ''", nil).where(github_repository_id: repo_ids).pluck('platform').compact.uniq
     end
