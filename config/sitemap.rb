0568de2a6b20fce2800a350ef2335656f5e0d2e8
Andrew Nesbitt <andrewnez@gmail.com>
2016-02-08 09:13:20 +0000

    Sitemap tweaks

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -13,7 +13,7 @@ SitemapGenerator::Sitemap.create do
   add root_path, :priority => 1, :changefreq => 'daily'
 
   puts "Generating Projects"
-  Project.find_each do |project|
+  Project.not_removed.find_each do |project|
     add project_path(project.to_param), :lastmod => project.updated_at
     add project_dependents_path(project.to_param), :lastmod => project.updated_at, :priority => 0.4
     add project_dependent_repos_path(project.to_param), :lastmod => project.updated_at, :priority => 0.4
@@ -31,10 +31,11 @@ SitemapGenerator::Sitemap.create do
 
   Version.includes(:project).find_each do |version|
     next if version.project.nil?
+    next if version.project.is_removed?
     add version_path(version.to_param), :lastmod => version.project.updated_at
   end
 
-  GithubRepository.open_source.find_each do |repo|
+  GithubRepository.open_source.not_removed.find_each do |repo|
     add github_repository_path(repo.owner_name, repo.project_name), :lastmod => repo.updated_at
     add github_repository_contributors_path(repo.owner_name, repo.project_name), :lastmod => repo.updated_at
     add github_repository_forks_path(repo.owner_name, repo.project_name), :lastmod => repo.updated_at
@@ -65,19 +66,19 @@ SitemapGenerator::Sitemap.create do
   end
 
   add licenses_path, :priority => 0.7, :changefreq => 'daily'
-  Project.popular_licenses(:facet_limit => 120).each do |license|
+  Project.popular_licenses(:facet_limit => 150).each do |license|
     name = license.term
     add license_path(name), :lastmod => Project.license(name).order('updated_at DESC').first.try(:updated_at)
   end
 
   add languages_path, :priority => 0.7, :changefreq => 'daily'
-  Project.popular_languages(:facet_limit => 120).each do |language|
+  Project.popular_languages(:facet_limit => 150).each do |language|
     name = language.term
     add language_path(name), :lastmod => Project.language(name).order('updated_at DESC').first.try(:updated_at)
   end
 
   add keywords_path, :priority => 0.7, :changefreq => 'daily'
-  Project.popular_keywords(:facet_limit => 500).each do |keyword|
+  Project.popular_keywords(:facet_limit => 1000).each do |keyword|
     name = keyword.term
     add keyword_path(name), :lastmod => Project.keyword(name).order('updated_at DESC').first.try(:updated_at)
   end
