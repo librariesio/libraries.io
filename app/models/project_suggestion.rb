3fa1c341d5fa5ddc03ec6a6a95acce7095b678e2
Andrew Nesbitt <andrewnez@gmail.com>
2016-02-14 09:39:44 +0000

    Project suggestion improvements

--- app/models/project_suggestion.rb
+++ app/models/project_suggestion.rb
@@ -3,4 +3,14 @@ class ProjectSuggestion < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :project
+
+  def done?
+    !pending
+  end
+
+  def pending
+    (repository_url.present? && repository_url != project.repository_url) ||
+    (licenses.present? && licenses != project.licenses) ||
+    (status.present? && status != project.status)
+  end
 end
