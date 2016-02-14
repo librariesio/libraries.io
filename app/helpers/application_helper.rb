e6bfb684575aa6316e83b77b08c7e1f30fa8b4fe
Andrew Nesbitt <andrewnez@gmail.com>
2016-02-14 09:47:46 +0000

    Set default will_paginate rendered

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -216,4 +216,14 @@ module ApplicationHelper
       text
     end
   end
+
+  def will_paginate(collection_or_options = nil, options = {})
+    if collection_or_options.is_a? Hash
+      options, collection_or_options = collection_or_options, nil
+    end
+    unless options[:renderer]
+      options = options.merge :renderer => BootstrapPagination::Rails
+    end
+    super *[collection_or_options, options].compact
+  end
 end
