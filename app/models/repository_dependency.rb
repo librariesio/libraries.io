a8d29e617f1bea4ad6e3393a3a18601920534567
Andrew Nesbitt <andrewnez@gmail.com>
2016-02-26 13:10:40 +0000

    Can’t strip nil

--- app/models/repository_dependency.rb
+++ app/models/repository_dependency.rb
@@ -13,7 +13,7 @@ class RepositoryDependency < ActiveRecord::Base
   end
 
   def find_project_id
-    Project.platform(platform).where('lower(name) = ?', project_name.try(:downcase).strip).limit(1).pluck(:id).first
+    Project.platform(platform).where('lower(name) = ?', project_name.try(:downcase).try(:strip)).limit(1).pluck(:id).first
   end
 
   def incompatible_license?
