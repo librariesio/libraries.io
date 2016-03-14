d271c210ec185e1230327699c2af875e243f8467
Andrew Nesbitt <andrewnez@gmail.com>
2016-03-14 15:48:12 +0000

    Coverage specs

--- spec/controllers/platforms_controller_spec.rb
+++ spec/controllers/platforms_controller_spec.rb
@@ -23,4 +23,21 @@ RSpec.describe PlatformsController, elasticsearch: true do
       expect(response).to render_template("index")
     end
   end
+
+  describe "GET #show" do
+    before do
+      project.platform_class
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :show, id: project.platform
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the show template" do
+      get :show, id: project.platform
+      expect(response).to render_template("show")
+    end
+  end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -24,4 +24,159 @@ RSpec.describe ProjectsController, elasticsearch: true do
       expect(response).to render_template("index")
     end
   end
+
+  describe "GET #bus_factor" do
+    let!(:project) { create(:project) }
+    it "responds successfully with an HTTP 200 status code" do
+      get :bus_factor
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :bus_factor
+      expect(response).to render_template("bus_factor")
+    end
+
+    context "filtered by language" do
+      it "responds successfully with an HTTP 200 status code" do
+        get :bus_factor, language: 'Ruby'
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders the index template" do
+        get :bus_factor, language: 'Ruby'
+        expect(response).to render_template("bus_factor")
+      end
+    end
+  end
+
+  describe "GET #unlicensed" do
+    let!(:project) { create(:project) }
+
+    before do
+      project.platform_class
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :unlicensed
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :unlicensed
+      expect(response).to render_template("unlicensed")
+    end
+
+    context "filtered by platform" do
+      it "responds successfully with an HTTP 200 status code" do
+        get :unlicensed, platform: 'Rubygems'
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders the index template" do
+        get :unlicensed, platform: 'Rubygems'
+        expect(response).to render_template("unlicensed")
+      end
+    end
+  end
+
+  describe "GET #deprecated" do
+    let!(:project) { create(:project) }
+
+    before do
+      project.platform_class
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :deprecated
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :deprecated
+      expect(response).to render_template("deprecated")
+    end
+
+    context "filtered by platform" do
+      it "responds successfully with an HTTP 200 status code" do
+        get :deprecated, platform: 'Rubygems'
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders the index template" do
+        get :deprecated, platform: 'Rubygems'
+        expect(response).to render_template("deprecated")
+      end
+    end
+  end
+
+  describe "GET #removed" do
+    let!(:project) { create(:project) }
+
+    before do
+      project.platform_class
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :removed
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :removed
+      expect(response).to render_template("removed")
+    end
+
+    context "filtered by platform" do
+      it "responds successfully with an HTTP 200 status code" do
+        get :removed, platform: 'Rubygems'
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders the index template" do
+        get :removed, platform: 'Rubygems'
+        expect(response).to render_template("removed")
+      end
+    end
+  end
+
+  describe "GET #unmaintained" do
+    let!(:project) { create(:project) }
+
+    before do
+      project.platform_class
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :unmaintained
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :unmaintained
+      expect(response).to render_template("unmaintained")
+    end
+
+    context "filtered by platform" do
+      it "responds successfully with an HTTP 200 status code" do
+        get :unmaintained, platform: 'Rubygems'
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders the index template" do
+        get :unmaintained, platform: 'Rubygems'
+        expect(response).to render_template("unmaintained")
+      end
+    end
+  end
 end
