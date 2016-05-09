206fab0a19dc7272457b2714dd113e87e327f6af
Andrew Nesbitt <andrewnez@gmail.com>
2016-05-09 22:31:20 +0100

    remove deprecated raise_in_transactional_callbacks config option

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,6 @@ module Libraries
 
     config.middleware.use Rack::Deflater
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.active_job.queue_adapter = :sidekiq
 
     config.exceptions_app = routes
