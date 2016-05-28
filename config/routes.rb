9c8616216adbe8b39172e5f367dc395877b8d855
sam <samjacobclift@gmail.com>
2016-05-28 15:58:38 +0100

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -88,7 +88,7 @@ Rails.application.routes.draw do
   get 'deprecated-libraries', to: 'projects#deprecated', as: :deprecated
   get 'removed-libraries', to: 'projects#removed', as: :removed
 
-  get '/issues/help-wanted', to: 'help_wanted#index', as: :help_wanted
+  get '/help-wanted', to: 'help_wanted#index', as: :help_wanted
 
   get '/platforms', to: 'platforms#index', as: :platforms
 
