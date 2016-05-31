ed921c77c9ea333a69398f41d1f9ffffe87386e8
Andrew Nesbitt <andrewnez@gmail.com>
2016-05-31 21:28:05 +0100

    Updated bower to remove dependency on deprecated search api

--- app/models/repositories/bower.rb
+++ app/models/repositories/bower.rb
@@ -14,18 +14,12 @@ module Repositories
     def self.projects
       @projects ||= begin
         projects = {}
-        p1 = get("https://bower-component-list.herokuapp.com")
-        p2 = get("https://bower.herokuapp.com/packages")
+        data = get("https://bower.herokuapp.com/packages")
 
-        p2.each do |hash|
-          projects[hash['name'].downcase] = hash.slice('name', 'url', 'hits')
+        data.each do |hash|
+          projects[hash['name'].downcase] = hash.slice('name', 'url')
         end
 
-        p1.each do |hash|
-          if projects[hash['name'].downcase]
-            projects[hash['name'].downcase].merge! hash.slice('description', "owner", "website", "forks", "stars", "created", "updated","keywords")
-          end
-        end
         projects
       end
     end
@@ -37,9 +31,7 @@ module Repositories
     def self.mapping(project)
       {
         :name => project["name"],
-        :description => project["description"],
-        :keywords_array => Array.wrap(project["keywords"]),
-        :repository_url => repo_fallback(project["website"], project["url"])
+        :repository_url => repo_fallback(nil, project["url"])
       }
     end
   end
