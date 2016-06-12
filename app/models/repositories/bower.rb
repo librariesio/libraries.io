311dfb83ebb0828d88032a8aaf905452e89b288c
Andrew Nesbitt <andrewnez@gmail.com>
2016-06-12 13:34:55 +0100

    Load more information from bower.json files on github when importing bower projects, fixes #621

--- app/models/repositories/bower.rb
+++ app/models/repositories/bower.rb
@@ -29,10 +29,22 @@ module Repositories
     end
 
     def self.mapping(project)
+      bower_json = load_bower_json(project) || {}
       {
         :name => project["name"],
-        :repository_url => repo_fallback(nil, project["url"])
+        :repository_url => repo_fallback(nil, project["url"]),
+        :licenses => bower_json['license'],
+        :keywords_array => bower_json['keywords'],
+        :homepage => bower_json["homepage"],
+        :description => bower_json["description"]
       }
+    end
+
+    def self.load_bower_json(mapped_project)
+      return {} unless mapped_project['url']
+      github_name_with_owner = GithubUrls.parse(mapped_project['url'])
+      return {} unless github_name_with_owner
+      get_json("https://raw.githubusercontent.com/#{github_name_with_owner}/master/bower.json") rescue {}
     end
   end
 end
