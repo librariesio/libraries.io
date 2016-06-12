25e5412388b2492bcb391916ac322ca4d8d373c0
Andrew Nesbitt <andrewnez@gmail.com>
2016-06-12 23:21:36 +0100

    production cap config

--- Capfile
+++ Capfile
@@ -1,3 +1,5 @@
+require 'dotenv'
+Dotenv.load
 require "capistrano/setup"
 require "capistrano/deploy"
 require 'capistrano/rails'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,21 +1,10 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+server ENV['WEB_SERVER'], user: ENV['WEB_USER'], roles: %w{app web}
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-# server 'db.example.com', user: 'deploy', roles: %w{db}
-
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+namespace :deploy do
+  task :restart do
+    on roles(:all) do |host|
+      execute "(kill -s SIGUSR1 $(pgrep -f 'puma 3')) || restart librariesio"
+    end
+  end
+  after :publishing, :restart
+end
