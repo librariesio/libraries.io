01158b1078f6b8160c6493c989f1be0b706e71ea
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-01 15:53:01 +0100

    More performant status api

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -2,7 +2,9 @@ class Api::StatusController < Api::ApplicationController
   before_action :require_api_key
 
   def check
-    @projects = params[:projects].map{|project| Project.platform(project[:platform]).includes(:github_repository, :versions).find_by_name(project[:name]) }.compact
+    @projects = params[:projects].group_by{|project| project[:platform] }.map do |platform, projects|
+      Project.platform(platform).where(name: projects.map{|project| project[:name] }).includes(:github_repository, :versions)
+    end.flatten.compact
 
     render json: @projects.as_json(only: Project::API_FIELDS, methods: [:package_manager_url, :stars, :forks, :keywords], include: {versions: {only: [:number, :published_at]} })
   end
