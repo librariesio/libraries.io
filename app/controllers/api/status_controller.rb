d0a213e3255307772386831ff556f3b89d6e9620
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-07 14:10:53 +0100

    Only check projects if there are some to check

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -2,10 +2,13 @@ class Api::StatusController < Api::ApplicationController
   before_action :require_api_key
 
   def check
-    @projects = params[:projects].group_by{|project| project[:platform] }.map do |platform, projects|
-      Project.platform(platform).where(name: projects.map{|project| project[:name] }).includes(:github_repository, :versions)
-    end.flatten.compact
-
+    if params[:projects].any?
+      @projects = params[:projects].group_by{|project| project[:platform] }.map do |platform, projects|
+        Project.platform(platform).where(name: projects.map{|project| project[:name] }).includes(:github_repository, :versions)
+      end.flatten.compact
+    else
+      @projects = []
+    end
     render json: @projects.as_json(only: Project::API_FIELDS, methods: [:package_manager_url, :stars, :forks, :keywords], include: {versions: {only: [:number, :published_at]} })
   end
 end
