ecb7b4c789e979c0a556da48a370565b2e6e3c58
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-15 11:13:21 +0100

    better cap restart task

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server ENV['WEB_SERVER'], user: ENV['WEB_USER'], roles: %w{app web}
 namespace :deploy do
   task :restart do
     on roles(:all) do |host|
-      execute "(kill -s SIGUSR1 $(pgrep -f 'puma 3')) || restart librariesio"
+      execute "restart librariesio"
     end
   end
   after :publishing, :restart
