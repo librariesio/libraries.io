591f25be113ae8314e46e06526771e856c87b337
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-19 08:44:36 +0100

    remove Rack::Deflater for now

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Libraries
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.middleware.use Rack::Deflater
+    # config.middleware.use Rack::Deflater
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
