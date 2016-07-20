8f291330503c18b698f5be88d303f8cd37d1de29
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-20 11:27:41 +0100

    Only update source rank once per day

--- app/workers/update_source_rank_worker.rb
+++ app/workers/update_source_rank_worker.rb
@@ -4,6 +4,6 @@ class UpdateSourceRankWorker
 
   def perform(project_id)
     project = Project.find_by_id(project_id)
-    project.update_source_rank if project
+    project.update_source_rank if project && project.updated_at.present? && project.updated_at < 1.day.ago
   end
 end
