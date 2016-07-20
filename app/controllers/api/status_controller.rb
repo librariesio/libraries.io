047f0a2e26b0d493f73bc9e02d6387c84bd214b5
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-20 09:38:09 +0100

    less intensive sql queries in status api

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -4,7 +4,9 @@ class Api::StatusController < Api::ApplicationController
   def check
     if params[:projects].any?
       @projects = params[:projects].group_by{|project| project[:platform] }.map do |platform, projects|
-        Project.platform(platform).where(name: projects.map{|project| project[:name] }).includes(:github_repository, :versions)
+        projects.map{|project| project[:name] }.each_slice(20).map do |names_slice|
+          Project.platform(platform).where(name: names_slice).includes(:github_repository, :versions)
+        end
       end.flatten.compact
     else
       @projects = []
