b51fdd2ab94b1b4548d304b2d5707da3f48f420f
Andrew Nesbitt <andrewnez@gmail.com>
2016-08-30 17:50:24 +0100

    Experiment with a different port for talking to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
                     }
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
-    port:                 '587',
+    port:                 '2525',
     authentication:       :plain,
     user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
