ef9b8ea342b8bb3a61a9d412a1420ade0e4d023f
Andrew Nesbitt <andrewnez@gmail.com>
2016-10-04 11:42:17 +0100

    Allow passing options to AuthToken

--- app/models/auth_token.rb
+++ app/models/auth_token.rb
@@ -1,12 +1,12 @@
 class AuthToken < ActiveRecord::Base
-  def self.client
+  def self.client(options = {})
     if @auth_token && @auth_token.high_rate_limit?
-      return @auth_token.github_client
+      return @auth_token.github_client(options)
     end
     auth_token = order("RANDOM()").limit(100).sample
     if auth_token.high_rate_limit?
       @auth_token = auth_token
-      return auth_token.github_client
+      return auth_token.github_client(options)
     end
     client
   end
@@ -27,16 +27,16 @@ class AuthToken < ActiveRecord::Base
     false
   end
 
-  def github_client
-    AuthToken.new_client(token)
+  def github_client(options = {})
+    AuthToken.new_client(token, options)
   end
 
   def self.fallback_client(token = nil)
     AuthToken.new_client(token)
   end
 
-  def self.new_client(token)
+  def self.new_client(token, options = {})
     token ||= AuthToken.token
-    Octokit::Client.new(access_token: token, auto_paginate: true)
+    Octokit::Client.new({access_token: token, auto_paginate: true}.merge(options))
   end
 end
