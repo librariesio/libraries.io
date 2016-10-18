8c28d7c2999b597296a95d6e54495e116fa15d2d
Andrew Nesbitt <andrewnez@gmail.com>
2016-10-18 12:54:37 +0100

    Gracefull pump restarts

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,4 +15,7 @@ set :nginx_domains, "libraries.io staging.libraries.io"
 set :nginx_read_timeout, 60
 set :app_server_port, 5000
 
+set :puma_conf, "#{current_path}/config/puma.rb"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+
 set :maintenance_template_path, File.join(File.expand_path('../../public/system', __FILE__), 'maintenance.html')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,15 @@
 server ENV['WEB_SERVER'], user: ENV['WEB_USER'], roles: %w{app web}
-server ENV['SIDEKIQ_SERVER'], user: ENV['SIDEKIQ_USER'], roles: %w{app}
-server ENV['SIDEKIQ_SERVER2'], user: ENV['SIDEKIQ_USER'], roles: %w{app}
-server ENV['SIDEKIQ_SERVER3'], user: ENV['SIDEKIQ_USER'], roles: %w{app}
+server ENV['SIDEKIQ_SERVER'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
+server ENV['SIDEKIQ_SERVER2'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
+server ENV['SIDEKIQ_SERVER3'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 
 namespace :deploy do
   task :restart do
-    on roles(:app) do |host|
+    on roles(:worker) do |host|
       execute "restart librariesio"
+    end
+    on roles(:web) do |host|
+      execute "cd #{current_path} && bundle exec pumactl -P #{fetch(:puma_state)} -F #{fetch(:puma_conf)} restart"
     end
   end
   after :publishing, :restart
