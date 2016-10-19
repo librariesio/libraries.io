d775b276d2ac98461eb4f2189f53166350680bd9
Andrew Nesbitt <andrewnez@gmail.com>
2016-10-19 11:06:02 +0100

    Gem updates
    Fixes #836
    Fixes #837

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,16 +1,18 @@
-server ENV['WEB_SERVER'], user: ENV['WEB_USER'], roles: %w{app web}
+# server ENV['WEB_SERVER'], user: ENV['WEB_USER'], roles: %w{app web}
+# server '163.172.185.77', user: ENV['WEB_USER'], roles: %w{app web}
+# server '163.172.149.206', user: ENV['WEB_USER'], roles: %w{app web}
 server ENV['SIDEKIQ_SERVER'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 server ENV['SIDEKIQ_SERVER2'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 server ENV['SIDEKIQ_SERVER3'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 
 namespace :deploy do
   task :restart do
-    on roles(:worker) do |host|
+    on roles(:app) do |host|
       execute "restart librariesio"
     end
-    on roles(:web) do |host|
-      execute "cd #{current_path} && bundle exec pumactl -P #{fetch(:puma_state)} -F #{fetch(:puma_conf)} restart"
-    end
+    # on roles(:web) do |host|
+    #   execute "cd #{current_path} && bundle exec pumactl -P #{fetch(:puma_state)} restart"
+    # end
   end
   after :publishing, :restart
 end
