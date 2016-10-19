54219a824d67d9b4e9e4bc582c082c0cee2b0618
Andrew Nesbitt <andrewnez@gmail.com>
2016-10-19 13:31:09 +0100

    wrap whole worker in begin block

--- app/workers/github_issue_worker.rb
+++ app/workers/github_issue_worker.rb
@@ -3,12 +3,14 @@ class GithubIssueWorker
   sidekiq_options queue: :low, unique: :until_executed
 
   def perform(name_with_owner, issue_number, token = nil)
-    token = token || AuthToken.token
-    repo = GithubRepository.create_from_github(name_with_owner, token)
-    return unless repo
-    issue_hash = AuthToken.fallback_client(token).issue(repo.full_name, issue_number)
-    GithubIssue.create_from_hash(repo, issue_hash)
-  rescue Octokit::NotFound
-    nil
+    begin
+      token = token || AuthToken.token
+      repo = GithubRepository.create_from_github(name_with_owner, token)
+      return unless repo
+      issue_hash = AuthToken.fallback_client(token).issue(repo.full_name, issue_number)
+      GithubIssue.create_from_hash(repo, issue_hash)
+    rescue Octokit::NotFound
+      nil
+    end
   end
 end
