308c6743cf6171707b405aaa8f624437073971a6
Andrew Nesbitt <andrewnez@gmail.com>
2016-10-20 18:01:42 +0100

    Added third web server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 server ENV['WEB_SERVER_1'],     user: ENV['WEB_USER'],     roles: %w{app web}
 server ENV['WEB_SERVER_2'],     user: ENV['WEB_USER'],     roles: %w{app web}
+server ENV['WEB_SERVER_3'],     user: ENV['WEB_USER'],     roles: %w{app web}
 server ENV['SIDEKIQ_SERVER_1'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 server ENV['SIDEKIQ_SERVER_2'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 server ENV['SIDEKIQ_SERVER_3'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
