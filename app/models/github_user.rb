c380094b1642a6a25445693881ad5b060d56d974
Andrew Nesbitt <andrewnez@gmail.com>
2016-10-24 12:55:40 +0100

    A user has contributors too

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -6,6 +6,7 @@ class GithubUser < ActiveRecord::Base
   has_many :dependencies, through: :open_source_github_repositories
   has_many :favourite_projects, -> { group('projects.id').order("COUNT(projects.id) DESC") }, through: :dependencies, source: :project
   has_many :contributed_repositories, -> { GithubRepository.source.open_source }, through: :github_contributions, source: :github_repository
+  has_many :contributors, -> { group('github_users.id').order("sum(github_contributions.count) DESC") }, through: :open_source_github_repositories, source: :contributors
   has_many :fellow_contributors, -> (object){ where.not(id: object.id).group('github_users.id').order("COUNT(github_users.id) DESC") }, through: :contributed_repositories, source: :contributors
   has_many :projects, through: :open_source_github_repositories
 
