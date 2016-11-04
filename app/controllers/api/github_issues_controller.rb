bb846d4cb930953f18cceb1decd33bcd26d60db5
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-04 11:28:09 +0000

    Refactored api github issues controller

--- app/controllers/api/github_issues_controller.rb
+++ app/controllers/api/github_issues_controller.rb
@@ -1,21 +1,12 @@
 class Api::GithubIssuesController < Api::ApplicationController
   def help_wanted
-    @search = paginate GithubIssue.search('', filters: {
-      license: current_license,
-      language: current_language,
-      labels: (['help wanted'] + [params[:labels]]).compact
-    }), page: page_number, per_page: per_page_number
-    @github_issues = @search.records.includes(:github_repository)
+    search_issues((['help wanted'] + [params[:labels]]).compact)
+
     render json: @github_issues.as_json(only: GithubIssue::API_FIELDS, include: {:github_repository => {only: GithubRepository::API_FIELDS}})
   end
 
   def first_pull_request
-    @search = paginate GithubIssue.first_pr_search('', filters: {
-      license: current_license,
-      language: current_language,
-      labels: params[:labels]
-    }), page: page_number, per_page: per_page_number
-    @github_issues = @search.records.includes(:github_repository)
+    search_issues(params[:labels])
     render json: @github_issues.as_json(only: GithubIssue::API_FIELDS, include: {:github_repository => {only: GithubRepository::API_FIELDS}})
   end
 
@@ -27,5 +18,14 @@ class Api::GithubIssuesController < Api::ApplicationController
 
   def current_license
     params[:license] if params[:license].present?
+  end
+
+  def search_issues(labels)
+    @search = paginate GithubIssue.search('', filters: {
+      license: current_license,
+      language: current_language,
+      labels: labels
+    }), page: page_number, per_page: per_page_number
+    @github_issues = @search.records.includes(:github_repository)
   end
 end
