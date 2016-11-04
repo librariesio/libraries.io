6e8f156c202376a747c47ddbee1cd3f2d68cecf4
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-04 13:50:14 +0000

    Remove conditional assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -168,7 +168,8 @@ module ApplicationHelper
 
   def emojify(content)
     h(content).to_str.gsub(/:([\w+-]+):/) do |match|
-      if emoji = Emoji.find_by_alias($1)
+      emoji = Emoji.find_by_alias($1)
+      if emoji
         %(<img alt="#$1" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
       else
         match
