039ccd02dd5dfc967b78a28de8a72c014159d692
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-09 13:09:35 +0000

    Add support for grabbing elm dependencies

--- app/models/repositories/elm.rb
+++ app/models/repositories/elm.rb
@@ -1,7 +1,7 @@
 module Repositories
   class Elm < Base
     HAS_VERSIONS = true
-    HAS_DEPENDENCIES = false
+    HAS_DEPENDENCIES = true
     LIBRARIAN_SUPPORT = true
     URL = 'http://package.elm-lang.org/'
     COLOR = '#60B5CC'
@@ -35,6 +35,35 @@ module Repositories
     def self.versions(project)
       project['versions'].map do |v|
         { :number => v }
+      end
+    end
+
+    def self.dependencies(name, version, _project)
+      dependencies =find_dependencies(name, version)
+      return [] unless dependencies.any?
+      dependencies.map do |dependency|
+        {
+          project_name: dependency["name"],
+          requirements: dependency["version"],
+          kind: dependency["type"],
+          platform: self.name.demodulize
+        }
+      end
+    end
+
+    def self.find_dependencies(name, version)
+      begin
+        url = "https://raw.githubusercontent.com/#{name}/#{version}/elm-package.json"
+
+        contents = Typhoeus.get(url).body
+
+        r = Typhoeus::Request.new("https://librarian.libraries.io/v2/parse_file?filepath=elm-package.json",
+          method: :post,
+          body: {contents: contents},
+          headers: { 'Accept' => 'application/json' }).run
+        return Oj.load(r.body)
+      rescue
+        []
       end
     end
   end
