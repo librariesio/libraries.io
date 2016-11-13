0b8ae1b8ccdc2a7682b9f2f65dbb8e02f66deefa
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-13 23:09:13 +0000

    Dont load actioncable

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
