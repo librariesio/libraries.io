128e5a233c163818805315f9ed107b95771493ec
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-14 00:35:01 +0000

    Extract dependency tree resolver from view (#852)
    * Better tree resolving
    
    * Working Dependency tree resolving and rendering
    
    * Render dependency warnings

---
+++ app/helpers/tree_helper.rb
@@ -0,0 +1,40 @@
+module TreeHelper
+  def generate_dependency_tree(project, version, kind)
+    @project_names = []
+    @license_names = []
+    full_graph = load_dependencies_for(version, nil, kind, 0)
+    # if any of the same dependency twice in the tree
+
+    # flatten tree
+
+    # try to find common version number between all requirements
+
+    # otherwise warn
+  end
+
+  def load_dependencies_for(version, dependency, kind, index)
+    if version
+      @license_names << version.project.try(:normalize_licenses)
+      kind = index.zero? ? kind : 'normal'
+      dependencies = version.dependencies.kind(kind).includes(project: :versions).limit(100).order(:project_name)
+      {
+        version: version,
+        dependency: dependency,
+        requirements: dependency.try(:requirements),
+        dependencies: dependencies.map do |dep|
+          if dep.project && !@project_names.include?(dep.project_name)
+            @project_names << dep.project_name
+            index < 10 ? load_dependencies_for(dep.latest_resolvable_version, dep, kind, index + 1) : ['MORE']
+          else
+            {
+              version: dep.latest_resolvable_version,
+              requirements: dep.try(:requirements),
+              dependency: dep,
+              dependencies: []
+            }
+          end
+        end
+      }
+    end
+  end
+end
