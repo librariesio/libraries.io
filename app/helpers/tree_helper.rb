90194cc92b3f8c0b72a0edfc9bd4e53dc6a4a12b
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-14 00:23:24 +0000

    Working Dependency tree resolving and rendering

--- app/helpers/tree_helper.rb
+++ app/helpers/tree_helper.rb
@@ -1,6 +1,7 @@
 module TreeHelper
   def generate_dependency_tree(project, version, kind)
-    @project_names =[]
+    @project_names = []
+    @license_names = []
     full_graph = load_dependencies_for(version, nil, kind, 0)
     # if any of the same dependency twice in the tree
 
@@ -13,33 +14,24 @@ module TreeHelper
 
   def load_dependencies_for(version, dependency, kind, index)
     if version
+      @license_names << version.project.try(:normalize_licenses)
       kind = index.zero? ? kind : 'normal'
-      dependencies = version.dependencies.kind(kind).includes(project: :versions).limit(100)
-      if dependencies.length > 0
-        {
-          version: version,
-          dependencies: dependencies.map do |dependency|
-            if dependency.project && !@project_names.include?(dependency.project_name)
-              @project_names << dependency.project_name
-              {
-                version: dependency.latest_resolvable_version,
-                dependencies: index < 10 ? load_dependencies_for(dependency.latest_resolvable_version, dependency, kind, index + 1) : ['MORE']
-              }
-            else
-              {
-                version: dependency.latest_resolvable_version.try(:number)
-              }
-            end
-          end
-        }
-      else
-        {
-          version: version
-        }
-      end
-    else
+      dependencies = version.dependencies.kind(kind).includes(project: :versions).limit(100).order(:project_name)
       {
-        version: version
+        version: version,
+        requirements: dependency.try(:requirements),
+        dependencies: dependencies.map do |dep|
+          if dep.project && !@project_names.include?(dep.project_name)
+            @project_names << dep.project_name
+            index < 10 ? load_dependencies_for(dep.latest_resolvable_version, dep, kind, index + 1) : ['MORE']
+          else
+            {
+              version: dep.latest_resolvable_version,
+              requirements: dep.try(:requirements),
+              dependencies: []
+            }
+          end
+        end
       }
     end
   end
