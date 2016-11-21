a07398e50b9baf0e1bdb97c6572c6e076cb8e8f8
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-21 09:18:36 +0000

    Move tree renderer into a separate class

--- app/models/tree_resolver.rb
+++ app/models/tree_resolver.rb
@@ -1,16 +1,20 @@
-module TreeHelper
-  def generate_dependency_tree(project, version, kind)
+class TreeResolver
+  attr_accessor :project_names
+  attr_accessor :license_names
+
+  def initialize(project, version, kind)
+    @project = project
+    @version = version
+    @kind = kind
     @project_names = []
     @license_names = []
-    load_dependencies_for(version, nil, kind, 0)
-    # if any of the same dependency twice in the tree
-
-    # flatten tree
-
-    # try to find common version number between all requirements
-
-    # otherwise warn
   end
+
+  def generate_dependency_tree
+    load_dependencies_for(@version, nil, @kind, 0)
+  end
+
+  private
 
   def load_dependencies_for(version, dependency, kind, index)
     if version
