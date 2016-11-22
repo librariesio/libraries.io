c93973b7130c7338cb2c53a0739577b2fb861e9d
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-22 11:09:27 +0000

    Capistrano tweaks

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,13 +4,11 @@ server ENV['WEB_SERVER_3'],     user: ENV['WEB_USER'],     roles: %w{app web}
 server ENV['SIDEKIQ_SERVER_1'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 server ENV['SIDEKIQ_SERVER_2'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 server ENV['SIDEKIQ_SERVER_3'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
+server ENV['SIDEKIQ_SERVER_4'], user: ENV['SIDEKIQ_USER'], roles: %w{app worker}
 
 namespace :deploy do
   task :restart do
-    on roles(:web) do |host|
-      execute "touch #{current_path}/tmp/restart.txt"
-    end
-    on roles(:worker) do |host|
+    on roles(:app) do |host|
       execute "restart librariesio"
     end
   end
