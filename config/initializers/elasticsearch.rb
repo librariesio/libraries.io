1e4ea573d4183a3dfa3714f591fa6ffbd478725e
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-23 10:46:57 +0000

    Swap elasticsearch ENV var name

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,6 @@
 require 'typhoeus/adapters/faraday'
 
-url = ENV['ELASTICSEARCH_URL'] || 'localhost:9200'
+url = ENV['ELASTICSEARCH_CLUSTER_URL'] || 'localhost:9200'
 
 Elasticsearch::Model.client = Elasticsearch::Client.new url: url do |builder|
   builder.use :http_cache, store: Rails.cache, logger: Rails.logger, shared_cache: false, serializer: Marshal
