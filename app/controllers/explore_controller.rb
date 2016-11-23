64483b230ee719263223b7ff9b0db23dbfe56f20
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-23 23:33:05 +0000

    Fix explore

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -6,7 +6,7 @@ class ExploreController < ApplicationController
     @licenses = Project.popular_licenses(:facet_limit => 40).first(10)
 
     @trending_projects = Project.includes(:github_repository).recently_created.maintained.hacker_news.limit(10).to_a.uniq(&:name).first(6)
-    @trending_repos = GithubRepository.trending.hacker_news.limit(10).to_a.uniq(&:name).first(6)
+    @trending_repos = GithubRepository.trending.hacker_news.limit(10).to_a.uniq(&:full_name).first(6)
     @new_projects = Project.includes(:github_repository).maintained.order('projects.created_at desc').limit(6)
     @new_repos = GithubRepository.open_source.source.maintained.order('created_at desc').limit(6)
   end
