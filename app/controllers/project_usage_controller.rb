00a374f5ad08745ffa8591d7eeab7115a2c4594c
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-24 13:49:30 +0000

    Better sorting of prerelease version ranges

--- app/controllers/project_usage_controller.rb
+++ app/controllers/project_usage_controller.rb
@@ -5,7 +5,7 @@ class ProjectUsageController < ApplicationController
     @total = @all_counts.sum{|k,v| v }
     if @all_counts.any?
       @kinds = @project.repository_dependencies.group(:kind).count
-      @counts = @all_counts.sort_by{|_k,v| -v}.first(18).sort_by{|k,_v| k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'').split('.').map{|i| i.to_i} }
+      @counts = @all_counts.sort_by{|_k,v| -v}.first(18).sort_by{|k,_v| k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'').gsub('-','.').split('.').map{|i| i.to_i} }
       @highest_percentage = @counts.map{|_k,v| v.to_f/@total*100 }.max
       scope = @project.dependent_repositories.open_source
       scope = scope.where("repository_dependencies.requirements = ?", params[:requirements]) if params[:requirements].present?
