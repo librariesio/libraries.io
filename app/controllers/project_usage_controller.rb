f54c6809bb20140f16d43cc70b66edd22b483c86
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-24 19:00:47 +0000

    Ensure only one manifest per repo is counted towards usage

--- app/controllers/project_usage_controller.rb
+++ app/controllers/project_usage_controller.rb
@@ -1,10 +1,10 @@
 class ProjectUsageController < ApplicationController
   before_action :find_project
   def show
-    @all_counts = @project.repository_dependencies.group(:requirements).count
+    @all_counts = @project.repository_dependencies.joins(:manifest).distinct('manifests.github_repository_id').group('repository_dependencies.requirements').count
     @total = @all_counts.sum{|k,v| v }
     if @all_counts.any?
-      @kinds = @project.repository_dependencies.group(:kind).count
+      @kinds = @project.repository_dependencies.joins(:manifest).distinct('manifests.github_repository_id').group('repository_dependencies.kind').count
       @counts = @all_counts.sort_by{|_k,v| -v}.first(18).sort_by{|k,_v| k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'').gsub('-','.').split('.').map{|i| i.to_i} }
       @highest_percentage = @counts.map{|_k,v| v.to_f/@total*100 }.max
       scope = @project.dependent_repositories.open_source
