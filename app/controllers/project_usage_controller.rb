42b81f54cca7827bf65ffccdcb058820057200d9
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-24 19:17:19 +0000

    Handle nil requirements gracefully

--- app/controllers/project_usage_controller.rb
+++ app/controllers/project_usage_controller.rb
@@ -5,12 +5,26 @@ class ProjectUsageController < ApplicationController
     @total = @all_counts.sum{|k,v| v }
     if @all_counts.any?
       @kinds = @project.repository_dependencies.joins(:manifest).distinct('manifests.github_repository_id').group('repository_dependencies.kind').count
-      @counts = @all_counts.sort_by{|_k,v| -v}.first(18).sort_by{|k,_v| k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'').gsub('-','.').split('.').map{|i| i.to_i} }
+      @counts = sort_by_semver_range(18)
       @highest_percentage = @counts.map{|_k,v| v.to_f/@total*100 }.max
       scope = @project.dependent_repositories.open_source
       scope = scope.where("repository_dependencies.requirements = ?", params[:requirements]) if params[:requirements].present?
       scope = scope.where("repository_dependencies.kind = ?", params[:kind]) if params[:kind].present?
       @repos = scope.paginate(page: page_number, per_page: 20)
     end
+  end
+
+  private
+
+  helper_method :sort_by_semver_range
+  def sort_by_semver_range(limit)
+    @all_counts.sort_by{|_k,v| -v}
+               .first(limit)
+               .select{|k,v| k.present? }
+               .sort_by{|k,_v|
+                 k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'')
+                 .gsub('-','.')
+                 .split('.').map{|i| i.to_i}
+               }
   end
 end
