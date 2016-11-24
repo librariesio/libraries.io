dc9119cdcf96823a93b09ad611bb02e64dac718c
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-24 13:26:41 +0000

    Add seo meta data on usage page

--- app/controllers/project_usage_controller.rb
+++ app/controllers/project_usage_controller.rb
@@ -2,9 +2,9 @@ class ProjectUsageController < ApplicationController
   before_action :find_project
   def show
     @all_counts = @project.repository_dependencies.group(:requirements).count
+    @total = @all_counts.sum{|k,v| v }
     if @all_counts.any?
       @kinds = @project.repository_dependencies.group(:kind).count
-      @total = @all_counts.sum{|k,v| v }
       @counts = @all_counts.sort_by{|_k,v| -v}.first(18).sort_by{|k,_v| k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'').split('.').map{|i| i.to_i} }
       @highest_percentage = @counts.map{|_k,v| v.to_f/@total*100 }.max
       scope = @project.dependent_repositories.open_source
