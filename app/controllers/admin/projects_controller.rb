ba72f13ffc46533abf3eb966fdc6a7813afc3586
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-25 14:52:31 +0000

    Sync projects when updated from admin

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -8,6 +8,7 @@ class Admin::ProjectsController < Admin::ApplicationController
     if @project.update_attributes(project_params)
       @project.normalize_licenses
       @project.update_github_repo_async
+      @project.async_sync
       @project.github_repository.try(:update_all_info_async)
       redirect_to project_path(@project.to_param)
     else
