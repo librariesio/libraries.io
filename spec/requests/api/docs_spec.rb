d8fa7b8bedf95769025215748a905d18e46b5b0f
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-09 17:25:07 +0000

    Removed comment

--- spec/requests/api/docs_spec.rb
+++ spec/requests/api/docs_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 describe "API::DocController" do
   describe "GET /api/", :vcr, type: :request do
     it "renders successfully" do
-      # create base62 project and version
       project = create(:project, name: 'base62', platform: 'npm')
       create(:version, project: project)
       create(:github_repository, full_name: 'gruntjs/grunt')
