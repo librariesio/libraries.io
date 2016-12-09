6a73ab4830f9d46ced06c873c6bae32ec65c9b80
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-09 18:15:13 +0000

    Added github users api request spec

---
+++ spec/requests/api/github_users_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe "Api::GithubUsersController" do
+  before :each do
+    @user = create(:github_user)
+  end
+
+  describe "GET /api/github/:login", :vcr, type: :request do
+    it "renders successfully" do
+      get "/api/github/#{@user.login}"
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(response.body).to be_json_eql @user.to_json
+    end
+  end
+
+  describe "GET /api/github/:login/repositories", :vcr, type: :request do
+    it "renders successfully" do
+      repo = create(:github_repository, github_user: @user)
+      get "/api/github/#{@user.login}/repositories"
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(response.body).to be_json_eql [repo.as_json({except: [:id, :github_organisation_id, :owner_id]})].to_json
+    end
+  end
+
+  describe "GET /api/github/:login/projects", :vcr, type: :request do
+    it "renders successfully" do
+      get "/api/github/#{@user.login}/projects"
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(json).to eq []
+    end
+  end
+end
