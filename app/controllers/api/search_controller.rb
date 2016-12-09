0896d0e3f3a77dbea66b3bd1e23ff747f4f05417
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-09 18:12:07 +0000

    Remove unused method in search controller

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,6 +1,4 @@
 class Api::SearchController < Api::ApplicationController
-  skip_before_action :check_api_key, only: :searchcode
-
   def index
     @search = paginate search_projects(params[:q])
     ids = @search.map{|r| r.id.to_i }
@@ -8,10 +6,6 @@ class Api::SearchController < Api::ApplicationController
     @projects = @search.records.includes(:github_repository, :versions).sort_by { |u| indexes[u.id] }
 
     render json: project_json_response(@projects)
-  end
-
-  def searchcode
-    render json: Project.where('updated_at > ?', 1.day.ago).pluck(:repository_url).compact.reject(&:blank?)
   end
 
   private
