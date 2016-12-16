d82c72debc00b9fb92ec36e7063c42b6d3cf354b
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-16 14:24:08 +0000

    Added more request specs

---
+++ spec/requests/api/search_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe "API::SearchController", :vcr do
+  describe "GET /api/search", type: :request do
+    it "renders successfully" do
+      get "/api/search"
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(response.body).to be_json_eql []
+    end
+  end
+end
---
+++ spec/requests/api/status_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "API::StatusController", :vcr do
+  let(:user) { create(:user) }
+  describe "GET /api/check", type: :request do
+    it "renders successfully" do
+      post "/api/check", params: {api_key: user.api_key, projects: [{name: 'rails', platform: 'rubygems'}] }
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(response.body).to be_json_eql []
+    end
+  end
+end
