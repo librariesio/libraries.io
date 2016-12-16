294ffd234e339d64a5446d650e59c572602ecf58
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-16 15:27:23 +0000

    Show "Other" column on usage graph, fixes #931

--- app/controllers/project_usage_controller.rb
+++ app/controllers/project_usage_controller.rb
@@ -1,11 +1,11 @@
 class ProjectUsageController < ApplicationController
   before_action :find_project
   def show
-    @all_counts = @project.repository_dependencies.where('github_repositories.fork = ?', false).joins(manifest: :github_repository).distinct('manifests.github_repository_id').group('repository_dependencies.requirements').count
+    @all_counts = @project.repository_dependencies.where('github_repositories.fork = ?', false).joins(manifest: :github_repository).distinct('manifests.github_repository_id').group('repository_dependencies.requirements').count.select{|k,v| k.present? }
     @total = @all_counts.sum{|k,v| v }
     if @all_counts.any?
       @kinds = @project.repository_dependencies.where('github_repositories.fork = ?', false).joins(manifest: :github_repository).distinct('manifests.github_repository_id').group('repository_dependencies.kind').count
-      @counts = sort_by_semver_range(18)
+      @counts = sort_by_semver_range(@all_counts.length > 18 ? 17 : 18)
       @highest_percentage = @counts.map{|_k,v| v.to_f/@total*100 }.max
       scope = @project.dependent_repositories.open_source.source
       scope = scope.where("repository_dependencies.requirements = ?", params[:requirements]) if params[:requirements].present?
@@ -20,7 +20,6 @@ class ProjectUsageController < ApplicationController
   def sort_by_semver_range(limit)
     @all_counts.sort_by{|_k,v| -v}
                .first(limit)
-               .select{|k,v| k.present? }
                .sort_by{|k,_v|
                  k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'')
                  .gsub('-','.')
