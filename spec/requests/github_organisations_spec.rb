150ff940faea6bb131091990e4cee890440899ff
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-21 16:42:38 +0000

    More request specs

---
+++ spec/requests/github_organisations_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe "ExploreController", :vcr do
+  describe "GET /github/organisations", type: :request do
+    it "renders successfully when logged out" do
+      visit github_organisations_path
+      expect(page).to have_content 'Organisations'
+    end
+  end
+end
---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe "SessionsController", :vcr do
+  describe "GET /login", type: :request do
+    it "redirects to github" do
+      get login_path
+      expect(response).to redirect_to('/auth/github')
+    end
+  end
+
+  describe "GET /enable_public", type: :request do
+    it "redirects to github" do
+      get enable_public_path
+      expect(response).to redirect_to('/auth/github_public')
+    end
+  end
+
+  describe "GET /enable_private", type: :request do
+    it "redirects to github" do
+      get enable_private_path
+      expect(response).to redirect_to('/auth/github_private')
+    end
+  end
+
+  describe "GET /logout", type: :request do
+    it "redirects to root" do
+      get logout_path
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe "GET /auth/failure", type: :request do
+    it "redirects to root" do
+      post '/auth/failure'
+      expect(response).to redirect_to(root_path)
+    end
+  end
+end
