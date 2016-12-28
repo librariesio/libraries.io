431a235ea9797aee9bcf3920eab737f088820945
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-28 16:42:29 +0000

    Fix typo

--- app/workers/update_repository_source_rank_worker.rb
+++ app/workers/update_repository_source_rank_worker.rb
@@ -3,7 +3,7 @@ class UpdateRepositorySourceRankWorker
   sidekiq_options :queue => :low, unique: :until_executed
 
   def perform(github_repository_id)
-    repo = GitHubRepository.find_by_id(github_repository_id)
+    repo = GithubRepository.find_by_id(github_repository_id)
     repo.update_source_rank if repo && repo.updated_at.present? && repo.updated_at < 1.day.ago
   end
 end
