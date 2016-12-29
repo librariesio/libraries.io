3e11c9fa84116c84a5b0d75feb823eb232e3ea21
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-29 11:58:00 +0000

    Error controller specs

---
+++ spec/requests/errors_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe "ErrorsController", :vcr do
+  describe "GET /404", type: :request do
+    it "renders successfully when logged out" do
+      visit '/404'
+      expect(page).to have_content "We can't find whatever it was you were looking for."
+    end
+  end
+
+  describe "GET /422", type: :request do
+    it "renders successfully when logged out" do
+      visit '/422'
+      expect(page).to have_content "Nope. Couldn't understand a word of that."
+    end
+  end
+
+  describe "GET /500", type: :request do
+    it "renders successfully when logged out" do
+      visit '/500'
+      expect(page).to have_content "Oh no!  We've had a problem at our end."
+    end
+  end
+end
