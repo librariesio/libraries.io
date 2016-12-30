eabec67a66327fc579fc2551db973cb2c897166d
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-30 16:12:38 +0000

    Add recommendations controller specs

---
+++ spec/requests/recommendations_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "RecommendationsController", :vcr do
+  let(:user) { create(:user) }
+
+  describe "GET /recommendations renders successfully when logged in", type: :request do
+    it "denies access when logged out" do
+      get recommendations_path
+      expect(response).to redirect_to(login_path)
+    end
+
+    it "renders successfully when logged in" do
+      login(user)
+      visit recommendations_path
+      expect(page).to have_content 'Recommended Libraries'
+    end
+  end
+end
