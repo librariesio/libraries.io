a19a49ccb339be89de56491a0e79aaeb65494a30
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-30 16:03:34 +0000

    Added project suggestion controller specs

---
+++ spec/requests/project_suggestions_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+describe "ProjectSuggestionsController", :vcr do
+  let(:user) { create(:user) }
+  let(:project) { create(:project) }
+
+  # get '/:platform/:name/suggestions', to: 'project_suggestions#new', as: :project_suggestions, constraints: { :name => /.*/ }
+  # post '/:platform/:name/suggestions', to: 'project_suggestions#create', constraints: { :name => /.*/ }
+
+  describe "GET /:platform/:name/suggestions", type: :request do
+    it "redirects to /login" do
+      get project_suggestions_path(project.to_param)
+      expect(response).to redirect_to(login_path)
+    end
+  end
+
+  describe "GET renders successfully when logged in", type: :request do
+    it "renders successfully when logged in" do
+      login(user)
+      visit project_suggestions_path(project.to_param)
+      expect(page).to have_content 'Add/Update data for'
+    end
+  end
+
+  describe "POST /:platform/:name/suggestions", type: :request do
+    it "redirects to /:platform/:name" do
+      login(user)
+      rack_test_session_wrapper = Capybara.current_session.driver
+      rack_test_session_wrapper.submit :post, project_suggestions_path(project.to_param), {project_suggestion: {license: 'MIT', notes: 'From LICENSE file'}}
+
+      expect(page.current_path).to eq project_path(project.to_param)
+    end
+  end
+end
