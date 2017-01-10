c5021a05b95de4baaf918fa7fceda08b15b76821
Andrew Nesbitt <andrewnez@gmail.com>
2017-01-10 18:37:53 +0000

    Fix specs

--- spec/models/package_manager/cran_spec.rb
+++ spec/models/package_manager/cran_spec.rb
@@ -9,11 +9,11 @@ describe PackageManager::CRAN, :vcr do
     let(:project) { create(:project, name: 'foo', platform: described_class.name) }
 
     it 'returns a link to project website' do
-      expect(described_class.package_link(project)).to eq("https://CRAN.R-project.org/package=foo")
+      expect(described_class.package_link(project)).to eq("https://cran.r-project.org/package=foo")
     end
 
     it 'ignores version' do
-      expect(described_class.package_link(project, '2.0.0')).to eq("https://CRAN.R-project.org/package=foo")
+      expect(described_class.package_link(project, '2.0.0')).to eq("https://cran.r-project.org/package=foo")
     end
   end
 
