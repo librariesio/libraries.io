3f197bdc319c4b78bf15a715f221b17eef2881d8
Andrew Nesbitt <andrewnez@gmail.com>
2017-01-17 09:13:17 +0000

    Swap tipsy for bootstrap tooltip

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( tipsy/tipsy.gif email.css emoji/*.png admin.js )
+Rails.application.config.assets.precompile += %w( email.css emoji/*.png admin.js )
