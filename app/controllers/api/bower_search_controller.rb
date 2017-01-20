73da30b33f9d3eca385e096660ad93549fbb8f7d
Andrew Nesbitt <andrewnez@gmail.com>
2017-01-20 18:01:09 +0000

    Don't do prefix searching for bower endpoint

--- app/controllers/api/bower_search_controller.rb
+++ app/controllers/api/bower_search_controller.rb
@@ -4,7 +4,7 @@ class Api::BowerSearchController < Api::ApplicationController
   def index
     @search = paginate Project.search(params[:q] || '', filters: {
       platform: 'Bower',
-    }, prefix: true, sort: format_sort, order: format_order), page: page_number, per_page: per_page_number
+    }, sort: format_sort, order: format_order), page: page_number, per_page: per_page_number
     ids = @search.map{|r| r.id.to_i }
     indexes = Hash[ids.each_with_index.to_a]
     @projects = @search.records.includes(:github_repository, :versions).sort_by { |u| indexes[u.id] }
