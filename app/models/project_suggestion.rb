c607c2304e964ec38c79f6c5c2800b83d04acdd9
Andrew Nesbitt <andrewnez@gmail.com>
2017-01-30 15:10:59 +0000

    Project suggestion refactoring

--- app/models/project_suggestion.rb
+++ app/models/project_suggestion.rb
@@ -5,12 +5,27 @@ class ProjectSuggestion < ApplicationRecord
   belongs_to :project
 
   def done?
+    return true if project.nil?
     !pending
   end
 
   def pending
-    (repository_url.present? && repository_url != project.try(:repository_url)) ||
-    (licenses.present? && licenses != project.try(:licenses)) ||
-    (status.present? && status != project.try(:status))
+    valid_repository_change? || valid_license_change? || valid_status_change?
+  end
+
+  def valid_repository_change?
+    return false unless repository_url.present?
+    repository_url != project.try(:repository_url)
+  end
+
+  def valid_license_change?
+    return false unless licenses.present?
+    licenses != project.try(:licenses)
+  end
+
+  def valid_status_change?
+    return false unless status.present?
+    return false if project.status.blank? && status == 'Active'
+    status != project.try(:status)
   end
 end
