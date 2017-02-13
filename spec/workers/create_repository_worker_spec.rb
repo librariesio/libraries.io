20f11117250b09ec55aa66c49478dd5be1d5ce3f
Andrew Nesbitt <andrewnez@gmail.com>
2017-02-13 10:16:25 +0000

    Fix specs

--- spec/workers/create_repository_worker_spec.rb
+++ spec/workers/create_repository_worker_spec.rb
@@ -7,7 +7,7 @@ describe CreateRepositoryWorker do
 
   it "should create from github" do
     repo_name = 'rails/rails'
-    expect(Repository).to receive(:create_from_github).with(repo_name, nil)
-    subject.perform(repo_name)
+    expect(Repository).to receive(:create_from_host).with('GitHub', repo_name, nil)
+    subject.perform('GitHub', repo_name)
   end
 end
