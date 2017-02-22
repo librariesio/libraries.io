2f59df8dd5997d12e374c24916e7ecd4dcee75ab
Andrew Nesbitt <andrewnez@gmail.com>
2017-02-22 16:28:55 +0000

    Refactored some duplicate code

--- app/controllers/api/tree_controller.rb
+++ app/controllers/api/tree_controller.rb
@@ -1,18 +1,9 @@
 class Api::TreeController < Api::ApplicationController
   before_action :require_api_key
   before_action :find_project
+  before_action :load_tree_resolver
 
   def show
-    @date = Date.parse(params[:date]) rescue Date.today
-
-    if params[:number].present?
-      @version = @project.versions.find_by_number(params[:number])
-    else
-      @version = @project.versions.where('versions.published_at <= ?', @date).select(&:stable?).sort.first
-    end
-    raise ActiveRecord::RecordNotFound if @version.nil?
-
-    @kind = params[:kind] || 'runtime'
-    render json: TreeResolver.new(@version, @kind, @date).tree
+    render json: @tree_resolver.tree
   end
 end
