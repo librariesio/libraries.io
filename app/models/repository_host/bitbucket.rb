7f2d2f1dd2268e22970da6d09fa720ad97008d25
Andrew Nesbitt <andrewnez@gmail.com>
2017-02-23 11:38:18 +0000

    Scrape bitbucket site for default branch

--- app/models/repository_host/bitbucket.rb
+++ app/models/repository_host/bitbucket.rb
@@ -12,6 +12,12 @@ module RepositoryHost
       nil
     end
 
+    def self.get_default_branch(full_name)
+      # n.b only works for public repos
+      r = Typhoeus.get("https://bitbucket.org/#{full_name}/branches/")
+      Nokogiri::HTML(r.body).css('.main-branch .branch-header a').try(:text) || 'master'
+    end
+
     def get_file_list(token = nil)
       api_client(token).get_request("1.0/repositories/#{full_name}/directory/")[:values]
     end
@@ -134,6 +140,7 @@ module RepositoryHost
       project = client.repos.get(user_name, repo_name)
       v1_project = client.repos.get(user_name, repo_name, api_version: '1.0')
       repo_hash = project.to_hash.with_indifferent_access.slice(:description, :language, :full_name, :name, :has_wiki, :has_issues, :scm)
+      default_branch = get_default_branch(full_name)
 
       repo_hash.merge!({
         id: project.uuid,
@@ -145,6 +152,7 @@ module RepositoryHost
         updated_at: project.updated_on,
         subscribers_count: v1_project.followers_count,
         forks_count: v1_project.forks_count,
+        default_branch: default_branch,
         private: project.is_private,
         size: project[:size].to_f/1000,
         parent: {
