62c8e5e7c939dd199d1f5b2203e46f8db391fa33
benjam <ben@libraries.io>
2017-02-23 16:23:47 +0000

    Add/update tests
    because tests are a thing. Fo sho.

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,7 +4,13 @@ describe "PagesController", :vcr do
   describe "GET /about", type: :request do
     it "renders successfully when logged out" do
       visit about_path
-      expect(page).to have_content 'What is Libraries.io?'
+      expect(page).to have_content 'About'
+    end
+  end
+  describe "GET /packagemanagercompatibility", type: :request do
+    it "renders successfully when logged out" do
+      visit pmmatrix_path
+      expect(page).to have_content 'Package Manager Compatibility Matrix'
     end
   end
 end
