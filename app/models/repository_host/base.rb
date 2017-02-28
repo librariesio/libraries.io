6a39712f0696612b087dc4ea4b71d4d6f1c0eaa9
Andrew Nesbitt <andrewnez@gmail.com>
2017-02-28 11:19:52 +0000

    Fix check repo status

--- app/models/repository_host/base.rb
+++ app/models/repository_host/base.rb
@@ -11,7 +11,7 @@ module RepositoryHost
     end
 
     def self.domain(host_type)
-      RepositoryHost.const_get(host_type.capitalize).new.domain
+      RepositoryHost.const_get(host_type.capitalize).new(nil).domain
     end
 
     def url
