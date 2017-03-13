40af025d6d6000069814b986ebf16d18cb68acc8
Andrew Nesbitt <andrewnez@gmail.com>
2017-03-13 17:04:30 +0000

    Create repository to index for admin repositories spec

--- spec/requests/admin/repositories_spec.rb
+++ spec/requests/admin/repositories_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe "Admin::RepositoriesController" do
   let(:user) { create :user }
+  let!(:repository) { create :repository }
 
   describe "GET /admin/repositories", type: :request do
     it "denies access when logged out" do
