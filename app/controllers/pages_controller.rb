5c4c1857cfcd253da07b65fad0b2d0b026d74ea4
benjam <ben@libraries.io>
2017-03-21 17:06:22 +0000

    Move page to pages#terms
    And fix some issues

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,7 @@ class PagesController < ApplicationController
   def compatibility
     
   end
+  def terms
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
     post '/check', to: 'status#check'
 
     get '/', to: 'docs#index'
-    get '/terms', to: 'docs#terms'
     get '/search', to: 'search#index'
     get '/bower-search', to: 'bower_search#index'
     get '/searchcode', to: 'projects#searchcode'
@@ -186,6 +185,7 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about', as: :about
   get '/team', to: 'pages#team', as: :team
   get '/privacy', to: 'pages#privacy', as: :privacy
+  get '/terms', to: 'pages#terms', as: :terms
   get '/compatibility', to: 'pages#compatibility', as: :compatibility
 
 
