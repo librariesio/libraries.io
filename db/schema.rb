a6d7a339c133ec244e36b01a7906555017fa1a49
Andrew Nesbitt <andrewnez@gmail.com>
2017-04-11 15:38:21 +0100

    enable pg_stat_statements extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411141219) do
+ActiveRecord::Schema.define(version: 20170411143624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "api_keys", force: :cascade do |t|
     t.string   "access_token"
