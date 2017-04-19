5aa7a566453263a8841df940e4de911ef5eb8efb
Andrew Nesbitt <andrewnez@gmail.com>
2017-04-19 12:25:57 +0100

    Make cron machine first in capistrano config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '51.15.130.54',    user: 'root', roles: %w{cron}
+
 server '163.172.185.77',  user: 'root', roles: %w{app web}
 server '163.172.154.122', user: 'root', roles: %w{app web}
 server '163.172.161.163', user: 'root', roles: %w{app web}
@@ -8,8 +10,6 @@ server '163.172.138.184', user: 'root', roles: %w{app worker}
 server '163.172.155.116', user: 'root', roles: %w{app worker}
 server '163.172.165.101', user: 'root', roles: %w{app worker}
 server '51.15.141.224', user: 'root', roles: %w{app worker}
-
-server '51.15.130.54',    user: 'root', roles: %w{cron}
 
 namespace :deploy do
   task :restart do
