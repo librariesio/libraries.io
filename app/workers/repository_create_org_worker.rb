11bd43d4690a52e2b56c8295cc20855e00c1e74b
Andrew Nesbitt <andrewnez@gmail.com>
2017-05-15 16:27:32 +0100

    Fix downloading of orgs and users

--- app/workers/repository_create_org_worker.rb
+++ app/workers/repository_create_org_worker.rb
@@ -3,6 +3,6 @@ class RepositoryCreateOrgWorker
   sidekiq_options queue: :owners, unique: :until_executed
 
   def perform(host_type, org_login)
-    RepositoryOwner::Base.download_org_from_host(host_type, org_login)
+    RepositoryOwner.const_get(host_type.capitalize).download_org_from_host(host_type, org_login)
   end
 end
--- app/workers/repository_create_user_worker.rb
+++ app/workers/repository_create_user_worker.rb
@@ -2,7 +2,7 @@ class RepositoryCreateUserWorker
   include Sidekiq::Worker
   sidekiq_options queue: :owners, unique: :until_executed
 
-  def perform(host_type, org_login)
-    RepositoryOwner::Base.download_user_from_host(host_type, org_login)
+  def perform(host_type, user_login)
+    RepositoryOwner.const_get(host_type.capitalize).download_user_from_host(host_type, user_login)
   end
 end
