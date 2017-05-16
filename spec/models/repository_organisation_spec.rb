ca32746f34d6f0c780e77c904037839d1499d07a
Andrew Nesbitt <andrewnez@gmail.com>
2017-05-16 12:29:01 +0100

    Fix specs

--- spec/models/repository_organisation_spec.rb
+++ spec/models/repository_organisation_spec.rb
@@ -9,6 +9,6 @@ describe RepositoryOrganisation, type: :model do
   it { should have_many(:contributors) }
   it { should have_many(:projects) }
 
-  it { should validate_uniqueness_of(:login) }
+  it { should validate_presence_of(:uuid) }
   it { should validate_uniqueness_of(:uuid) }
 end
