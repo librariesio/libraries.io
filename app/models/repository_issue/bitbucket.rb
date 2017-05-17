e63251e96f6b552004c596f0f071c23833f6fabe
Andrew Nesbitt <andrewnez@gmail.com>
2017-05-17 17:29:20 +0100

    Mostly working Bitbucket issue syncing

--- app/models/repository_issue/bitbucket.rb
+++ app/models/repository_issue/bitbucket.rb
@@ -2,15 +2,44 @@ module RepositoryIssue
   class Bitbucket < Base
 
     def url
-      # TODO
+      path = issue.pull_request ? 'pull-requests' : 'issues'
+      "#{issue.repository.url}/#{path}/#{issue.number}"
     end
 
     def self.fetch_issue(repo_full_name, issue_number, token = nil)
-      # TODO
+      owner, repo_name = repo_full_name.split('/')
+      api_client.issues.get(owner, repo_name, issue_number).to_hash.with_indifferent_access.merge(type: 'issue')
+    rescue *RepositoryHost::Bitbucket::IGNORABLE_EXCEPTIONS
+      nil
     end
 
     def self.create_from_hash(name_with_owner, issue_hash, token = nil)
-      # TODO
+      issue_hash = issue_hash.to_hash.with_indifferent_access
+      repository = Repository.host('Bitbucket').find_by_full_name(name_with_owner) || RepositoryHost::Bitbucket.create(name_with_owner)
+      uuid = make_uuid(repository.uuid, issue_hash[:type], issue_hash[:id])
+      i = repository.issues.find_or_create_by(uuid: issue_hash[:id])
+      #i.repository_user_id = issue_hash[:reporter][:uuid] # problematic
+      i.repository_id = repository.id
+      i.labels = [issue_hash[:metadata][:kind], issue_hash[:metadata][:component], issue_hash[:metadata][:milestone]].reject(&:empty?)
+      i.pull_request = issue_hash[:type] == 'pull_request'
+      i.comments_count = issue_hash[:comment_count]
+      i.host_type = 'Bitbucket'
+      i.number = issue_hash[:local_id]
+      i.state = issue_hash[:state] == 'opened' ? 'open' : 'closed'
+      i.body = issue_hash[:description]
+      i.locked = false
+      i.created_at = issue_hash[:utc_created_on]
+      i.updated_at = issue_hash[:utc_last_updated]
+      i.assign_attributes issue_hash.slice(:title)
+      if i.changed?
+        i.last_synced_at = Time.now
+        i.save!
+      end
+      i
+    end
+
+    def self.make_uuid(repository_uuid, type, id)
+      "#{repository_uuid}-#{type}-#{id}"
     end
 
     private
