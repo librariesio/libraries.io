80170aadb24c3a4b918ff4a37201a143e1dbcc05
Andrew Nesbitt <andrewnez@gmail.com>
2017-05-25 14:01:59 +0100

    Added specs for search rss feeds

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -10,4 +10,12 @@ describe "SearchController", elasticsearch: true do
       expect(page).to have_content project.name
     end
   end
+
+  describe "GET /search.atom", type: :request do
+    it "renders successfully when logged out" do
+      Project.__elasticsearch__.refresh_index!
+      visit search_path(format: :atom)
+      expect(page).to have_content project.name
+    end
+  end
 end
