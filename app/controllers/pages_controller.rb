841a4b5dff246803953ef12bf7bc6444d81e14aa
Andrew Nesbitt <andrewnez@gmail.com>
2017-06-05 14:21:00 +0100

    Open Data page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,21 @@
 class PagesController < ApplicationController
   def about
-    
+
   end
+
   def team
-    
+
   end
+
   def privacy
-    
+
   end
+
   def compatibility
-    
+
+  end
+
+  def open_data
+
   end
 end
