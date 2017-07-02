fb553e2cea2299f277e8b3163a2943ac311c6e2e
Andrew Nesbitt <andrewnez@gmail.com>
2017-07-02 21:27:10 +0100

    Handle nil keywords_arrays on projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -162,7 +162,7 @@ class Project < ApplicationRecord
   end
 
   def keywords
-    (keywords_array + repository.try(:keywords) || []).uniq(&:downcase)
+    (Array(keywords_array) + Array(repository.try(:keywords) || [])).uniq(&:downcase)
   end
 
   def package_manager_url(version = nil)
