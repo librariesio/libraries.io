7d47947cb07c1d59220823fc834ddafe0a1db5eb
Andrew Nesbitt <andrewnez@gmail.com>
2017-08-17 12:47:19 +0100

    Fix double escaping issues with gitlab api

--- spec/models/repository_host/gitlab_spec.rb
+++ spec/models/repository_host/gitlab_spec.rb
@@ -3,10 +3,4 @@ require 'rails_helper'
 describe RepositoryHost::Gitlab do
   let(:repository) { build(:repository, host_type: 'GitLab') }
   let(:repository_host) { described_class.new(repository) }
-
-  describe '#escaped_full_name' do
-    it 'should escape / in full name' do
-      expect(repository_host.escaped_full_name).to eq('rails%2Frails')
-    end
-  end
 end
