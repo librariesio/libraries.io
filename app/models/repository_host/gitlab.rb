055aad5335a277de7cbc802323b040865c32ceff
Elliott Beach <elliott2.71828@gmail.com>
2017-08-29 09:20:00 -0500

    bumb gitlab API version (#1684)

--- app/models/repository_host/gitlab.rb
+++ app/models/repository_host/gitlab.rb
@@ -162,7 +162,7 @@ module RepositoryHost
     private
 
     def self.api_client(token = nil)
-      ::Gitlab.client(endpoint: 'https://gitlab.com/api/v3', private_token: token || ENV['GITLAB_KEY'])
+      ::Gitlab.client(endpoint: 'https://gitlab.com/api/v4', private_token: token || ENV['GITLAB_KEY'])
     end
 
     def api_client(token = nil)
