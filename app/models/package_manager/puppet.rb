9fbd731c58e8ed8f5d9e28b5741bc5977c5d3fc4
Gareth Rushgrove <gareth@morethanseven.net>
2017-09-12 16:51:54 +0100

    Initial work on supporting Puppet Forge packages (#1691)
    * Initial work on supporting Puppet Forge packages
    
    This is pretty much just the minimal methods implemented, but lots of
    the others are possible based on the capabilities of the API.
    
    * Implemented versions, dependecies and other metadata for Puppet
    
    * Added unit tests for Puppet package manager
    
    * Support dependencies for different module versions

---
+++ app/models/package_manager/puppet.rb
@@ -0,0 +1,76 @@
+module PackageManager
+  class Puppet < Base
+    HAS_VERSIONS = true
+    HAS_DEPENDENCIES = true
+    BIBLIOTHECARY_SUPPORT = false
+    BIBLIOTHECARY_PLANNED = true
+    URL = 'https://forge.puppet.com'
+    COLOR = '#302B6D'
+
+    def self.project_names
+      offset = 0
+      projects = []
+      while true
+        results = get_json("https://forgeapi.puppetlabs.com/v3/modules?limit=100&offset=#{offset}")['results'].map { |result| result['slug'] }
+	break if results == []
+	projects += results
+	offset +=100
+      end
+      projects
+    end
+
+    def self.project(name)
+      get_json("https://forgeapi.puppetlabs.com/v3/modules/#{name}")
+    end
+
+    def self.mapping(project)
+      current_release = project['current_release']
+      metadata = current_release['metadata']
+      {
+        name: project['slug'],
+        repository_url: metadata['source'],
+        description: metadata['description'],
+        keywords_array: current_release['tags'],
+        licenses: metadata['license']
+      }
+    end
+
+    def self.versions(project)
+      project['releases'].map do |release|
+        {
+          number: release['version'],
+          published_at: release['created_at']
+        }
+      end
+    end
+
+    def self.dependencies(name, version, _project)
+      release = get_json("https://forgeapi.puppetlabs.com/v3/releases/#{name}-#{version}")
+      metadata = release['metadata']
+      metadata['dependencies'].map do |dependency|
+        {
+          project_name: dependency['name'].sub('/', '-'),
+          requirements: dependency['version_requirement'],
+          kind: 'runtime',
+          platform: self.name.demodulize
+        }
+      end
+    end
+
+    def self.recent_names
+      get_json("https://forgeapi.puppetlabs.com/v3/modules?limit=100&sort_by=latest_release")['results'].map { |result| result['slug'] }
+    end 
+
+    def self.install_instructions(project, version = nil)
+      "puppet module install #{project.name}" + (version ? " --version #{version}" : "")
+    end
+
+    def self.package_link(project, version = nil)
+      "https://forge.puppet.com/#{project.name.sub('-', '/')}" + (version ? "/#{version}" : "")
+    end
+
+    def self.download_url(name, version = nil)
+      "https://forge.puppet.com/v3/files/#{name}-#{version}.tar.gz"
+    end
+  end
+end
---
+++ spec/models/package_manager/puppet_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe PackageManager::Puppet do
+  let(:project) { create(:project, name: 'foo-bar', platform: described_class.name) }
+
+  it 'has formatted name of "Puppet"' do
+    expect(described_class.formatted_name).to eq('Puppet')
+  end
+
+  describe '#package_link' do
+    it 'returns a link to project website' do
+      expect(described_class.package_link(project)).to eq("https://forge.puppet.com/foo/bar")
+    end
+
+    it 'handles version' do
+      expect(described_class.package_link(project, '2.0.0')).to eq("https://forge.puppet.com/foo/bar/2.0.0")
+    end
+  end
+
+  describe 'download_url' do
+    it 'returns a link to project tarball' do
+      expect(described_class.download_url('foo', '1.0.0')).to eq("https://forge.puppet.com/v3/files/foo-bar-1.0.0.tar.gz")
+    end
+  end
+
+  describe '#install_instructions' do
+    it 'returns a command to install the project' do
+      expect(described_class.install_instructions(project)).to eq("puppet module install foo-bar")
+    end
+
+    it 'handles version' do
+      expect(described_class.install_instructions(project, '2.0.0')).to eq("puppet module install foo-bar --version 2.0.0")
+    end
+  end
+end
