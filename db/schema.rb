82ba318f34c963e667a669c7d65425aca3e37ab6
benjam <ben@libraries.io>
2017-09-27 12:41:36 +0100

    Add optin and notice
    Adds option to user take and a notice if the current user is logged in
    but not opted in.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607094720) do
+ActiveRecord::Schema.define(version: 20170927113217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -392,6 +392,7 @@ ActiveRecord::Schema.define(version: 20170607094720) do
     t.boolean  "currently_syncing", :default=>false, :null=>false
     t.datetime "last_synced_at"
     t.boolean  "emails_enabled",    :default=>true
+    t.boolean  "optin",             :default=>false
   end
 
   create_table "versions", force: :cascade do |t|
