c870e2bafccd934140287a91368e3216f4ca4596
benjam <ben@libraries.io>
2017-11-27 13:55:39 +0000

    *sigh* fix typo

--- spec/requests/project_activities_spec.rb
+++ spec/requests/project_activities_spec.rb
@@ -6,7 +6,7 @@ describe "ProjectActivitiesController" do
   describe "GET renders successfully when logged in", type: :request do
     it "renders successfully when logged in" do
       visit project_usage_path(project.to_param)
-      expect(page).to have_content 'Actvitity for'
+      expect(page).to have_content 'Activity for'
     end
   end
 end
