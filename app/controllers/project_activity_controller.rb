24d154f44321f7fd1cbe3a2b73f779b519fe310f
Andrew Nesbitt <andrewnez@gmail.com>
2017-11-28 13:07:07 +0000

    Hooking up activity type filter

--- app/controllers/project_activity_controller.rb
+++ app/controllers/project_activity_controller.rb
@@ -1,11 +1,17 @@
 class ProjectActivityController < ApplicationController
   before_action :find_project
-  
+
   def show
-    @all_counts = @project.repository_dependencies.where('repositories.fork = ?', false).joins(manifest: :repository).distinct('manifests.repository_id').group('repository_dependencies.requirements').count.select{|k,v| k.present? }
-    @total = @all_counts.sum{|k,v| v }
-    
-    @activities = @project.dependency_activities.includes(:repository).paginate(page: page_number, per_page: 20) 
+    original_scope = @project.dependency_activities
+    scope = original_scope
+    scope = original_scope.where(action: params[:type]) if params[:type].present?
+    @types = original_scope.group(:action).count
+    @total = DependencyActivity.where(project_id: @project.id).distinct.count(:repository_id)
+
+    @activities = scope.includes(:repository)
+                       .order('committed_at DESC')
+                       .paginate(page: page_number, per_page: 20)
+    @chart_data = scope.group(:action).group_by_month(:committed_at).count
   end
 
 end
