09653a199de0620cd4a3607aaaf87fbb1b5e19d6
benjam <ben@libraries.io>
2017-11-28 14:03:04 +0000

    add requirement filter

--- app/controllers/project_activity_controller.rb
+++ app/controllers/project_activity_controller.rb
@@ -5,6 +5,8 @@ class ProjectActivityController < ApplicationController
     original_scope = @project.dependency_activities
     scope = original_scope
     scope = original_scope.where(action: params[:type]) if params[:type].present?
+    scope = original_scope.where(requirement: params[:requirements]) if params[:requirements].present?
+    @all_counts = original_scope.group(:requirement).count.select{|k,v| k.present? }
     @types = original_scope.group(:action).count
     @total = DependencyActivity.where(project_id: @project.id).distinct.count(:repository_id)
 
@@ -14,4 +16,16 @@ class ProjectActivityController < ApplicationController
     @chart_data = scope.group(:action).group_by_month(:committed_at).count
   end
 
+  private
+
+  helper_method :sort_by_semver_range
+  def sort_by_semver_range(limit)
+    @all_counts.sort_by{|_k,v| -v}
+               .first(limit)
+               .sort_by{|k,_v|
+                 k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'')
+                 .gsub('-','.')
+                 .split('.').map{|i| i.to_i}
+               }
+  end
 end
