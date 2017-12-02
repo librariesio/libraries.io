44493d0fd369d61fe6f64bfbef018801753d27dc
aelaa <aelaau@gmail.com>
2017-12-03 00:50:08 +0400

    Add Racket

---
+++ app/models/package_manager/racket.rb
@@ -0,0 +1,38 @@
+module PackageManager
+  class Racket < Base
+    HAS_VERSIONS = false
+    HAS_DEPENDENCIES = false
+    BIBLIOTHECARY_SUPPORT = false
+    URL = 'http://pkgs.racket-lang.org/'
+    COLOR = '#375eab'
+
+    def self.package_link(project, version = nil)
+      "http://pkgs.racket-lang.org/package/#{project.name}"
+    end
+
+    def self.project_names
+      get_raw("http://pkgs.racket-lang.org/pkgs")[2..-3].split('" "')
+    end
+
+    def self.project(name)
+      {
+        name: name,
+        page: get_html("http://pkgs.racket-lang.org/package/#{name}")
+      }
+    end
+
+    def self.mapping(project)
+      {
+        name: project[:name],
+        repository_url: project[:page].at('a:contains("Code")')&.attributes.try(:[], "href")&.text,
+        description: project[:page].css('.jumbotron p')&.first&.children&.first&.text,
+        homepage: homepage_link(project[:page]).present? ? homepage_link(project[:page]).attributes['href'].value : '',
+        keywords_array: project[:page].at('th:contains("Tags")').parent.css('a')&.map{|el| el.children.first.text}
+      }
+    end
+
+    def self.homepage_link(page)
+      page.at('a:contains("Code")') || page.at('th:contains("Documentation")').parent.css('a').first
+    end
+  end
+end
---
+++ spec/models/package_manager/racket_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe PackageManager::Racket do
+  let(:project) { create(:project, name: 'foo', platform: described_class.name) }
+
+  it 'has formatted name of "Racket"' do
+    expect(described_class.formatted_name).to eq('Racket')
+  end
+
+  describe '#package_link' do
+    it 'returns a link to project website' do
+      expect(described_class.package_link(project)).to eq("http://pkgs.racket-lang.org/package/foo")
+    end
+  end
+end
