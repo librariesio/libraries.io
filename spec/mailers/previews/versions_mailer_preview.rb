891efabca8308a817c26a62173e805cb3748a552
Andrew Nesbitt <andrewnez@gmail.com>
2017-12-18 13:43:52 +0000

    Prompt to users who've not updated their terms of service to login
    Logged in users who've not optted in will see a red alert bar with the
    accept button.
    
    Also added a mailer preview for the new version emails

---
+++ spec/mailers/previews/versions_mailer_preview.rb
@@ -0,0 +1,10 @@
+# Preview all emails at http://localhost:3000/rails/mailers/versions_mailer
+class VersionsMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/versions_mailer/new_version
+  def new_version
+    user = User.new({email: 'foo@bar.com', optin: false})
+    version = Version.first
+    VersionsMailer.new_version(user, version.project, version)
+  end
+end
