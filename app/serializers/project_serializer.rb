7b159a40dfa1c4eeac713b37d98be0f21e73287e
Andrew Nesbitt <andrewnez@gmail.com>
2018-01-02 12:56:11 +0000

    Cache project API serialization

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,7 @@
 class ProjectSerializer < ActiveModel::Serializer
+  cache
+  delegate :cache_key, to: :object
+
   attributes :name, :platform, :description, :homepage, :repository_url,
              :normalized_licenses, :rank, :latest_release_published_at,
              :latest_release_number, :language, :status, :package_manager_url,
