6c491141f1f7eff59cf34c9ec1a6b717a039546e
Andrew Nesbitt <andrewnez@gmail.com>
2018-01-02 12:06:05 +0000

    whitespace

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,7 +3,7 @@ require 'typhoeus/adapters/faraday'
 url = ENV['ELASTICSEARCH_CLUSTER_URL'] || 'localhost:9200'
 
 Elasticsearch::Model.client = Elasticsearch::Client.new hosts: url.split(','),
-                                                        retry_on_failure: true, 
+                                                        retry_on_failure: true,
                                                         randomize_hosts: true,
                                                         transport_options: { request: { timeout: 10 } } do |builder|
   builder.use :http_cache, store: Rails.cache, logger: Rails.logger, shared_cache: false, serializer: Marshal
