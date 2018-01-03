4a9d550092c2a213891e6572a576344d43c4b2e4
Andrew Nesbitt <andrewnez@gmail.com>
2018-01-03 08:29:52 +0000

    Fix licensing parsing for cargo

--- app/models/package_manager/cargo.rb
+++ app/models/package_manager/cargo.rb
@@ -46,12 +46,13 @@ module PackageManager
     end
 
     def self.mapping(project)
+      latest_version = project["versions"].to_a.first
       {
         :name => project['crate']['id'],
         :homepage => project['crate']['homepage'],
         :description => project['crate']['description'],
         :keywords_array => Array.wrap(project['crate']['keywords']),
-        :licenses => project['crate']['license'],
+        :licenses => latest_version['license'],
         :repository_url => repo_fallback(project['crate']['repository'], project['crate']['homepage'])
       }
     end
