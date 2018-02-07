7c9c54bcad4a63c70d9106179600e2b7b1172be5
Andrew Nesbitt <andrewnez@gmail.com>
2018-02-07 11:13:35 +0000

    Record last login timestamp on users during when logging in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109154509) do
+ActiveRecord::Schema.define(version: 20180207110908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -292,6 +292,7 @@ ActiveRecord::Schema.define(version: 20171109154509) do
     t.datetime "last_synced_at"
     t.boolean  "emails_enabled",    :default=>true
     t.boolean  "optin",             :default=>false
+    t.datetime "last_login_at"
   end
 
   create_table "versions", force: :cascade do |t|
