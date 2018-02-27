c46c43a2f4dff301d04152dbdc7bcbcb6a82c546
Andrew Nesbitt <andrewnez@gmail.com>
2018-02-27 09:02:30 +0000

    Remove old tidelift notice mailer

--- app/mailers/notice_mailer.rb
+++
@@ -1,8 +0,0 @@
-class NoticeMailer < ApplicationMailer
-  def tidelift(user)
-    @user = user
-    mail to: user.email,
-         subject: 'Libraries.io is joining Tidelift',
-         reply_to: 'support@libraries.io'
-  end
-end
--- lib/tasks/emails.rake
+++
@@ -1,8 +0,0 @@
-namespace :emails do
-  desc 'Notify existing users about joining Tidelift'
-  task tidelift_notice: :environment do
-    User.find_each do |user|
-      NoticeMailer.tidelift(user).deliver_now
-    end
-  end
-end
