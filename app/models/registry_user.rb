a3d943084266f373856d28cfa3a568e83169334b
Andrew Nesbitt <andrewnez@gmail.com>
2018-02-27 09:51:14 +0000

    Remove unused registry user method

--- app/models/registry_user.rb
+++ app/models/registry_user.rb
@@ -1,14 +1,4 @@
 class RegistryUser < ApplicationRecord
   has_many :registry_permissions
   has_many :projects, through: :registry_permissions
-
-  def download_projects
-    return unless platform.downcase == 'rubygems'
-
-    projects = PackageManager::Base.get_json("https://rubygems.org/api/v1/owners/#{login}/gems.json")
-
-    projects.each do |project|
-      PackageManager::Rubygems.update(project['name'])
-    end
-  end
 end
