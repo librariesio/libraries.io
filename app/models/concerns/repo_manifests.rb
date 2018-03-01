7fd9fed7e9978b75c45601b37833a276217a6424
Andrew Nesbitt <andrewnez@gmail.com>
2018-03-01 11:50:53 +0000

    contributing.md and code_of_conduct.md may be stored in doc or .github folders

--- app/models/concerns/repo_manifests.rb
+++ app/models/concerns/repo_manifests.rb
@@ -35,9 +35,9 @@ module RepoManifests
     return if file_list.nil?
     self.has_readme       = file_list.find{|file| file.match(/^README/i) }
     self.has_changelog    = file_list.find{|file| file.match(/^CHANGE|^HISTORY/i) }
-    self.has_contributing = file_list.find{|file| file.match(/^CONTRIBUTING/i) }
+    self.has_contributing = file_list.find{|file| file.match(/^(docs\/)?(.github\/)?CONTRIBUTING/i) }
     self.has_license      = file_list.find{|file| file.match(/^LICENSE|^COPYING|^MIT-LICENSE/i) }
-    self.has_coc          = file_list.find{|file| file.match(/^CODE[-_]OF[-_]CONDUCT/i) }
+    self.has_coc          = file_list.find{|file| file.match(/^(docs\/)?(.github\/)?CODE[-_]OF[-_]CONDUCT/i) }
     self.has_threat_model = file_list.find{|file| file.match(/^THREAT[-_]MODEL/i) }
     self.has_audit        = file_list.find{|file| file.match(/^AUDIT/i) }
     save if self.changed?
