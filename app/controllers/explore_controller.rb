1c7816c7dc018d28a02300944ad3651cbcf3b597
Andrew Nesbitt <andrewnez@gmail.com>
2018-03-02 11:07:59 +0000

    Remove most used from explore

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -10,11 +10,4 @@ class ExploreController < ApplicationController
     @trending_projects = project_scope.recently_created.hacker_news.limit(20).to_a.uniq(&:name).first(10)
     @new_projects = project_scope.order('projects.created_at desc').limit(10)
   end
-
-  private
-
-  helper_method :project_search
-  def project_search(sort)
-    Project.search('', sort: sort, order: 'desc').paginate(per_page: 10, page: 1).results.map{|result| ProjectSearchResult.new(result) }
-  end
 end
