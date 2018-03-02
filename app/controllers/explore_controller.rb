4a3d199b699bcc06f118059762dbe9d7e1d597ca
Andrew Nesbitt <andrewnez@gmail.com>
2018-03-02 11:06:01 +0000

    Load 10 projects per list on explore page

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -6,15 +6,15 @@ class ExploreController < ApplicationController
     @licenses = Project.popular_licenses(:facet_limit => 40).first(10)
 
     project_scope = Project.includes(:repository).maintained.with_description
-  
-    @trending_projects = project_scope.recently_created.hacker_news.limit(10).to_a.uniq(&:name).first(6)
-    @new_projects = project_scope.order('projects.created_at desc').limit(6)
+
+    @trending_projects = project_scope.recently_created.hacker_news.limit(20).to_a.uniq(&:name).first(10)
+    @new_projects = project_scope.order('projects.created_at desc').limit(10)
   end
 
   private
 
   helper_method :project_search
   def project_search(sort)
-    Project.search('', sort: sort, order: 'desc').paginate(per_page: 6, page: 1).results.map{|result| ProjectSearchResult.new(result) }
+    Project.search('', sort: sort, order: 'desc').paginate(per_page: 10, page: 1).results.map{|result| ProjectSearchResult.new(result) }
   end
 end
