1e0e1ffa8dff6fc6bd175a199da4bd96f3d9ceea
Andrew Nesbitt <andrewnez@gmail.com>
2018-03-13 15:47:22 +0000

    Add number of rows

--- app/helpers/open_data_helper.rb
+++ app/helpers/open_data_helper.rb
@@ -30,7 +30,7 @@ module OpenDataHelper
         filename: 'Libraries.io-open-data-1.2.0.tar.gz',
         md5: 'TODO',
         size: 'TODO',
-        rows: 'TODO',
+        rows: '397 million',
         download: 'https://zenodo.org/record/1196312/files/Libraries.io-open-data-1.2.0.tar.gz'
       }
     }
