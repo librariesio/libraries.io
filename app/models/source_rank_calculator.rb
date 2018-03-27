9b0f8de6840cfe8e5b5d708b03aef128be4b81d0
Andrew Nesbitt <andrewnez@gmail.com>
2018-03-27 12:00:57 +0100

    Overall score should be the average of all three category scores
    popularity, community and quality

--- app/models/source_rank_calculator.rb
+++ app/models/source_rank_calculator.rb
@@ -4,6 +4,24 @@ class SourceRankCalculator
   end
 
   def overall_score
+    total_score/3.0
+  end
+
+  def popularity_score
     0
+  end
+
+  def community_score
+    0
+  end
+
+  def quality_score
+    0
+  end
+
+  private
+
+  def total_score
+    popularity_score + community_score + quality_score
   end
 end
