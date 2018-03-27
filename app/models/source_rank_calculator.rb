796c2784eca326fbbaff2e92c273ed4f17301081
Andrew Nesbitt <andrewnez@gmail.com>
2018-03-27 12:24:16 +0100

    Adding comments for each existing score element to relevant categories

--- app/models/source_rank_calculator.rb
+++ app/models/source_rank_calculator.rb
@@ -9,17 +9,44 @@ class SourceRankCalculator
 
   def popularity_score
     0
+    # dependent_projects
+    # dependent_repositories
+    # stars
+    # forks
+    # watchers
   end
 
   def community_score
     0
+    # recent_release
+    # not_brand_new
+    # contributors
+    # code_of_conduct_present
+    # contributing docs present
   end
 
   def quality_score
     0
+    # basic_info_present
+    # source repository_present
+    # readme_present
+    # license_present
+    # multiple_versions_present
+    # follows_semver
+    # one_point_oh
+    # all_prereleases
+    # is_deprecated
+    # is_unmaintained
+    # is_removed
+
+    # dependencies_score
   end
 
   private
+
+  def dependencies_score
+    # any_outdated_dependencies
+  end
 
   def total_score
     popularity_score + community_score + quality_score
