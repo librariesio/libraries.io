c8c76fb1f109d19184618b18137736dcebbad33c
Andrew Nesbitt <andrewnez@gmail.com>
2018-03-27 15:26:23 +0100

    Add basic info score to calculator

--- spec/models/source_rank_calculator_spec.rb
+++ spec/models/source_rank_calculator_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe SourceRankCalculator do
-  let!(:project) { create(:project) }
+  let(:project) { build(:project) }
   let(:calculator) { SourceRankCalculator.new(project) }
 
   describe "#overall_score" do
@@ -11,6 +11,34 @@ describe SourceRankCalculator do
       allow(calculator).to receive(:quality_score) { 30 }
 
       expect(calculator.overall_score).to eq(20)
+    end
+  end
+
+  describe '#basic_info_score' do
+    let(:repository) { create(:repository) }
+    let!(:readme) { create(:readme, repository: repository) }
+
+    context "if all four basic info fields are present" do
+      let!(:project) { build(:project, repository: repository,
+                                       description: 'project description',
+                                       homepage: 'http://homepage.com',
+                                       repository_url: 'https://github.com/foo/bar',
+                                       keywords_array: ['foo', 'bar', 'baz'],
+                                       normalized_licenses: ['MIT']) }
+      it "should be 100" do
+        expect(calculator.basic_info_score).to eq(100)
+      end
+    end
+
+    context "if none of four basic info fields are present" do
+      let!(:project) { build(:project, description: '',
+                                        homepage: '',
+                                        repository_url: '',
+                                        keywords_array: [],
+                                        normalized_licenses: []) }
+      it "should be 0" do
+        expect(calculator.basic_info_score).to eq(0)
+      end
     end
   end
 end
