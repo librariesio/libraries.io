bf7270e8a14dad93cc660666d1eaf35e809de68d
Andrew Nesbitt <andrewnez@gmail.com>
2018-04-04 12:20:39 +0100

    Use new db field name

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -20,7 +20,7 @@ class Dependency < ApplicationRecord
   alias_attribute :deprecated, :is_deprecated?
   alias_method :outdated, :outdated?
 
-  delegate :latest_stable_release_number, :latest_release_number, :is_deprecated?, :source_rank_2_score, to: :project, allow_nil: true
+  delegate :latest_stable_release_number, :latest_release_number, :is_deprecated?, :sourcerank_2, to: :project, allow_nil: true
 
   def filepath
     nil
