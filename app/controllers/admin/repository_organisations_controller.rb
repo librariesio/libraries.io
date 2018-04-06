60cfbb71f7e6fa6dfb9d5316902269569b14993f
Andrew Nesbitt <andrewnez@gmail.com>
2018-04-06 12:31:29 +0100

    Status ordering is broken here

--- app/controllers/admin/repository_organisations_controller.rb
+++ app/controllers/admin/repository_organisations_controller.rb
@@ -1,7 +1,7 @@
 class Admin::RepositoryOrganisationsController < Admin::ApplicationController
   def show
     @org = RepositoryOrganisation.host(current_host).find_by_login(params[:login])
-    @top_repos = @org.repositories.open_source.source.order('status ASC NULLS FIRST, contributions_count DESC NULLS LAST, rank DESC NULLS LAST').limit(10)
+    @top_repos = @org.repositories.open_source.source.order('contributions_count DESC NULLS LAST, rank DESC NULLS LAST').limit(10)
     @total_commits = @org.repositories.map{|r| r.contributions.sum(:count) }.sum
   end
 end
