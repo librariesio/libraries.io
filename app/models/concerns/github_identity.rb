654c562baec9d8c805127edc290a2d71822e411f
Andrew Nesbitt <andrewnez@gmail.com>
2018-05-21 13:40:48 +0100

    Fix specs

--- app/models/concerns/github_identity.rb
+++ app/models/concerns/github_identity.rb
@@ -105,7 +105,7 @@ module GithubIdentity
   end
 
   def github_identity
-    viewable_identities.find{|i| i.provider == 'github' }
+    identities.find{|i| i.provider == 'github' }
   end
 
   def github_public_identity
