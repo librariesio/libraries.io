ddc237ce75c641b6595692deef58e00366b3d0d8
Jeremy Katz <jeremy@tidelift.com>
2018-05-31 15:51:01 -0400

    Don't try to send an email to an empty email address
    That ends up throwing an error

--- app/mailers/versions_mailer.rb
+++ app/mailers/versions_mailer.rb
@@ -5,6 +5,8 @@ class VersionsMailer < ApplicationMailer
     @project = project
     @repos = @project.subscribed_repos(@user)
 
+    return if user.email.empty?
+
     mail to: user.email, subject: "New release of #{@project} (#{version.number}) on #{@project.platform_name}"
   end
 end
