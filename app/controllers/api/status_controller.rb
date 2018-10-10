7173e15ea956b5a06415e21473f9d555681d9233
Tyrel Souza <tyrelsouza@gmail.com>
2018-10-10 16:40:12 -0400

    On the /api/check controller, if we don't find every project, try again forcing lowercase comparison

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -3,11 +3,32 @@ class Api::StatusController < Api::ApplicationController
 
   def check
     if params[:projects].any?
+      # Try to get all the projects passed in.
       @projects = params[:projects].group_by{|project| project[:platform] }.map do |platform, projects|
         projects.each_slice(1000).map do |slice|
           Project.platform(platform).where(name: slice.map{|project| project[:name] }.uniq).includes(:repository, :versions)
         end.flatten.compact
       end.flatten.compact
+
+      # If there are any that don't get found, downcase them and try again
+      if params[:projects].length != @projects.length
+        # "slugify" the platform projects that we found
+        found = @projects.map { |project| slug_project project }
+        # Find any projects that were passed that we didn't find, to try them again but case insensitive
+        missing = params[:projects].select { |project| !found.include?(slug_project project) }
+        lowercase_project_named = missing.group_by{|project| project[:platform] }.map do |platform, projects|
+          projects.each_slice(1000).map do |slice|
+            slice.map do |project|
+              # Get the name from the strong params
+              permitted_name = project.permit(:name).to_h['name']
+              # Filter by platform and lowercase compare the names
+              Project.platform(platform).where('lower(name) = lower(?)', permitted_name).includes(:repository, :versions)
+            end
+          end.flatten.compact
+        end.flatten.compact
+        # Concatanate any new items found.
+        @projects = @projects + lowercase_project_named
+      end
     else
       @projects = []
     end
@@ -25,4 +46,8 @@ class Api::StatusController < Api::ApplicationController
       }
     })
   end
+end
+
+def slug_project project
+  "#{project[:platform].downcase}#{project[:name].downcase}"
 end
--- spec/requests/api/status_spec.rb
+++ spec/requests/api/status_spec.rb
@@ -3,12 +3,29 @@ require "rails_helper"
 describe "API::StatusController" do
   let(:user) { create(:user) }
   let!(:project) { create(:project) }
+  let!(:project_django) {create(:project, name: 'Django', platform: 'Pypi')}
+
   describe "GET /api/check", type: :request do
-    it "renders successfully" do
+    it "renders successfully with one" do
+      post "/api/check", params: {api_key: user.api_key, projects: [{name: 'rails1', platform: 'rubygems'}] }
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(response.body).to be_json_eql []
+    end
+
+    it "renders successfully with one missing" do
       post "/api/check", params: {api_key: user.api_key, projects: [{name: 'rails', platform: 'rubygems'}] }
       expect(response).to have_http_status(:success)
       expect(response.content_type).to eq('application/json')
       expect(response.body).to be_json_eql []
+    end
+
+    it "renders successfully" do
+      post "/api/check", params: {api_key: user.api_key, projects: [{name: 'rails1', platform: 'rubygems'}, {name: 'django', platform: 'Pypi'}] }
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(response.body.include?('Django')).to be == true
+      expect(response.body.include?('rails1')).to be == true
     end
   end
 end
