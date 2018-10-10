aa4c7120272a96f6f19f1e8b092baa9f3f80f432
Jeremy Katz <jeremy@tidelift.com>
2018-10-10 15:52:01 -0400

    Add latest_stable_release fields to projects

---
+++ db/migrate/20181010192042_add_stable_release_to_projects.rb
@@ -0,0 +1,6 @@
+class AddStableReleaseToProjects < ActiveRecord::Migration[5.1]
+  def change
+    add_column :projects, :latest_stable_release_number, :string
+    add_column :projects, :latest_stable_release_published_at, :datetime
+  end
+end
