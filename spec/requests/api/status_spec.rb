b61960e5d7e5d06b6379d1e80b409c1216e1b804
Tyrel Souza <tyrelsouza@gmail.com>
2018-10-11 13:05:17 -0400

    use platform.name and don't hardcode any name-numbers

--- spec/requests/api/status_spec.rb
+++ spec/requests/api/status_spec.rb
@@ -2,15 +2,15 @@ require "rails_helper"
 
 describe "API::StatusController" do
   let(:user) { create(:user) }
-  let!(:project) { create(:project, name: 'rails1') }
+  let!(:project) { create(:project) }
   let!(:project_django) { create(:project, name: 'Django', platform: 'Pypi') }
 
   describe "GET /api/check", type: :request do
     it "renders successfully with one" do
-      post "/api/check", params: {api_key: user.api_key, projects: [{name: 'rails1', platform: 'rubygems'}] }
+      post "/api/check", params: {api_key: user.api_key, projects: [{name: project.name, platform: 'rubygems'}] }
       expect(response).to have_http_status(:success)
       expect(response.content_type).to eq('application/json')
-      expect(response.body.include?('rails1')).to be == true
+      expect(response.body.include?(project.name)).to be == true
     end
 
     it "renders empty json list if cannot find Project" do
@@ -21,10 +21,10 @@ describe "API::StatusController" do
     end
 
     it "renders successfully" do
-      post "/api/check", params: {api_key: user.api_key, projects: [{name: 'rails1', platform: 'rubygems'}, {name: 'django', platform: 'Pypi'}] }
+      post "/api/check", params: {api_key: user.api_key, projects: [{name: project.name, platform: 'rubygems'}, {name: 'django', platform: 'Pypi'}] }
       expect(response).to have_http_status(:success)
       expect(response.content_type).to eq('application/json')
-      expect(response.body.include?('rails1')).to be == true
+      expect(response.body.include?(project.name)).to be == true
       expect(response.body.include?('Django')).to be == true
     end
   end
