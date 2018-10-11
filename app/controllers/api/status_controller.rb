12181f9c88d82a090af5cb1458feb90d7f2065c7
Tyrel Souza <tyrelsouza@gmail.com>
2018-10-11 15:28:48 -0400

    On the /api/check controller, if we don't find every project, try again forcing lowercase comparison (#2232)

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -3,11 +3,31 @@ class Api::StatusController < Api::ApplicationController
 
   def check
     if params[:projects].any?
+      # Try to get all the projects passed in.
       @projects = params[:projects].group_by{|project| project[:platform] }.map do |platform, projects|
         projects.each_slice(1000).map do |slice|
           Project.platform(platform).where(name: slice.map{|project| project[:name] }.uniq).includes(:repository, :versions)
-        end.flatten.compact
+        end
       end.flatten.compact
+
+      # If there are any that don't get found, downcase them and try again
+      if params[:projects].length != @projects.length
+
+        # downcase all platform/name pairs we've found so we can match against them later
+        found = @projects.map { |project| [project[:platform].downcase, project[:name].downcase] }
+        # Find any projects that were passed that we didn't find, to try them again but case insensitive
+        missing = params[:projects].select { |project| !found.include?([project[:platform].downcase, project[:name].downcase]) }
+
+        lowercase_project_named = missing.group_by{|project| project[:platform] }.map do |platform, projects|
+          projects.each_slice(1000).map do |slice|
+            downcased_names = slice.map {|project| project[:name].downcase }
+            # Filter by platform and lowercase compare the downcased_names
+            Project.platform(platform).where('lower(name) in (?)', downcased_names).includes(:repository, :versions)
+          end
+        end.flatten.compact
+        # Concatanate any new items found.
+        @projects.concat(lowercase_project_named)
+      end
     else
       @projects = []
     end
