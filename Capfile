35611d29f9bab57e7ebc4321016de06da2185e8a
Tobias Macey <tmacey@boundlessnotions.com>
2018-10-13 07:30:01 -0400

    Added support for dynamic update of env files for dependent servers
    Added capistrano plugins to handle uploading `.env` and `config/secrets.yml`, as well as updating the `.env` file with IP addresses of hosts pulled dynamically from the GCE API for Elasticsearch, Redis, and Memcached.
    
    Also added a capistrano plugin for managing sidekiq deployment.

--- Capfile
+++ Capfile
@@ -1,12 +1,15 @@
 require 'dotenv'
 Dotenv.load
 require "capistrano/setup"
+require "capistrano/linked_files"
 require "capistrano/deploy"
 require 'capistrano/rails'
+require "capistrano/env"
 require 'capistrano/maintenance'
 require "capistrano/scm/git"
 require 'capistrano/rails/console'
 require 'capistrano/puma'
+require 'capistrano/sidekiq'
 install_plugin Capistrano::SCM::Git
 install_plugin Capistrano::Puma
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,34 +10,54 @@ goog = Fog::Compute::Google.new(config=goog_config)
 
 web = goog.servers.all(filter: 'labels.project = libraries AND labels.environment = production AND labels.type = app')
 
+memcached = goog.servers.all(filter: 'labels.project = libraries AND labels.environment = production AND labels.role = memcached')
+
+redis = goog.servers.all(filter: 'labels.project = libraries AND labels.environment = production AND labels.role = redis')
+
+elastic = goog.servers.all(filter: 'labels.project = libraries AND labels.environment = production AND labels.role = elasticsearch')
+
+set :capenv, ->(env) {
+  Dotenv.load.keys.each {|k| env.add k}
+  env.add 'MEMCACHIER_SERVERS', memcached.map {|mem| mem.network_interfaces[0][:network_ip] + ':11211'}.join(',')
+  env.add 'REDISCLOUD_URL', "redis://#{redis.first.network_interfaces[0][:network_ip]}:6379/0"
+  env.add 'ELASTICSEARCH_CLUSTER_URL', elastic.map {|es| es.network_interfaces[0][:network_ip] + ':9200'}.join(',')
+}
+
 web.each do |s|
   server s.network_interfaces[0][:access_configs][0][:nat_ip], user: 'deploy', roles: [s.labels.fetch(:type, ''), s.labels.fetch(:role, '')]
 end
 
 # server '51.15.130.54',    user: 'root', roles: %w{cron}
 
-namespace :deploy do
-  task :restart do
-    on roles(:app) do |host|
-      execute "sleep #{rand(100)} && systemctl --user restart librariesio"
-    end
-  end
+# namespace :deploy do
+#   task :restart do
+#     on roles(:app) do |host|
+#       within '/var/www/librariesio/current/' do
+#         execute :sleep, rand(10)
+#         execute :bundle, :exec, :pumactl, :restart
+#       end
+#     end
+#   end
 
-  task :restart_web do
-    on roles(:web) do |host|
-      execute "sleep #{rand(100)} && systemctl --user restart librariesio"
-    end
-  end
+#   task :restart_web do
+#     on roles(:web) do |host|
+#       within '/var/www/librariesio/current/' do
+#         execute :sleep, rand(10)
+#         execute :bundle, :exec, :pumactl, :restart
+#       end
+#     end
+#   end
 
-  task :stop do
-    on roles(:worker) do |host|
-      execute "systemctl --user stop librariesio"
-    end
-  end
-  task :start do
-    on roles(:worker) do |host|
-      execute "systemctl --user start librariesio"
-    end
-  end
-  after :publishing, :restart
-end
+#   task :stop do
+#     on roles(:worker) do |host|
+#       execute "systemctl --user stop librariesio"
+#     end
+#   end
+
+#   task :start do
+#     on roles(:worker) do |host|
+#       execute "systemctl --user start librariesio"
+#     end
+#   end
+#   after :publishing, :restart
+# end
