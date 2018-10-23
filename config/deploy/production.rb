5f87bb7f12024bf9e285797740039f83311949ae
Tobias Macey <tmacey@boundlessnotions.com>
2018-10-22 21:28:43 -0400

    Cleaned up some unneeded code and fixed repo_url for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -26,38 +26,3 @@ set :capenv, ->(env) {
 web.each do |s|
   server s.network_interfaces[0][:access_configs][0][:nat_ip], user: 'deploy', roles: [s.labels.fetch(:type, ''), s.labels.fetch(:role, '')]
 end
-
-# server '51.15.130.54',    user: 'root', roles: %w{cron}
-
-# namespace :deploy do
-#   task :restart do
-#     on roles(:app) do |host|
-#       within '/var/www/librariesio/current/' do
-#         execute :sleep, rand(10)
-#         execute :bundle, :exec, :pumactl, :restart
-#       end
-#     end
-#   end
-
-#   task :restart_web do
-#     on roles(:web) do |host|
-#       within '/var/www/librariesio/current/' do
-#         execute :sleep, rand(10)
-#         execute :bundle, :exec, :pumactl, :restart
-#       end
-#     end
-#   end
-
-#   task :stop do
-#     on roles(:worker) do |host|
-#       execute "systemctl --user stop librariesio"
-#     end
-#   end
-
-#   task :start do
-#     on roles(:worker) do |host|
-#       execute "systemctl --user start librariesio"
-#     end
-#   end
-#   after :publishing, :restart
-# end
