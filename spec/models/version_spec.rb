e3b45ead347cda841a94bf1799cc35b77d4e5c2e
Jeremy Katz <jeremy@tidelift.com>
2018-10-23 10:45:14 -0400

    Allow runtime dependencies to include 'normal'
    Some package managers (notably cargo) use 'normal' for 'runtime'
    deps so put that in the allowed kinds for runtime

--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Version, type: :model do
   it { should belong_to(:project) }
   it { should have_many(:dependencies) }
-  it { should have_many(:runtime_dependencies).conditions(kind: 'runtime') }
+  it { should have_many(:runtime_dependencies).conditions(kind: ['runtime', 'normal']) }
 
   it { should validate_presence_of(:project_id) }
   it { should validate_presence_of(:number) }
