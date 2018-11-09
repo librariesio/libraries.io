f96bb1bf55348a8fe44f9bcec4f4f0ba3c6b4cd0
Jeremy Katz <jeremy@tidelift.com>
2018-11-08 20:52:53 -0500

    Add an index on platform, dependents_count for the projects table

---
+++ db/migrate/20181109014916_add_platform_dependents_count_index.rb
@@ -0,0 +1,7 @@
+class AddPlatformDependentsCountIndex < ActiveRecord::Migration[5.1]
+  disable_ddl_transaction!
+
+  def change
+    add_index :projects, [:platform, :dependents_count], algorithm: :concurrently
+  end
+end
