3735faeef1b2208d65a3e340b5b086ebb343faa4
Jeremy Katz <jeremy@katzbox.net>
2018-11-13 18:20:54 -0500

    Send fewer samples to cloud trace for now

--- config/initializers/stackdriver.rb
+++ config/initializers/stackdriver.rb
@@ -4,7 +4,7 @@ unless ENV["GCLOUD_PROJECT_ID"].nil?
     sampler = Google::Cloud::Trace::TimeSampler.default
     # there doesn't seem to be a good way to override the configuration for trace as initialize
     # is private and you can't override the defaults?
-    sampler.send(:initialize, path_blacklist: ["/_ah/health", "/healthz", "/healthcheck"].freeze)
+    sampler.send(:initialize, path_blacklist: ["/_ah/health", "/healthz", "/healthcheck"].freeze, qps: 0.01)
     config.sampler = sampler
   end
 end
