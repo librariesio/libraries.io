ae3df3991313fbfc6d76b61e9be92ae235d21e62
Jeremy Katz <jeremy@tidelift.com>
2018-12-06 14:53:38 -0500

    Disable client command for google cloud memorystore redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,10 @@
 require 'resolv-replace' # pure ruby DNS
+
+# disable id so that sidekiq will work with google cloud memorystore redis
+# https://github.com/mperham/sidekiq/issues/3518#issuecomment-390896088
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDISCLOUD_URL"], id: nil }
+end
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDISCLOUD_URL"], id: nil }
+end
