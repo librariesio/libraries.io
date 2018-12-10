0d03298994c8884715196f87236a9b870e9e596c
Jeremy Katz <jeremy@tidelift.com>
2018-12-10 13:01:42 -0500

    Use cloud trace faraday middleware for es calls in production

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -8,6 +8,7 @@ Elasticsearch::Model.client = Elasticsearch::Client.new hosts: url.split(','),
                                                         transport_options: { request: { timeout: 10 } } do |builder|
   builder.use :http_cache, store: Rails.cache, logger: Rails.logger, shared_cache: false, serializer: Marshal
   builder.use FaradayMiddleware::Gzip
+  builder.use Google::Cloud::Trace::FaradayMiddleware if Rails.env.production?
   builder.use :instrumentation
   builder.request :retry
   builder.adapter :typhoeus
