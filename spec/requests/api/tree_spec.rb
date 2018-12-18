a7c31b20d74b2d8bac92f89aea97fed02c880683
Matt Ellis <mellis@tidelift.com>
2018-12-18 14:17:16 -0500

    Fix extra lines

--- spec/requests/api/tree_spec.rb
+++ spec/requests/api/tree_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+
 describe "Api::TreesController" do
   let(:version) { create(:version) }
   let(:user) { create(:user) }
@@ -8,7 +9,6 @@ describe "Api::TreesController" do
       get "/api/#{version.project.platform}/#{version.project.name}/tree?api_key=#{user.api_key}"
       expect(response).to have_http_status(:success)
       expect(response.content_type).to eq('application/json')
-
       expect(response.body).to be_json_eql TreeResolver.new(version, 'runtime', Date.today).tree.to_json
     end
   end
