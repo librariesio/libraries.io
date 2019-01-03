34e1dadc1c0a39e675ad109786186c914cf0b848
Jeremy Katz <jeremy@katzbox.net>
2019-01-02 21:14:43 -0500

    Optimize how we get the watched dependent projects
    User.watched_dependent_projects is used on the homepage for User.all_subscribed_versions
    and ends up being a pretty slow query
    
    (SELECT "projects"."*" FROM "projects" INNER JOIN "repository_dependencies" ON "projects"."id" = "repository_dependencies"."project_id" INNER JOIN "manifests" ON "repository_dependencies"."manifest_id" = "manifests"."id" INNER JOIN "repositories" ON "manifests"."repository_id" = "repositories"."id" INNER JOIN "repository_subscriptions" ON "repositories"."id" = "repository_subscriptions"."repository_id" WHERE "repository_subscriptions"."user_id" = ? GROUP BY projects.id)
    
    We can speed this up by making explicit queries that just return ids as necessary
    and then querying by id (primary key) rather than ending up doing large table scans

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,8 +12,6 @@ describe User, type: :model do
   it { should have_many(:adminable_repository_organisations) }
   it { should have_many(:source_repositories) }
   it { should have_many(:watched_repositories) }
-  it { should have_many(:watched_dependencies) }
-  it { should have_many(:watched_dependent_projects) }
   it { should have_many(:dependencies) }
   it { should have_many(:really_all_dependencies) }
   it { should have_many(:all_dependent_projects) }
