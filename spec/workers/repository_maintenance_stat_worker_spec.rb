6d72dc005a8327679eb92ed36f57ffb0f088053a
mikeyoung85 <mike.young@tidelift.com>
2019-01-07 16:46:54 -0500

    added more tests for queue priority

--- spec/workers/repository_maintenance_stat_worker_spec.rb
+++ spec/workers/repository_maintenance_stat_worker_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe RepositoryMaintenanceStatWorker do
   let!(:repository) { create(:repository) }
 
+  before do
+    Sidekiq::Worker.clear_all
+  end
+
   it "should use the medium priority queue by default" do
     is_expected.to be_processed_in :repo_maintenance_stat
   end
@@ -14,5 +18,31 @@ describe RepositoryMaintenanceStatWorker do
   it "should gather stats for the repository" do
     expect(GatherRepositoryMaintenanceStats).to receive(:gather_stats).with(repository)
     subject.perform(repository.id)
+  end
+
+  it "should queue jobs in high priority" do
+    # need to disable the queue locking for tests to work with Sidekiq mocking
+    SidekiqUniqueJobs.use_config(enabled: false) do
+      expect(Sidekiq::Queues["repo_maintenance_stat_high"].size).to eql 0
+      expect(RepositoryMaintenanceStatWorker.jobs.size).to eql 0
+
+      RepositoryMaintenanceStatWorker.queue(repository.id, priority: :high)
+    
+      expect(RepositoryMaintenanceStatWorker.jobs.size).to eql 1
+      expect(Sidekiq::Queues["repo_maintenance_stat_high"].size).to eql 1
+    end
+  end
+
+  it "should queue jobs in low priority" do
+    # need to disable the queue locking for tests to work with Sidekiq mocking
+    SidekiqUniqueJobs.use_config(enabled: false) do
+      expect(Sidekiq::Queues["repo_maintenance_stat_low"].size).to eql 0
+      expect(RepositoryMaintenanceStatWorker.jobs.size).to eql 0
+
+      RepositoryMaintenanceStatWorker.queue(repository.id, priority: :low)
+    
+      expect(RepositoryMaintenanceStatWorker.jobs.size).to eql 1
+      expect(Sidekiq::Queues["repo_maintenance_stat_low"].size).to eql 1
+    end
   end
 end
