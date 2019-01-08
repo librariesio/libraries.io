841bdd6e32c8b4d027d9f883620394ee96737b4e
mikeyoung85 <mike.young@tidelift.com>
2019-01-08 15:22:28 -0500

    added comment around sidekiq settings and expected behavior and changed queue to enqueue

--- app/workers/repository_maintenance_stat_worker.rb
+++ app/workers/repository_maintenance_stat_worker.rb
@@ -1,12 +1,19 @@
 class RepositoryMaintenanceStatWorker
     include Sidekiq::Worker
+    ##
+    # These settings will create a lock using a combination of the worker class and arguments passed.
+    # This will prevent a second request for the same repository to be added to the Sidekiq queue and
+    # will keep the existing request. There are three queues in use which are set with different priority levels.
+    # This lock will exist across the different priority queues to prevent
+    # duplication at different priorities for the same repository. By default this worker will use the
+    # medium priority queue. If a request is rejected because of an existing lock, then a message will be sent to the logs.
     sidekiq_options queue: :repo_maintenance_stat, lock: :until_and_while_executing, unique_across_queues: true, retry: 3, on_conflict: :log
 
     def perform(repo_id)
         GatherRepositoryMaintenanceStats.gather_stats(Repository.find_by_id(repo_id))
     end
 
-    def self.queue(repo_id, priority: :medium)
+    def self.enqueue(repo_id, priority: :medium)
         queue_name = "repo_maintenance_stat"
         case priority
         when :low
