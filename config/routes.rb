53b9211c22739eaaf6902af720f64e085a9c7f0b
mikeyoung85 <mike.young@tidelift.com>
2019-01-08 13:41:32 -0500

    changed how the locking works to avoid locks not being removed and added the unique gem to the web routes to allow management of unique hashes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
+  require 'sidekiq_unique_jobs/web'
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
   end if Rails.env.production?
