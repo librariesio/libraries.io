bc5658bc125234bbdf7b0741a02b3d3438b70ebf
Matt Ellis <mellis@tidelift.com>
2019-01-29 10:35:45 -0500

    Add after_commit trigger to enqueue a maintenance stats update when a high priority item is requested without maintenance stats already

--- app/models/project_update_priority.rb
+++ app/models/project_update_priority.rb
@@ -2,6 +2,8 @@ class ProjectUpdatePriority < ApplicationRecord
   belongs_to :project
   enum priority: { low: 0, medium: 1, high: 2 }
 
+  after_commit :enqueue_high_priority
+
   def self.low_priority_date
     1.month.ago
   end
@@ -12,5 +14,10 @@ class ProjectUpdatePriority < ApplicationRecord
 
   def self.high_priority_date
     1.week.ago
+  end
+
+  def enqueue_high_priority
+    return unless high?
+    project.update_maintenance_stats_async(priority: :high) if project.repository_maintenance_stats.length == 0
   end
 end
