1c614cd0a7cacf214a153e1826f25f77544777c3
Matt Ellis <mellis@tidelift.com>
2019-02-13 11:29:48 -0500

    Mellisio/cleanup/error handling improvements to decrease log noise (#2308)

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,33 @@
 class ErrorsController < ApplicationController
   def not_found
-    render status: 404
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.atom { render xml: "not found", root: "error", status: :not_found }
+      format.json { render json: { error: "not found" }, status: :not_found }
+    end
   end
 
   def unprocessable
-    render status: 422
+    respond_to do |format|
+      format.html { render status: :unprocessable_entity }
+      format.atom { render xml: "unprocessable", root: "error", status: :unprocessable_entity }
+      format.json { render json: { error: "unprocessable" }, status: :unprocessable_entity }
+    end
+  end
+
+  def not_acceptable
+    respond_to do |format|
+      format.html { render status: :not_acceptable }
+      format.atom { render xml: "not_acceptable", root: "error", status: :not_acceptable }
+      format.json { render json: { error: "not_acceptable" }, status: :not_acceptable }
+    end
   end
 
   def internal
-    render status: 500
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.atom { render xml: "internal server error", root: "error", status: :internal_server_error }
+      format.json { render json: { error: "internal server error" }, status: :internal_server_error }
+    end
   end
 end
