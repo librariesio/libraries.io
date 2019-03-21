1dccccdd1230b5b7acc68882d8d8219bc211783f
mikeyoung85 <mike.young@tidelift.com>
2019-03-21 17:03:40 -0400

    remove unique digest configuration on stats queue

--- app/workers/repository_maintenance_stat_worker.rb
+++ app/workers/repository_maintenance_stat_worker.rb
@@ -1,13 +1,6 @@
 class RepositoryMaintenanceStatWorker
     include Sidekiq::Worker
-    ##
-    # These settings will create a lock using a combination of the worker class and arguments passed.
-    # This will prevent a second request for the same repository to be added to the Sidekiq queue and
-    # will keep the existing request. There are three queues in use which are set with different priority levels.
-    # This lock will exist across the different priority queues to prevent
-    # duplication at different priorities for the same repository. By default this worker will use the
-    # medium priority queue. If a request is rejected because of an existing lock, then a message will be sent to the logs.
-    sidekiq_options queue: :repo_maintenance_stat, lock: :until_and_while_executing, unique_across_queues: true, retry: 3, on_conflict: :log
+    sidekiq_options queue: :repo_maintenance_stat, retry: 3
 
     def perform(repo_id)
         GatherRepositoryMaintenanceStats.gather_stats(Repository.find_by_id(repo_id))
