aa544fc26272b1ea32a981cd73f309632afb8e71
Keith Barrette <etterrab@gmail.com>
2019-03-27 13:20:59 -0400

    Define ProjectSearch#marshal_load to match marshal_dump

---
+++ spec/models/concerns/project_search_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe ProjectSearch do
+  subject { build(:project) }
+
+  it "Marshals properly" do
+    original = subject.to_json
+    result = Marshal.load(Marshal.dump(subject)).to_json
+
+    expect(result).to eq(original)
+  end
+end
