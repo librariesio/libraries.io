681c0b756ddd875ae2e7b20da115c37ce90531a6
mikeyoung85 <mike.young@tidelift.com>
2019-06-11 17:19:17 -0400

    change status_controller to use smaller methods and package manager find_names instead of running different queries for name and downcased name

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -5,32 +5,26 @@ class Api::StatusController < Api::ApplicationController
     if params[:projects].any?
       # Try to get all the projects passed in.
       @projects = params[:projects].group_by{|project| project[:platform] }.map do |platform, projects|
-        projects.each_slice(1000).map do |slice|
-          Project.platform(platform).where(name: slice.map{|project| project[:name] }.uniq).includes(:repository, :versions, :repository_maintenance_stats)
-        end
+        projects.each_slice(1000).map{|slice| find_projects(slice, platform)}
       end.flatten.compact
-
-      # If there are any that don't get found, downcase them and try again
-      if params[:projects].length != @projects.length
-
-        # downcase all platform/name pairs we've found so we can match against them later
-        found = @projects.map { |project| [project[:platform]&.downcase, project[:name].downcase] }
-        # Find any projects that were passed that we didn't find, to try them again but case insensitive
-        missing = params[:projects].select { |project| !found.include?([project[:platform]&.downcase, project[:name].downcase]) }
-
-        lowercase_project_named = missing.group_by{|project| project[:platform] }.map do |platform, projects|
-          projects.each_slice(1000).map do |slice|
-            downcased_names = slice.map {|project| project[:name].downcase }
-            # lowercase compare the downcased_names... use lower(platform) to improve index usage
-            Project.where('lower(platform)=? AND lower(name) in (?)', platform.downcase, downcased_names).includes(:repository, :versions, :repository_maintenance_stats)
-          end
-        end.flatten.compact
-        # Concatanate any new items found.
-        @projects.concat(lowercase_project_named)
-      end
     else
       @projects = []
     end
     render json: @projects, each_serializer: ProjectStatusSerializer, show_score: params[:score], show_stats: internal_api_key?
+  end
+
+  private
+
+  def find_projects(projects, platform)
+    project_find_names = projects.map{|project| project_names(project, platform)}
+    Project.platform(platform).where(name: project_find_names).includes(:repository, :versions, :repository_maintenance_stats)
+  end
+
+  def project_names(project, platform)
+    begin
+      "PackageManager::#{platform.capitalize}".constantize.project_find_names(project[:name])
+    rescue => exception
+      PackageManager::Base.project_find_names(project[:name])
+    end
   end
 end
