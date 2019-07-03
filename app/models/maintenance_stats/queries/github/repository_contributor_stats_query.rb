c5e41cafcde01903109b34bb2f35075907d030a7
mikeyoung85 <mike.young@tidelift.com>
2019-07-03 09:53:56 -0400

    update query modules and fix indentation

--- app/models/maintenance_stats/queries/github/repository_contributor_stats_query.rb
+++ app/models/maintenance_stats/queries/github/repository_contributor_stats_query.rb
@@ -1,22 +1,24 @@
 module MaintenanceStats
   module Queries
-    class RepositoryContributorStatsQuery < BaseQuery
-      @@valid_params = [:full_name]
-      @@required_params = [:full_name]
-      @@retry_amount = 4
-      def self.client_type
-        :v3
-      end
-      def query(params: {}, count: 0)
-        raise Octokit::Error.new "Didn't get a response after #{@@retry_amount} attempts" if params[:count].present? && params[:count] >= @@retry_amount
-        validate_params(params)
-
-        resp = @client.contributor_stats(params[:full_name])
-        if @client.last_response.status == 202
-          sleep 1
-          return query(params: params, count: count+=1)
+    module Github
+      class RepositoryContributorStatsQuery < BaseQuery
+        @@valid_params = [:full_name]
+        @@required_params = [:full_name]
+        @@retry_amount = 4
+        def self.client_type
+          :v3
         end
-        resp
+        def query(params: {}, count: 0)
+          raise Octokit::Error.new "Didn't get a response after #{@@retry_amount} attempts" if params[:count].present? && params[:count] >= @@retry_amount
+          validate_params(params)
+
+          resp = @client.contributor_stats(params[:full_name])
+          if @client.last_response.status == 202
+            sleep 1
+            return query(params: params, count: count+=1)
+          end
+          resp
+        end
       end
     end
   end
