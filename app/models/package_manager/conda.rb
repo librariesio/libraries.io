ec04e217d2b6821057545e1da3e50dcdc60abfc0
Tyrel Souza <923113+tyrelsouza@users.noreply.github.com>
2019-09-16 13:46:10 -0400

    get data from conda-parser

--- app/models/package_manager/conda.rb
+++ app/models/package_manager/conda.rb
@@ -8,9 +8,10 @@ module PackageManager
 
     def self.project(name)
       chanel, name = urlize_channel_name(name)
-      versions = get_json("http://conda-parser.libraries.io/info/#{channel}/#{name}")
-      latest_version = versions.keys.sort_by{|version| version.split('.').map{|v| v.to_i}}.last
-      versions[latest_version].merge('versions' => versions)
+      latest_version = URI.escape(get_json("http://conda.libraries.io/packages/#{channel}/#{name}"))
+      latest_version_data = get_json("http://conda-parser.libraries.io/info/#{channel}/#{name}/#{latest_version}")
+
+      latest_version_data
     end
 
     def self.mapping(project)
