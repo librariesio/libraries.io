5cff469a2ccfdeaa66fdbb1117f8d21e96243514
mikeyoung85 <mike.young@tidelift.com>
2019-12-30 17:28:59 -0500

    create endpoint to begin watching projects in bulk

---
+++ app/controllers/api/bulk_project_controller.rb
@@ -0,0 +1,21 @@
+class Api::BulkProjectController < Api::ApplicationController
+  def project_status_queries
+    @project_status_queries ||= params[:projects]
+      .group_by { |project| project[:platform] }
+      .map { |platform, group| ProjectStatusQuery.new(platform, group.map { |p| p[:name] }) }
+  end
+
+  def projects
+    project_status_queries
+      .map(&:projects_by_name)
+      .flat_map(&:values)
+  end
+
+  def project_names
+    project_status_queries.each_with_object({}) do |psq, result|
+      psq.projects_by_name.each do |requested_name, project|
+        result[[project.platform, project.name]] = requested_name
+      end
+    end
+  end
+end
---
+++ spec/requests/api/maintenance_stat_spec.rb
@@ -0,0 +1,69 @@
+require "rails_helper"
+
+describe "API::MaintenanceStatsController" do
+  let(:internal_user) { create(:user) }
+  let(:normal_user) { create(:user) }
+  let!(:repository) { create(:repository) }
+  let!(:repository_django) { create(:repository, full_name: 'django/django') }
+  let!(:project) { create(:project, repository: repository) }
+  let!(:project_django) { create(:project, name: 'Django', platform: 'Pypi', repository: repository_django) }
+
+  before do
+    internal_user.current_api_key.update_attribute(:is_internal, true)
+  end
+
+  describe "POST /api/maintenance/stats/begin/bulk", type: :request do
+    it "begins watching projects" do
+      expect(RepositoryMaintenanceStatWorker).to receive(:enqueue).with(repository_django.id, priority: :high).exactly(1).times
+      expect(RepositoryMaintenanceStatWorker).to receive(:enqueue).with(repository.id, priority: :high).exactly(1).times
+
+      post(
+        "/api/maintenance/stats/begin/bulk",
+        params: {
+          api_key: internal_user.api_key,
+          projects: [
+            { name: project_django.name.downcase, platform: project_django.platform },
+            { name: project.name.downcase, platform: project.platform }
+          ]
+        }
+      )
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it "skips projects with stats" do
+      create(:repository_maintenance_stat, repository: repository)
+      expect(RepositoryMaintenanceStatWorker).to receive(:enqueue).with(repository_django.id, priority: :high).exactly(1).times
+      expect(RepositoryMaintenanceStatWorker).to receive(:enqueue).with(repository.id, priority: :high).exactly(0).times
+
+      post(
+        "/api/maintenance/stats/begin/bulk",
+        params: {
+          api_key: internal_user.api_key,
+          projects: [
+            { name: project_django.name.downcase, platform: project_django.platform },
+            { name: project.name.downcase, platform: project.platform }
+          ]
+        }
+      )
+
+      expect(response).to have_http_status(:success)
+    end
+
+    context "with normal API key" do
+      it "returns no maintenance stats" do
+        post(
+          "/api/maintenance/stats/begin/bulk",
+          params: {
+            api_key: normal_user.api_key,
+            projects: [
+              { name: project_django.name.downcase, platform: project_django.platform },
+              { name: project.name.downcase, platform: project.platform }
+            ]
+          }
+        )
+        expect(response).to have_http_status(403)
+      end
+    end
+  end
+end
