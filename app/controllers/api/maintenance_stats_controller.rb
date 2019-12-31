0fc0e5ea8b5077f500d5daf67de771e64dd650df
Mike Young <mike.young@tidelift.com>
2019-12-31 11:33:50 -0500

    Create Endpoint to Begin Watching Projects Maintenance Stats in Bulk (#2471)
    * create endpoint to begin watching projects in bulk
    
    * use each instead of map

--- app/controllers/api/maintenance_stats_controller.rb
+++ app/controllers/api/maintenance_stats_controller.rb
@@ -1,5 +1,6 @@
-class Api::MaintenanceStatsController < Api::ApplicationController
-  before_action :require_internal_api_key, :find_project
+class Api::MaintenanceStatsController < Api::BulkProjectController
+  before_action :require_internal_api_key
+  before_action :find_project, except: [:begin_watching_bulk]
 
   def enqueue
     @project.update_maintenance_stats_async(priority: :high)
@@ -7,7 +8,16 @@ class Api::MaintenanceStatsController < Api::ApplicationController
   end
 
   def begin_watching
-    @project.update_maintenance_stats_async(priority: :high) unless @project.repository_maintenance_stats.exists?
+    begin_project_watch(@project)
     head :accepted
+  end
+
+  def begin_watching_bulk
+    projects.each(&method(:begin_project_watch))
+    head :accepted
+  end
+
+  def begin_project_watch(project)
+    project.update_maintenance_stats_async(priority: :high) unless project.repository_maintenance_stats.exists?
   end
 end
