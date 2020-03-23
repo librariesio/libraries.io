7469d186eefe6143a82d6f3778c37cb8020618ad
Matt Ellis <mellis@tidelift.com>
2020-03-23 16:30:09 -0400

    Add original license to version serialization

--- app/serializers/version_serializer.rb
+++ app/serializers/version_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class VersionSerializer < ActiveModel::Serializer
-  attributes :number, :published_at, :spdx_expression
+  attributes :number, :published_at, :spdx_expression, :original_license
 
   belongs_to :project
 end
