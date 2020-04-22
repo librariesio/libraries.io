55853eaba78b788f80946896425efa7be425227e
Matt Ellis <mellis@tidelift.com>
2020-04-22 11:24:48 -0400

    Update spec

--- spec/serializers/version_serializer_spec.rb
+++ spec/serializers/version_serializer_spec.rb
@@ -6,6 +6,6 @@ describe VersionSerializer do
   subject { described_class.new(build(:version)) }
 
   it "should have expected attribute names" do
-    expect(subject.attributes.keys).to eql(%i[number published_at spdx_expression original_license])
+    expect(subject.attributes.keys).to eql(%i[number published_at spdx_expression original_license researched_at])
   end
 end
