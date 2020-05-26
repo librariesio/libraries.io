5c78c3e99532659eaf065a768b227998d4d355cd
Matt Ellis <mellis@tidelift.com>
2020-05-26 11:19:22 -0400

    Add a couple suggested indices

---
+++ db/migrate/20200526151754_add_index_on_issues_and_repository_users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddIndexOnIssuesAndRepositoryUsers < ActiveRecord::Migration[5.2]
+  def change
+    commit_db_transaction
+    add_index :issues, [:last_synced_at], algorithm: :concurrently
+    add_index :repository_users, %i[hidden last_synced_at], algorithm: :concurrently
+  end
+end
