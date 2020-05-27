3ed6c5399bc63b98a5d568d6a31e09d32e2575c5
Matt Ellis <mellis@tidelift.com>
2020-05-27 13:14:30 -0400

    add ddl thing

--- db/migrate/20200526151754_add_index_on_issues_and_repository_users.rb
+++ db/migrate/20200526151754_add_index_on_issues_and_repository_users.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AddIndexOnIssuesAndRepositoryUsers < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
     commit_db_transaction
     add_index :issues, [:last_synced_at], algorithm: :concurrently
