081b1a5d78cfe13b64cb9af6389119a8c6aff2ac
Tieg Zaharia <tieg@tidelift.com>
2020-06-05 19:29:17 -0400

    Only use parent module page as a backup for versions if they aren't on pkg page.

--- app/models/package_manager/go.rb
+++ app/models/package_manager/go.rb
@@ -35,7 +35,7 @@ module PackageManager
     end
 
     def self.project_names
-      # Currently the index only shows the last <=2000 modules from the date given. (https://proxy.golang.org/)
+      # Currently the index only shows the last <=2000 package version releases from the date given. (https://proxy.golang.org/)
       project_window = 1.day.ago.strftime("%FT%TZ")
       get_raw("https://index.golang.org/index?since=#{project_window}&limit=2000")
         .lines
@@ -43,24 +43,20 @@ module PackageManager
     end
 
     def self.project(name)
-      if pkg_html = get_html("https://pkg.go.dev/#{name}?tab=doc")
-        go_module, _latest_version = pkg_html.css('a[data-test-id="DetailsHeader-infoLabelModule"]').first&.text&.split('@', 2)
-        go_module_html = get_html("https://pkg.go.dev/mod/#{go_module}")
+      if doc_html = get_html("https://pkg.go.dev/#{name}?tab=doc")
+        overview_html = get_html("https://pkg.go.dev/#{name}?tab=overview")
+
         # NB fetching versions from the html only gets dates without timestamps, but we could alternatively use the go proxy too:
-        #   1) Fetch the list of versions: http://proxy.golang.org/#{go_module}/@v/list
-        #   2) And for each version, fetch http://proxy.golang.org/#{go_module}/@v/#{v}.info
-        versions = if (versions_html = get_html("https://pkg.go.dev/mod/#{go_module}?tab=versions"))
-          versions_html.css('.Versions-item').map do |v|
-            {
-              number: v.css('a').first.text,
-              published_at: Chronic.parse(v.css('.Versions-commitTime').first.text)
-            }
-          end
-        else
-          nil
+        #   1) Fetch the list of versions: http://proxy.golang.org/#{module_name}/@v/list
+        #   2) And for each version, fetch http://proxy.golang.org/#{module_name}/@v/#{v}.info
+        versions_html = get_html("https://pkg.go.dev/#{name}?tab=versions")
+
+        # Some package pages don't have a Versions tab, but the parent module page may have the Versions tab (e.g. golang.org/x/tools)
+        if versions_html&.css('.Versions-item').size.zero? && (mod_path = doc_html.css('a[data-test-id="DetailsHeader-infoLabelModule"]').first&.attr('href'))
+          versions_html = get_html("https://pkg.go.dev/#{mod_path}?tab=versions")
         end
 
-        { name: name, go_module: go_module, html: pkg_html, go_module_html: go_module_html, versions: versions }
+        { name: name, html: doc_html, overview_html: overview_html, versions_html: versions_html }
       else
         { name: name }
       end
@@ -71,7 +67,9 @@ module PackageManager
 
     def self.versions(project, name)
       return [] if project.nil?
-      return project[:versions]
+      project[:versions_html]&.css('.Versions-item')&.map do |v|
+        { number: v.css('a').first.text, published_at: Chronic.parse(v.css('.Versions-commitTime').first.text) }
+      end
     end
 
     def self.mapping(project)
@@ -79,10 +77,12 @@ module PackageManager
         {
           name: project[:name],
           description: project[:html].css('.Documentation-overview p').map(&:text).join("\n").strip,
-          licenses: project[:go_module_html].css('*[data-test-id="DetailsHeader-infoLabelLicense"] a').map(&:text),
-          repository_url: project[:go_module_html].css('.Overview-sourceCodeLink a').first&.text,
-          homepage: project[:go_module_html].css('.Overview-sourceCodeLink a').first&.text,
-          versions: project[:versions]
+          licenses: project[:html].css('*[data-test-id="DetailsHeader-infoLabelLicense"] a').map(&:text).join(","),
+          repository_url: project[:overview_html]&.css('.Overview-sourceCodeLink a')&.first&.text,
+          homepage: project[:overview_html]&.css('.Overview-sourceCodeLink a')&.first&.text,
+          versions: project[:versions_html]&.css('.Versions-item')&.map do |v|
+            { number: v.css('a').first.text, published_at: Chronic.parse(v.css('.Versions-commitTime').first.text) }
+          end
         }
       else
         { name: project[:name] }
