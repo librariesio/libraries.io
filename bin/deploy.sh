c8fec2610351cbc7aa4abbf5ad10682fd4a07ab8
Phil Davis <phil@jankaritech.com>
2020-07-31 08:29:12 +0545

    Fix operator-command precedence in deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 REVISION=$(git show-ref origin/master |cut -f 1 -d ' ')
 TAGGED_IMAGE=gcr.io/${GOOGLE_PROJECT}/libraries.io:${REVISION}
-gcloud --quiet container images describe ${TAGGED_IMAGE} || status=$?; echo "Container not finished building" >&2; exit $status
+gcloud --quiet container images describe ${TAGGED_IMAGE} || { status=$?; echo "Container not finished building" >&2; exit $status; }
 
 gcloud --quiet container images add-tag ${TAGGED_IMAGE} gcr.io/${GOOGLE_PROJECT}/libraries.io:latest
 
