5579088c3d76f749880faa5f8f5b5f8de3a2268c
Tieg Zaharia <tieg@tidelift.com>
2020-09-23 12:08:17 -0400

    Disable all code related to repository_dependencies temporarily.

--- app/controllers/api/project_usage_controller.rb
+++ app/controllers/api/project_usage_controller.rb
@@ -1,7 +1,7 @@
 class Api::ProjectUsageController < ApplicationController
   before_action :find_project
   def show
-    @all_counts = @project.repository_dependencies.where('repositories.fork = ?', false).joins(manifest: :repository).distinct('manifests.repository_id').group('repository_dependencies.requirements').count.select{|k,v| k.present? }
+    @all_counts = {} # @project.repository_dependencies.where('repositories.fork = ?', false).joins(manifest: :repository).distinct('manifests.repository_id').group('repository_dependencies.requirements').count.select{|k,v| k.present? }
 
     render json: @all_counts
   end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -32,7 +32,7 @@ class Tag < ApplicationRecord
     repository.projects.without_versions.each do |project|
       repos = project.subscriptions.map(&:repository).compact.uniq
       repos.each do |repo|
-        requirements = repo.repository_dependencies.select{|rd| rd.project == project }.map(&:requirements)
+        requirements = [] # repo.repository_dependencies.select{|rd| rd.project == project }.map(&:requirements)
         repo.web_hooks.each do |web_hook|
           web_hook.send_new_version(project, project.platform, self, requirements)
         end
