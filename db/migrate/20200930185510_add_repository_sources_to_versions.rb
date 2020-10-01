28ef82c08b57b44a0b49fa2f56f6888d93637304
mikeyoung85 <mike.young@tidelift.com>
2020-10-01 16:17:04 -0400

    drop default for column

--- db/migrate/20200930185510_add_repository_sources_to_versions.rb
+++ db/migrate/20200930185510_add_repository_sources_to_versions.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 class AddRepositorySourcesToVersions < ActiveRecord::Migration[5.2]
-  def up
+  def change
     add_column :versions, :repository_sources, :jsonb
-    change_column_default :versions, :repository_sources, []
-  end
-
-  def down
-    remove_column :versions, :repository_sources
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_192759) do
+ActiveRecord::Schema.define(version: 2020_09_30_185510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -394,7 +394,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_192759) do
     t.string "spdx_expression"
     t.jsonb "original_license"
     t.datetime "researched_at"
-    t.jsonb "repository_sources", default: []
+    t.jsonb "repository_sources"
     t.index ["project_id", "number"], name: "index_versions_on_project_id_and_number", unique: true
   end
 
