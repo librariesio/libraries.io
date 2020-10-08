dbbbaac4bd9b126394e0df45950baf8ac2db55d5
mikeyoung85 <mike.young@tidelift.com>
2020-10-08 15:18:08 -0400

    reuse download_url logic in package_link

--- app/models/package_manager/maven/atlassian.rb
+++ app/models/package_manager/maven/atlassian.rb
@@ -16,11 +16,7 @@ class PackageManager::Maven::Atlassian < PackageManager::Maven
   end
 
   def self.package_link(project, version = nil)
-    if version
-      MavenUrl.from_name(project.name, repository_base).jar(version)
-    else
-      MavenUrl.from_name(project.name, repository_base).base
-    end
+    download_url(project.name, version)
   end
 
   def self.download_url(name, version = nil)
