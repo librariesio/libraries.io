6c1276fb5c67407744c6aeddd9d12bb71d5adda3
Tieg Zaharia <tieg@tidelift.com>
2020-12-14 16:41:19 -0500

    Fix VersionsMailer nil email bug:
    "undefined method `empty?' for nil:NilClass"

--- app/mailers/versions_mailer.rb
+++ app/mailers/versions_mailer.rb
@@ -5,7 +5,7 @@ class VersionsMailer < ApplicationMailer
     @project = project
     @repos = @project.subscribed_repos(@user)
 
-    return if user.email.empty?
+    return if user.email.blank?
 
     mail to: user.email, subject: "New release of #{@project} (#{version.number}) on #{@project.platform_name}"
   end
