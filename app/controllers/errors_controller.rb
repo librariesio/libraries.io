f6065c289fce3b0e3759ddfa8e22c6425d89efd2
Tieg Zaharia <tieg@tidelift.com>
2020-12-17 12:42:32 -0500

    Add fallbacks for error endpoints and backfill a missing template.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,6 +4,7 @@ class ErrorsController < ApplicationController
       format.html { render status: :not_found }
       format.atom { render xml: "not found", root: "error", status: :not_found }
       format.json { render json: { error: "not found" }, status: :not_found }
+      format.any { head :not_found }
     end
   end
 
@@ -12,6 +13,7 @@ class ErrorsController < ApplicationController
       format.html { render status: :unprocessable_entity }
       format.atom { render xml: "unprocessable", root: "error", status: :unprocessable_entity }
       format.json { render json: { error: "unprocessable" }, status: :unprocessable_entity }
+      format.any { head :unprocessable_entity }
     end
   end
 
@@ -20,6 +22,7 @@ class ErrorsController < ApplicationController
       format.html { render status: :not_acceptable }
       format.atom { render xml: "not_acceptable", root: "error", status: :not_acceptable }
       format.json { render json: { error: "not_acceptable" }, status: :not_acceptable }
+      format.any { head :not_acceptable }
     end
   end
 
@@ -28,6 +31,7 @@ class ErrorsController < ApplicationController
       format.html { render status: :internal_server_error }
       format.atom { render xml: "internal server error", root: "error", status: :internal_server_error }
       format.json { render json: { error: "internal server error" }, status: :internal_server_error }
+      format.any { head :internal_server_error }
     end
   end
 end
