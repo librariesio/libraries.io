c2e2f96cad7913533c233025b09ef1339ccb0c19
jsonperl <jason.a.pearl@gmail.com>
2020-12-23 13:35:28 -0500

    Add google cloud tracing to active model serializers

---
+++ config/initializers/active_model_serializers.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module ASMTrace
+  def serializable_hash(*)
+    if Rails.env.production?
+      Google::Cloud::Trace.in_span "active_model_serializers#serializable_hash##{self.class.name}" do |_span|
+        super
+      end
+    elsif ENV["ASM_TRACING"]
+      result = nil
+      b = Benchmark.measure { result = super }
+      puts "active_model_serializers#serializable_hash##{self.class.name}: #{b.real}ms"
+      result
+    else
+      super
+    end
+  end
+end
+
+# We're prepending our tracing module to hook into the serializer_hash(render) method of ASM for tracing
+ActiveModel::Serializer.prepend(ASMTrace)
+ActiveModel::Serializer::ArraySerializer.prepend(ASMTrace)
