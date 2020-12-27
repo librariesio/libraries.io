2cf23abc2208ab58bff6e6cbe1116eb95db2e1ac
jsonperl <jason.a.pearl@gmail.com>
2020-12-27 16:02:02 -0500

    Add a new optimized version of /check avoiding active model serializers

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 class Api::StatusController < Api::BulkProjectController
   before_action :require_api_key
 
   def check
+    if params[:v2] == "true"
+      check_new
+    else
+      check_legacy
+    end
+  end
+
+  def check_legacy
     render(
       json: projects,
       each_serializer: ProjectStatusSerializer,
@@ -10,5 +20,12 @@ class Api::StatusController < Api::BulkProjectController
       show_updated_at: internal_api_key?,
       project_names: project_names
     )
+  end
+
+  def check_new
+    # This forces serialization with Oj
+    # TODO: Investigate calling Oj.mimic_JSON on initializationh to swap universally
+    serializer = OptimizedProjectSerializer.new(projects, project_names, internal_api_key?)
+    render json: Oj.dump(serializer.serialize)
   end
 end
