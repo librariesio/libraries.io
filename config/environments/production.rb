2f86781e3ea2e14802d148711a43023e314ba25d
Jeremy Katz <jeremy@tidelift.com>
2020-12-28 23:28:39 +0000

    Move assets to be served from assets/REVISION
    Put the git revision as a prefix on all the asset filenames

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.assets.prefix = "assets/#{ENV['REVISION_ID']}"
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
