141805c99d54f5adfd96ea4f872ea76bee611b61
Jeremy Katz <jeremy@tidelift.com>
2020-12-28 23:27:31 +0000

    Fix a couple of test failures

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -19,13 +19,13 @@ describe "PagesController" do
       expect(page).to have_content 'Team'
     end
   end
-  describe "GET /open-data", type: :request do
+  describe "GET /open-data", type: :request, elasticsearch: true do
     it "renders successfully when logged out" do
       visit data_path
       expect(page).to have_content 'Open Data'
     end
   end
-  describe "GET /experiments", type: :request do
+  describe "GET /experiments", type: :request, elasticsearch: true do
     it "renders successfully when logged out" do
       visit data_path
       expect(page).to have_content 'Experiments'
