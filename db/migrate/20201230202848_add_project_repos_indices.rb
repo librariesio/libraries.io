0dc49f637877967ac3ef7550fc9ebe097919723c
Jeremy Katz <jeremy@tidelift.com>
2020-12-30 20:33:25 +0000

    Add some new indexes on projects and repositories based on query patterns

---
+++ db/migrate/20201230202848_add_project_repos_indices.rb
@@ -0,0 +1,9 @@
+class AddProjectReposIndices < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def change
+    add_index :projects, [:status], algorithm: :concurrently
+    add_index :repositories, [:fork], algorithm: :concurrently
+    add_index :repositories, [:private], algorithm: :concurrently
+  end
+end
