16387464dedd4cb5d48e1413902deaacf99910d5
Jeremy Katz <jeremy@tidelift.com>
2021-01-06 13:41:15 +0000

    Add indexes on languages and licenses for projects table

---
+++ db/migrate/20210106132735_add_projects_index_languages_licenses.rb
@@ -0,0 +1,8 @@
+class AddProjectsIndexLanguagesLicenses < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def change
+    add_index :projects, 'lower(language)', algorithm: :concurrently
+    add_index :projects, :normalized_licenses, using: 'gin', algorithm: :concurrently
+  end
+end
