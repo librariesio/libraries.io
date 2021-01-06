40626898b61417a7a38d2d00187ab801e95da621
Jeremy Katz <jeremy@tidelift.com>
2021-01-06 15:57:53 +0000

    Stop using faraday http cache where it is unlikely to help
    We're not hitting the same URLs over and over so this is just overhead
    and driving up memcache usage without being effective.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -7,7 +7,6 @@ Elasticsearch::Model.client = Elasticsearch::Client.new hosts: url.split(','),
                                                         retry_on_failure: true,
                                                         randomize_hosts: true,
                                                         transport_options: { request: { timeout: 10 } } do |builder|
-  builder.use :http_cache, store: Rails.cache, logger: Rails.logger, shared_cache: false, serializer: Marshal
   builder.use FaradayMiddleware::Gzip
   builder.use Google::Cloud::Trace::FaradayMiddleware if Rails.env.production?
   builder.use :instrumentation
--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,6 +1,4 @@
 Octokit.middleware = Faraday::RackBuilder.new do |builder|
-  logger = Logger.new(nil)
-  builder.use :http_cache, store: Rails.cache, logger: logger, shared_cache: false, serializer: Marshal
   builder.use Octokit::Middleware::FollowRedirects
   builder.use Octokit::Response::RaiseError
   builder.use Octokit::Response::FeedParser
@@ -10,5 +8,5 @@ Octokit.middleware = Faraday::RackBuilder.new do |builder|
   builder.adapter :typhoeus
   # ethon doesn't expose any other way to shut up its logging :(
   # https://github.com/typhoeus/ethon/issues/82
-  Ethon.logger = logger
+  Ethon.logger = Logger.new(nil)
 end
