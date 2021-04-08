96ff1d1524b5db6fa1eb46919fd8b73f2bcf7f8b
Tieg Zaharia <tieg@tidelift.com>
2021-04-08 17:32:00 -0400

    Fix

--- spec/models/package_manager/maven_spec.rb
+++ spec/models/package_manager/maven_spec.rb
@@ -89,9 +89,11 @@ describe PackageManager::Maven do
 
   describe ".mapping" do
     context "with missing pom" do
-      allow(described_class).to receive(:download_pom).and_raise(POMNotFound.new("https://a-spring-url"))
+      it "should return nil" do
+        allow(described_class).to receive(:download_pom).and_raise(POMNotFound.new("https://a-spring-url"))
 
-      expect(described_class.mapping({group_id: "org", artifact_id: "foo", version: "1.0.0"})).to eq(1)
+        expect(described_class.mapping({group_id: "org", artifact_id: "foo", version: "1.0.0"})).to eq(1)
+      end
     end
   end
 
