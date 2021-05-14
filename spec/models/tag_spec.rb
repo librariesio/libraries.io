7e437c3b5df432967cc33234122a7a6dbb2b1a86
Matt Ellis <mellis@tidelift.com>
2021-05-14 11:22:00 -0400

    fix issue with tag updates

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -8,4 +8,16 @@ describe Tag, type: :model do
   it { should validate_presence_of(:sha) }
   it { should validate_presence_of(:repository) }
   it { should validate_uniqueness_of(:name).scoped_to(:repository_id) }
+
+  context "notify_subscribers" do
+    let(:repository) { create(:repository) }
+    let(:project) { create(:project, repository: repository) }
+    let(:user) { create(:user) }
+    it "notifies subscribers" do
+      Subscription.create(project: project, user: user)
+      tag = Tag.create(repository: repository, name: "somename", sha: "somesha")
+      allow_any_instance_of(VersionsMailer).to receive(:new_version).with(anything).and_return(true)
+      expect(tag.notify_subscribers.count).to eq 1
+    end
+  end
 end
