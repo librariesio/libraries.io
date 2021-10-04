46b1fa4c6ce9f4ed45fd7bbae158698b02e555ef
Jeremy Katz <jeremy@tidelift.com>
2021-10-04 20:52:15 +0000

    Bump oj and go back to using for status check serializer
    We stopped using oj for serializing this because it was messing
    up for timezones but updating oj seems to fix that (and confirmed
    by some trawling of oj issues)

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -5,6 +5,6 @@ class Api::StatusController < Api::BulkProjectController
 
   def check
     serializer = OptimizedProjectSerializer.new(projects, project_names, internal_api_key?)
-    render json: serializer.serialize
+    render json: Oj.dump(serializer.serialize, :use_as_json => true, :mode => :rails)
   end
 end
