08f07e81c66b0eb9b163e3e54b9820a1372dd43d
Tieg Zaharia <tieg@tidelift.com>
2022-02-11 15:30:05 -0500

    Spec

--- spec/requests/api/tree_spec.rb
+++ spec/requests/api/tree_spec.rb
@@ -2,15 +2,25 @@
 require "rails_helper"
 
 describe "Api::TreesController" do
-  let(:version) { create(:version) }
+  let!(:old_version) { create(:version, number: "1.0.0", published_at: 1.month.ago) }
+  let!(:new_version) { create(:version, number: "2.0.0", project: old_version.project, published_at: 1.day.ago) }
   let(:user) { create(:user) }
 
   describe "GET /api/:platform/:name/tree", type: :request do
     it "renders successfully" do
-      get "/api/#{version.project.platform}/#{version.project.name}/tree?api_key=#{user.api_key}"
+      get "/api/#{new_version.project.platform}/#{new_version.project.name}/tree?api_key=#{user.api_key}"
       expect(response).to have_http_status(:success)
       expect(response.content_type).to eq('application/json')
-      expect(response.body).to be_json_eql TreeResolver.new(version, 'runtime', Date.today).tree.to_json
+      expect(response.body).to be_json_eql TreeResolver.new(new_version, 'runtime', Date.today).tree.to_json
+    end
+  end
+
+  describe "GET /api/:platform/:name/:version/tree", type: :request do
+    it "renders successfully" do
+      get "/api/#{old_version.project.platform}/#{old_version.project.name}/#{old_version.number}/tree?api_key=#{user.api_key}"
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(response.body).to be_json_eql TreeResolver.new(old_version, 'runtime', Date.today).tree.to_json
     end
   end
 end
