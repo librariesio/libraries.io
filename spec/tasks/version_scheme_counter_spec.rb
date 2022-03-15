98b71ffe00168f1cd6810af52925d366de45420d
Pierre Pirault <pierre.pirault@outlook.com>
2022-03-15 14:09:43 -0400

    Fix version scheme rake task bugs and increase logging and write all logs to file incrementally (#2915)

--- spec/tasks/version_scheme_counter_spec.rb
+++ spec/tasks/version_scheme_counter_spec.rb
@@ -65,7 +65,7 @@ describe "version:scheme_counter" do
       end
     end
 
-    let(:blank_tallies) {{ semver: 0, pep440: 0, maven: 0, osgi: 0, calver: 0, unknown: 0, no_versions: 0, unknown_schemes: [], warnings: []}}
+    let(:blank_tallies) {{ semver: 0, pep440: 0, maven: 0, osgi: 0, calver: 0, unknown: 0, no_versions: 0, unknown_schemes: [], warnings: [], versionless_packages: []}}
 
     describe "Tally counting" do
       before(:example) do
@@ -82,7 +82,7 @@ describe "version:scheme_counter" do
           end
 
           Rake::Task["version:scheme_counter"].execute(package_list: tempfile.path)
-          expect(JSON).to have_received(:pretty_generate).with({ **blank_tallies, **expect })
+          expect(JSON).to have_received(:pretty_generate).twice.with({ **blank_tallies, **expect })
         end
       end
 
@@ -97,7 +97,7 @@ describe "version:scheme_counter" do
         let(:project) { create(:project, platform: "maven") }
         let(:versions) { %w[3.7 3.8.1 4.11-beta-1 4.13-rc-1 1.2-SNAPSHOT 1.4.2-12] }
 
-        it_should_behave_like "Detects scheme", { maven: 1 }
+        it_should_behave_like "Detects scheme", { osgi: 1 }
       end
 
       context "Semver" do
