bf22e208596020fe1a9158509c9e688854b4d1c4
mikeyoung85 <mike.young@tidelift.com>
2022-03-21 16:40:02 -0400

    revert adding index

--- db/migrate/20220318153151_add_published_at_index_to_version.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishedAtIndexToVersion < ActiveRecord::Migration[5.2]
-  def change
-    add_index :versions, [:project_id, :published_at]
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_153151) do
+ActiveRecord::Schema.define(version: 2021_01_26_141107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -402,8 +402,6 @@ ActiveRecord::Schema.define(version: 2022_03_18_153151) do
     t.jsonb "repository_sources"
     t.string "status"
     t.index ["project_id", "number"], name: "index_versions_on_project_id_and_number", unique: true
-    t.index ["project_id", "published_at"], name: "index_versions_on_project_id_and_published_at"
-    t.index ["updated_at"], name: "index_versions_on_updated_at"
   end
 
   create_table "web_hooks", id: :serial, force: :cascade do |t|
