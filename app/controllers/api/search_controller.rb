3fa8e5941b6ddb657e3c0604a1c0d17ffa655690
Jeremy Katz <jeremy@katzbox.net>
2022-08-03 16:26:10 -0400

    Require an internal api key for the search endpoint
    We're falling over due to excessive requests to this so let's
    block that for the moment

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 class Api::SearchController < Api::ApplicationController
+  # this is expensive, so let's restrict to internal api keys to avoid breaking hte site
+  before_action :require_internal_api_key
+
   def index
+    raise ActionController::BadRequest unless internal_api_key?
+
     @search = paginate search_projects(params[:q])
     @projects = @search.records.includes(:repository, :versions)
 
