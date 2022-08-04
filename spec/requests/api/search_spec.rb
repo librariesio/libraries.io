12b2c42b3d91ed41a291dfaac4b43da69376f7cb
Travis Collins <travistx@gmail.com>
2022-08-04 04:04:09 -0600

    fix specs

--- spec/requests/api/search_spec.rb
+++ spec/requests/api/search_spec.rb
@@ -1,13 +1,35 @@
 # frozen_string_literal: true
+
 require "rails_helper"
 
 describe "API::SearchController" do
   describe "GET /api/search", type: :request do
-    it "renders successfully" do
-      get "/api/search"
-      expect(response).to have_http_status(:success)
-      expect(response.content_type).to eq('application/json')
-      expect(response.body).to be_json_eql []
+    let!(:user) { create(:user) }
+
+    context "with normal api key" do
+      before :each do
+        user.current_api_key.update_attribute(:is_internal, false)
+      end
+
+      it "errors if api_key is not internal" do
+        get "/api/search?api_key=#{user.api_key}"
+
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    context "with internal api key" do
+      before :each do
+        user.current_api_key.update_attribute(:is_internal, true)
+      end
+
+      it "renders successfully" do
+        get "/api/search?api_key=#{user.api_key}"
+
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq('application/json')
+        expect(response.body).to be_json_eql []
+      end
     end
   end
 end
