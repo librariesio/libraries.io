deb308c51bb9a7ff08f8cf166adadafb0802cc80
Jeremy Katz <jeremy@tidelift.com>
2022-08-31 20:16:52 +0000

    Remove need for ES for /explore endpoint
    Remove lists of "popular" keywords/licenses from the /explore
    endpoint and then pull platforms and languages directly from
    postgresql

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 class ExploreController < ApplicationController
   def index
-    @platforms = Project.popular_platforms(facet_limit: 40).first(28)
-    @keywords = Project.popular_keywords(facet_limit: 40).first(15)
-    @languages = Project.popular_languages(facet_limit: 40).first(21)
-    @licenses = Project.popular_licenses(facet_limit: 40).first(10)
+    @platforms = Project.maintained.group(:platform).order('count_id DESC').limit(28).count('id').keys
+    @languages = Project.maintained.group(:language).order('count_id DESC').limit(21).count('id').keys
 
     project_scope = Project.includes(:repository).maintained.with_description
 
