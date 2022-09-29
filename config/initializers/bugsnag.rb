e1bf0bba45406460355985385185b3ff2a6678d6
Tieg Zaharia <tieg@tidelift.com>
2022-09-29 15:21:55 -0700

    VersionUpdateFailure silence for now

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
     config.api_key = ENV["BUGSNAG_API_KEY"]
     config.ignore_classes << ActiveRecord::RecordNotFound
     # Temprarily uncomment this is we get a huge spike of these errors and need to investigate:
-    # config.ignore_classes << PackageManagerDownloadWorker::VersionUpdateFailure
+    config.ignore_classes << PackageManagerDownloadWorker::VersionUpdateFailure
 
     if File.exist?("#{Rails.root}/REVISION")
       config.app_version = File.read("#{Rails.root}/REVISION").strip
