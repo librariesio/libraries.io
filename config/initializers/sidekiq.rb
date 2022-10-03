af804b9e6138b14df9bb54f413b4913bb47a46da
Tieg Zaharia <tieg@tidelift.com>
2022-10-03 13:05:16 -0700

    Replace stackdriver tracing with datadog tracing

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,9 +5,6 @@ require 'resolv-replace' # pure ruby DNS
 # https://github.com/mperham/sidekiq/issues/3518#issuecomment-390896088
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDISCLOUD_URL"], id: nil }
-  config.server_middleware do |chain|
-    chain.add Sidekiq::GoogleCloudTraceMiddleware, capture_stack: false
-  end
 end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDISCLOUD_URL"], id: nil }
