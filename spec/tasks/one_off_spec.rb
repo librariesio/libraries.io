99bebf293081ae918f21d6af734574278433478b
Pierre Pirault <pierre.pirault@outlook.com>
2022-11-17 11:59:11 -0500

    Add test case for category with single stat

--- spec/tasks/one_off_spec.rb
+++ spec/tasks/one_off_spec.rb
@@ -16,13 +16,15 @@ describe "one_off rake tasks" do
       create_list(:repository_maintenance_stat, 2, category: "bar", repository: repository) do |rms, i|
         rms.update!(updated_at: (i+1).days.ago)
       end
+
+      create(:repository_maintenance_stat, category: "baz", repository: repository, updated_at: 1.days.ago)
     end
 
     it "dedupes correctly" do
       expect do
         Rake::Task["one_off:dedupe_repository_maintenance_stats"].execute
       end.to change(RepositoryMaintenanceStat, :count)
-               .from(5).to(2)
+               .from(6).to(3)
 
       expect(RepositoryMaintenanceStat.all).to all(have_attributes(updated_at: 1.days.ago))
     end
