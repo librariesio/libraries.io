a62351494b828036defc0b0d69ec0d5356063d42
Madeline Cowie <madeline@cowie.me>
2023-03-08 17:54:03 -0600

    Use github's built in retry mechanism instead of broken adhoc one

--- app/models/maintenance_stats/queries/github/repository_contributor_stats_query.rb
+++ app/models/maintenance_stats/queries/github/repository_contributor_stats_query.rb
@@ -5,22 +5,15 @@ module MaintenanceStats
       class RepositoryContributorStatsQuery < BaseQuery
         VALID_PARAMS = [:full_name]
         REQUIRED_PARAMS = [:full_name]
-        RETRY_AMOUNT = 4
 
         def self.client_type
           :v3
         end
 
-        def query(params: {}, count: 0)
-          raise Octokit::Error.new "Didn't get a response after #{RETRY_AMOUNT} attempts" if params[:count].present? && params[:count] >= RETRY_AMOUNT
+        def query(params: {})
           validate_params(params)
 
-          resp = @client.contributor_stats(params[:full_name])
-          if @client.last_response.status == 202
-            sleep (count + 1) * 0.5
-            return query(params: params, count: count+=1)
-          end
-          resp
+          @client.contributor_stats(params[:full_name], retry_wait: 0.5, retry_timeout: 10)
         end
       end
     end
