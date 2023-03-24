ef6b1cc6121792375119c5eb79d8b410e8117639
John Bintz <johnbintz@tidelift.com>
2023-03-24 16:08:54 -0400

    Allow parsing of https URLs in Maven POM XML comments

--- spec/models/package_manager/maven_spec.rb
+++ spec/models/package_manager/maven_spec.rb
@@ -277,31 +277,62 @@ describe PackageManager::Maven do
     end
 
     context "with licences in the comments" do
-      it "returns those licenses" do
-        pom = Ox.parse(
-          <<-EOF
-            <?xml version="1.0"?>
-            <!--
-               Licensed to the Apache Software Foundation (ASF) under one or more
-               contributor license agreements.  See the NOTICE file distributed with
-               this work for additional information regarding copyright ownership.
-               The ASF licenses this file to You under the Apache License, Version 2.0
-               (the "License"); you may not use this file except in compliance with
-               the License.  You may obtain a copy of the License at
+      context "with http URL" do
+        it "returns those licenses" do
+          pom = Ox.parse(
+            <<-EOF
+              <?xml version="1.0"?>
+              <!--
+                 Licensed to the Apache Software Foundation (ASF) under one or more
+                 contributor license agreements.  See the NOTICE file distributed with
+                 this work for additional information regarding copyright ownership.
+                 The ASF licenses this file to You under the Apache License, Version 2.0
+                 (the "License"); you may not use this file except in compliance with
+                 the License.  You may obtain a copy of the License at
 
-                   http://www.apache.org/licenses/LICENSE-2.0
+                     http://www.apache.org/licenses/LICENSE-2.0
 
-               Unless required by applicable law or agreed to in writing, software
-               distributed under the License is distributed on an "AS IS" BASIS,
-               WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-               See the License for the specific language governing permissions and
-               limitations under the License.
-            -->
-            <project></project>
-          EOF
-        )
+                 Unless required by applicable law or agreed to in writing, software
+                 distributed under the License is distributed on an "AS IS" BASIS,
+                 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                 See the License for the specific language governing permissions and
+                 limitations under the License.
+              -->
+              <project></project>
+            EOF
+          )
 
-        expect(described_class.licenses(pom)).to eq(["Apache-2.0"])
+          expect(described_class.licenses(pom)).to eq(["Apache-2.0"])
+        end
+      end
+
+      context "with https URL" do
+        it "returns those licenses" do
+          pom = Ox.parse(
+            <<-EOF
+              <?xml version="1.0"?>
+              <!--
+                 Licensed to the Apache Software Foundation (ASF) under one or more
+                 contributor license agreements.  See the NOTICE file distributed with
+                 this work for additional information regarding copyright ownership.
+                 The ASF licenses this file to You under the Apache License, Version 2.0
+                 (the "License"); you may not use this file except in compliance with
+                 the License.  You may obtain a copy of the License at
+
+                     https://www.apache.org/licenses/LICENSE-2.0
+
+                 Unless required by applicable law or agreed to in writing, software
+                 distributed under the License is distributed on an "AS IS" BASIS,
+                 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                 See the License for the specific language governing permissions and
+                 limitations under the License.
+              -->
+              <project></project>
+            EOF
+          )
+
+          expect(described_class.licenses(pom)).to eq(["Apache-2.0"])
+        end
       end
     end
 
