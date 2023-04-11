0e5d1456f7d0c02b5dc6912ec351dbe5f113e2f8
Tieg Zaharia <tieg@tidelift.com>
2023-04-11 11:50:31 -0700

    Fix spec

--- spec/models/package_manager/packagist_spec.rb
+++ spec/models/package_manager/packagist_spec.rb
@@ -3,13 +3,13 @@
 require "rails_helper"
 
 describe PackageManager::Packagist do
+  let(:project) { create(:project, name: "foo", platform: described_class.formatted_name) }
+
   it 'has formatted name of "Packagist"' do
     expect(described_class.formatted_name).to eq("Packagist")
   end
 
   describe "#package_link" do
-    let(:project) { create(:project, name: "foo", platform: described_class.formatted_name) }
-
     it "returns a link to project website" do
       expect(described_class.package_link(project)).to eq("https://packagist.org/packages/foo#")
     end
