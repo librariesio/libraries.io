f28fc54e912a7d17a0b10b703f19d52c8c773bfb
Tieg Zaharia <tieg@tidelift.com>
2023-04-14 08:12:20 -0700

    Use simpler way of configuring rack-attack

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,32 +1,34 @@
 # frozen_string_literal: true
 
-class Rack::Attack::Request < ::Rack::Request
-  def valid_key
-    return @valid_key if defined? @valid_key
+class Rack::Attack
+  class Request < ::Rack::Request
+    def valid_key
+      return @valid_key if defined? @valid_key
 
-    @valid_key = ApiKey.active.find_by_access_token(params["api_key"])
+      @valid_key = ApiKey.active.find_by_access_token(params["api_key"])
+    end
   end
-end
 
-Rack::Attack.blocklist("invalid api key") do |req|
-  !req.valid_key if req.params["api_key"].present?
-end
-
-limit_proc = proc do |req|
-  if req.params["api_key"].present?
-    req.valid_key.rate_limit
-  else
-    10 # req/min for anonymous users
+  blocklist("invalid api key") do |req|
+    !req.valid_key if req.params["api_key"].present?
   end
-end
 
-Rack::Attack.throttle('api', limit: limit_proc, period: 1.minute) do |req|
-  if req.path.match(/^\/api\/.+/) && !req.path.match(/^\/api\/bower-search/)
-    req.params['api_key'] || req.ip
+  limit_proc = proc do |req|
+    if req.params["api_key"].present?
+      req.valid_key.rate_limit
+    else
+      10 # req/min for anonymous users
+    end
   end
-end
 
-# throttle scraping
-Rack::Attack.throttle('scrapers', limit: 30, period: 5.minutes) do |req|
-  req.ip if req.user_agent && req.user_agent.match(/Scrapy.*/)
+  throttle('api', limit: limit_proc, period: 1.minute) do |req|
+    if req.path.match(/^\/api\/.+/) && !req.path.match(/^\/api\/bower-search/)
+      req.params['api_key'] || req.ip
+    end
+  end
+
+  # throttle scraping
+  throttle('scrapers', limit: 30, period: 5.minutes) do |req|
+    req.ip if req.user_agent && req.user_agent.match(/Scrapy.*/)
+  end
 end
