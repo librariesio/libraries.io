dc9fe67696deac2090eb5234152013cb03b39b96
Dave Powers <djpowers89@gmail.com>
2023-05-16 11:29:11 -0400

    Remove VCR sequence matching code
    - rerecord cassette using static project name

--- spec/workers/check_status_worker_spec.rb
+++ spec/workers/check_status_worker_spec.rb
@@ -8,22 +8,8 @@ describe CheckStatusWorker do
   end
 
   it "should check repo status" do
-    uri_ignoring_trailing_id = lambda do |request1, request2|
-      uri1 = request1.uri
-      uri2 = request2.uri
-      # capture all versions matching "rails" with sequence
-      regexp_trail_id = /rails\d+.json/
-      if uri1.match(regexp_trail_id)
-        r1_without_id = uri1.gsub(regexp_trail_id, "")
-        r2_without_id = uri2.gsub(regexp_trail_id, "")
-        uri1.match(regexp_trail_id) && uri2.match(regexp_trail_id) && r1_without_id == r2_without_id
-      else
-        uri1 == uri2
-      end
-    end
-
-    VCR.use_cassette("project/check_status/rails", match_requests_on: [:method, uri_ignoring_trailing_id]) do
-      project = create(:project)
+    VCR.use_cassette("project/check_status/rails") do
+      project = create(:project, name: "rails")
       expect(Project).to receive(:find_by_id).with(project.id).and_return(project)
       subject.perform(project.id)
     end
