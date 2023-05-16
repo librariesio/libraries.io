8a3b26d3cbd5effda723c5bb24513938c9bcd81a
Dylan Mahalingam <dylan@tidelift.com>
2023-05-16 11:46:13 -0400

    Add index concurrently

--- db/migrate/20230516150532_add_status_checked_at_to_projects.rb
+++ db/migrate/20230516150532_add_status_checked_at_to_projects.rb
@@ -1,6 +1,8 @@
 class AddStatusCheckedAtToProjects < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
     add_column :projects, :status_checked_at, :datetime
-    add_index :projects, :status_checked_at
+    add_index :projects, :status_checked_at, algorithm: :concurrently
   end
 end
