3593fd23d715dd80fe80be656be5d469fe3612c7
Jeremy Katz <jeremy@tidelift.com>
2023-05-16 22:29:07 -0400

    Set a default for the parameter too

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -4,7 +4,7 @@ class CheckStatusWorker
   include Sidekiq::Worker
   sidekiq_options queue: :status, unique: :until_executed
 
-  def perform(project_id, _ignored)
+  def perform(project_id, _ignored = nil)
     Project.find_by_id(project_id).try(:check_status)
   end
 end
