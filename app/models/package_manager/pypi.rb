4eb5ba60099df5373d603a795112bb03ca160af2
Pierre Pirault <pierre.pirault@outlook.com>
2023-05-31 20:36:18 -0400

    Update app/models/package_manager/pypi.rb
    Co-authored-by: Matthew Pace <matt.pace@tidelift.com>

--- app/models/package_manager/pypi.rb
+++ app/models/package_manager/pypi.rb
@@ -13,11 +13,14 @@ module PackageManager
     PYPI_PRERELEASE = /(a|b|rc|dev)[0-9]+$/.freeze
     # Adapted from https://peps.python.org/pep-0508/#names
     PEP_508_NAME_REGEX = /^([A-Z0-9][A-Z0-9._-]*[A-Z0-9]|[A-Z0-9])/i.freeze
-    PEP_508_ENVIRONMENT_MARKERS = "python_version|python_full_version|" +
-      "os_name|sys_platform|platform_release|" +
-      "platform_system|platform_version|" +
-      "platform_machine|platform_python_implementation|" +
-      "implementation_name|implementation_version|extra"
+    PEP_508_ENVIRONMENT_MARKERS = %w[
+      python_version python_full_version os_name
+      sys_platform platform_release platform_system
+      platform_version platform_machine platform_python_implementation
+      implementation_name implementation_version extra
+    ].freeze
+    
+    PEP_508_ENVIRONMENT_MARKER_REGEX = PEP_508_ENVIRONMENT_MARKERS.join("|")
 
     def self.package_link(db_project, version = nil)
       # NB PEP 503: "All URLs which respond with an HTML5 page MUST end with a / and the repository SHOULD redirect the URLs without a / to add a / to the end."
