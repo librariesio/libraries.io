5cc70a51b940954332f3dc674d5108cc5dd1dbb5
John Bintz <johnbintz@tidelift.com>
2023-07-19 12:28:14 -0400

    Remove deprecation class code/test mocks, move calculation to PORO
    A mocked implementation of the output of project in the deprecation_info
    tests made it so I missed that project was being used there. This moves
    deprecation calculation into the JSON API classes and replaces the mocks
    with a verifying instance double.

--- app/models/package_manager/pypi/json_api_project.rb
+++ app/models/package_manager/pypi/json_api_project.rb
@@ -1,6 +1,8 @@
 module PackageManager
   class Pypi
     class JsonApiProject
+      CLASSIFIER_INACTIVE = "Development Status :: 7 - Inactive"
+
       def self.request(project_name:)
         data = begin
           ApiService.request_json_with_headers("https://pypi.org/pypi/#{project_name}/json")
@@ -28,7 +30,7 @@ module PackageManager
       end
 
       def license_classifiers
-        license_classifiers = @data.dig("info", "classifiers").select { |c| c.start_with?("License :: ") }
+        license_classifiers = classifiers.select { |c| c.start_with?("License :: ") }
         license_classifiers.map { |l| l.split(":: ").last }.join(",")
       end
 
@@ -73,9 +75,13 @@ module PackageManager
       def releases
         JsonApiProjectReleases.new(
           @data["releases"].map do |version_number, details|
+            first_details = details.first || {}
+
             JsonApiProjectRelease.new(
               version_number: version_number,
-              published_at: release_data_published_at(details)
+              published_at: release_data_published_at(details),
+              is_yanked: first_details["yanked"] == true,
+              yanked_reason: first_details["yanked_reason"]
             )
           end
         )
@@ -94,7 +100,41 @@ module PackageManager
         }
       end
 
+      def deprecated?
+        deprecation_status[:is_deprecated]
+      end
+
+      def deprecation_message
+        deprecation_status[:message]
+      end
+
       private
+
+      def classifiers
+        @data.dig("info", "classifiers")
+      end
+
+      def deprecation_status
+        return @deprecation_status if @deprecation_status
+
+        is_deprecated = false
+        message = nil
+
+        latest_stable = releases.reject(&:prerelease?).last
+
+        if latest_stable&.yanked?
+          is_deprecated = true
+          message = latest_stable.yanked_reason
+        elsif classifiers.include?(CLASSIFIER_INACTIVE)
+          is_deprecated = true
+          message = CLASSIFIER_INACTIVE
+        end
+
+        @deprecation_status = {
+          is_deprecated: is_deprecated,
+          message: message,
+        }
+      end
 
       def release_data_published_at(details)
         return nil if details == []
--- app/models/package_manager/pypi/json_api_project_release.rb
+++ app/models/package_manager/pypi/json_api_project_release.rb
@@ -1,15 +1,32 @@
 module PackageManager
   class Pypi
     class JsonApiProjectRelease
-      attr_reader :version_number, :published_at
+      attr_reader :version_number, :published_at, :yanked_reason
 
-      def initialize(version_number:, published_at:)
+      PYPI_PRERELEASE = /(a|b|rc|dev)[-_.]?[0-9]*$/.freeze
+
+      def initialize(
+        version_number:,
+        published_at:,
+        is_yanked:,
+        yanked_reason:
+      )
         @version_number = version_number
         @published_at = published_at
+        @is_yanked = is_yanked
+        @yanked_reason = yanked_reason
       end
 
       def published_at?
         !@published_at.nil?
+      end
+
+      def prerelease?
+        @version_number =~ PYPI_PRERELEASE
+      end
+
+      def yanked?
+        @is_yanked
       end
     end
   end
