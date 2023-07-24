82f27e03541058f3b589271b0503fa3f65a7bcad
Tieg Zaharia <tieg@tidelift.com>
2023-07-24 09:51:48 -0700

    Add api key id to rack attack logs, if it exists.

--- lib/subscribers/rack-attack.rb
+++ lib/subscribers/rack-attack.rb
@@ -5,6 +5,20 @@ ActiveSupport::Notifications.subscribe(/rack_attack/) do |_name, _start, _finish
   match = req.env["rack.attack.matched"]
   match_type = req.env["rack.attack.match_type"]
   match_discriminator = req.env["rack.attack.match_discriminator"].to_s[0, 16] # truncate, could be an api key
+  api_key = begin
+    req.params["api_key"]
+  rescue EOFError # req.params can blow up with bad data
+    req.GET["api_key"]
+  end
+  api_key_id = ApiKey.find_by_access_token(api_key) if api_key
 
-  Rails.logger.info "[RACK_ATTACK] method=#{req.request_method} path=#{req.fullpath} match_type=#{match_type} match=#{match} match_discriminator=#{match_discriminator} request_id=#{request_id} remote_ip=#{req.remote_ip}"
+  Rails.logger.info "[RACK_ATTACK] " +
+    "method=#{req.request_method} " +
+    "path=#{req.fullpath} " +
+    "match_type=#{match_type} " +
+    "match=#{match} " +
+    "match_discriminator=#{match_discriminator} " +
+    "request_id=#{request_id} " +
+    "remote_ip=#{req.remote_ip} " +
+    "api_key_id=#{api_key_id&.id || 'nil'}"
 end
