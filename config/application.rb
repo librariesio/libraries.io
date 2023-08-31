3951763c6ce1571f06dd8b79004c220359639840
Madeline Cowie <madeline@cowie.me>
2023-08-31 15:04:09 -0500

    Roll out bumping rails' defaults to current production version

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,7 @@ Bundler.require(*Rails.groups)
 
 module Libraries
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.1
     config.autoloader = :zeitwerk
 
     # Configuration for the application, engines, and railties goes here.
