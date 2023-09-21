edef1d3699a38635ad74ae4ea7b542cca84b8f00
Matt Pace <matt.pace@tidelift.com>
2023-09-21 16:45:05 -0400

    SearchController#index: short-circuit when no search term is provided

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,11 +41,10 @@ module ApplicationHelper
                                      else
                                        collection.first.class.name.split("::").last.titleize
                                      end)
-    if collection.total_pages < 2
-      case collection.size
-      when 0 then "No #{entry_name.pluralize} found"
-      else; "#{collection.total_entries} #{entry_name.pluralize}"
-      end
+    if collection.empty?
+      "No #{entry_name.pluralize} found"
+    elsif collection.total_pages < 2
+      "#{collection.total_entries} #{entry_name.pluralize}"
     else
       format(%(%d - %d of #{number_to_human(collection.total_entries)} #{entry_name.pluralize}), collection.offset + 1, collection.offset + collection.length)
     end
