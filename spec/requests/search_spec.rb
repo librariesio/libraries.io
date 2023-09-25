9e54d5c55837fa6337f641a8ab35ad269cb9e3c3
Matt Pace <matt.pace@tidelift.com>
2023-09-25 12:40:25 -0400

    SearchController#index: short-circuit when no search criteria are provided

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -4,19 +4,43 @@ require "rails_helper"
 
 describe "SearchController", elasticsearch: true do
   let!(:project) { create(:project) }
+  let(:search_criteria) { project.name }
 
   describe "GET /search", type: :request do
+    context "without search criteria" do
+      let(:search_criteria) { "" }
+
+      it "renders instructions to try again" do
+        Project.__elasticsearch__.refresh_index!
+        visit search_path(params: { q: search_criteria })
+
+        expect(page).not_to have_content project.name
+        expect(page).to have_content "Please provide search criteria and try again"
+      end
+    end
+
     it "renders successfully when logged out" do
       Project.__elasticsearch__.refresh_index!
-      visit search_path
+      visit search_path(params: { q: search_criteria })
       expect(page).to have_content project.name
     end
   end
 
   describe "GET /search.atom", type: :request do
+    context "without search criteria" do
+      let(:search_criteria) { "" }
+
+      it "renders no results" do
+        Project.__elasticsearch__.refresh_index!
+        visit search_path(params: { q: search_criteria })
+
+        expect(page).not_to have_content project.name
+      end
+    end
+
     it "renders successfully when logged out" do
       Project.__elasticsearch__.refresh_index!
-      visit search_path(format: :atom)
+      visit search_path(params: { q: search_criteria }, format: :atom)
       expect(page).to have_content project.name
     end
   end
