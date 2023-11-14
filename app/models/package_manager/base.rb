adc3bdb7068907280fbfb621e6a10b8eb2f0a94d
Tieg Zaharia <tieg@tidelift.com>
2023-11-14 12:26:07 -0700

    Log fix

--- app/models/package_manager/base.rb
+++ app/models/package_manager/base.rb
@@ -260,7 +260,7 @@ module PackageManager
       db_versions = db_versions.where(number: sync_version) unless sync_version == :all
 
       if db_versions.empty?
-        StructuredLog.capture("SAVE_DEPENDENCIES_FAILURE", { platform: db_platform, name: name, version: version, message: "no versions found" })
+        StructuredLog.capture("SAVE_DEPENDENCIES_FAILURE", { platform: db_platform, name: name, version: sync_version, message: "no versions found" })
       end
 
       db_versions.each do |db_version|
