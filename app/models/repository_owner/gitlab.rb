106e226501c994f4382d9551c53e51084ed6ce74
Tieg Zaharia <tieg@tidelift.com>
2023-12-03 11:45:04 -0700

    Lint

--- app/models/repository_owner/gitlab.rb
+++ app/models/repository_owner/gitlab.rb
@@ -163,7 +163,7 @@ module RepositoryOwner
     def get_json(url)
       r = Typhoeus::Request.new(url,
                                 method: :get,
-                                followlocation: true,                                
+                                followlocation: true,
                                 headers: { "Accept" => "application/json" }).run
       Oj.load(r.body)
     end
