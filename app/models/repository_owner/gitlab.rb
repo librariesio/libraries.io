01e6e1729e7b5894ad8e351452e0fa1b97b8f1d5
Tieg Zaharia <tieg@tidelift.com>
2023-12-04 10:57:00 -0700

    Follow redirects for repos that are moved on GitLab (#3270)
    * Follow redirects for repos that are moved on GitLab
    
    * Using a PackageManager::ApiService helper instead

--- app/models/repository_owner/gitlab.rb
+++ app/models/repository_owner/gitlab.rb
@@ -161,10 +161,7 @@ module RepositoryOwner
     private
 
     def get_json(url)
-      r = Typhoeus::Request.new(url,
-                                method: :get,
-                                headers: { "Accept" => "application/json" }).run
-      Oj.load(r.body)
+      PackageManager::ApiService.request_json_with_headers(url)
     end
   end
 end
