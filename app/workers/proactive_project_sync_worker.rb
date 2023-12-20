50ef226441b36ba4e59697af071789e568653db0
Madeline Cowie <madeline@cowie.me>
2023-12-20 13:33:23 -0600

    Probably dont need this arg shuffling

--- app/workers/proactive_project_sync_worker.rb
+++ app/workers/proactive_project_sync_worker.rb
@@ -8,12 +8,13 @@ class ProactiveProjectSyncWorker
   PLATFORMS = %w[NPM Maven].freeze
   DEFAULT_LIMIT = 1000
 
-  def perform(limit = nil)
-    projects_query(limit: limit)
+  def perform(limit = DEFAULT_LIMIT)
+    projects_query
+      .limit(limit)
       .each(&:async_sync)
   end
 
-  def projects_query(limit: DEFAULT_LIMIT)
+  def projects_query
     Project
       .distinct
       .visible
@@ -21,7 +22,6 @@ class ProactiveProjectSyncWorker
       .where.associated(:repository_maintenance_stats)
       .where("projects.last_synced_at IS NULL OR projects.last_synced_at < ?", last_synced_at_cutoff)
       .order("projects.last_synced_at ASC NULLS FIRST")
-      .limit(limit)
   end
 
   def last_synced_at_cutoff
