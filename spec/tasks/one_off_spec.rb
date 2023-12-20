6b97b3627f99c57c4d6c71c46718913ed1ffe0b0
Mike Young <mike.young@tidelift.com>
2023-12-20 13:28:37 -0500

    fix flaky spec

--- spec/tasks/one_off_spec.rb
+++ spec/tasks/one_off_spec.rb
@@ -115,7 +115,7 @@ describe "one_off" do
 
         it "deletes ignored versions" do
           expect { Rake::Task["one_off:delete_ignored_maven_versions_and_resync_packages"].invoke("", "yes") }
-            .to change(Version, :all).from([no_source_version, ignored_source_version]).to([])
+            .to change(Version, :all).from(match_array([no_source_version, ignored_source_version])).to([])
         end
       end
 
