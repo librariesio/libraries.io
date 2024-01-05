4eedd5983a3d5ef2f905a9155751100691f09f37
Mike Young <mike.young@tidelift.com>
2024-01-05 09:47:00 -0500

    use repo refreshed_at date for scheduling stat refresh

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -132,37 +132,47 @@ describe Project, type: :model do
         results = Project.no_existing_stats.where(id: project.id)
         expect(results.count).to eql 1
       end
+
+      context "with refreshed at date" do
+        let!(:repository) { create(:repository, maintenance_stats_refreshed_at: Time.current) }
+
+        it "should not be included in no_existing_stats query" do
+          # if the repository has a refreshed_at date but no stats that means
+          # there was a problem getting stats and should not be considered for
+          # the no_existing_stats query
+          results = Project.no_existing_stats.where(id: project.id)
+          expect(results.count).to eql 0
+        end
+      end
     end
 
     context "with stats" do
       let!(:stat1) { create(:repository_maintenance_stat, repository: repository) }
 
+      context "with refreshed at date" do
+        let!(:repository) { create(:repository, maintenance_stats_refreshed_at: Time.current) }
+
+        it "should show up in least_recently_updated_stats query" do
+          results = Project.least_recently_updated_stats.where(id: project.id)
+
+          expect(results.count).to eql 1
+        end
+      end
+
       it "should not be in no_existing_stats query" do
         results = Project.no_existing_stats.where(id: project.id)
         expect(results.count).to eql 0
       end
-
-      it "should show up in least_recently_updated_stats query" do
-        results = Project.least_recently_updated_stats.where(id: project.id)
-        # count will return a hash
-        # the key is the grouped column which is the project id
-        # the value is the count for that project id
-        expect(results.count.key?(project.id)).to be true
-        expect(results.count[project.id]).to eql 1
-      end
     end
 
     context "two projects with stats" do
+      let!(:repository) { create(:repository, maintenance_stats_refreshed_at: 1.day.ago) }
       let!(:stat1) { create(:repository_maintenance_stat, repository: repository) }
-      let!(:repository2) { create(:repository, full_name: "octokit/octokit") }
+      let!(:repository2) { create(:repository, full_name: "octokit/octokit", maintenance_stats_refreshed_at: 1.year.ago) }
       let!(:project2) { create(:project, repository: repository2) }
       let!(:stat2) { create(:repository_maintenance_stat, repository: repository2) }
 
-      before do
-        stat2.update_column(:updated_at, Date.today - 1.month)
-      end
-
-      it "should return project with oldest stats first" do
+      it "should return project with oldest refreshed at date first" do
         results = Project.least_recently_updated_stats
         expect(results.first.id).to eql project2.id
       end
