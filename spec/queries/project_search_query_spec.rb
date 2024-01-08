3fac1fd8fe249e533055aa4def1a565283df0200
Madeline Cowie <madeline@cowie.me>
2024-01-08 11:34:21 -0600

    Add more spec cases to build off of

--- spec/queries/project_search_query_spec.rb
+++ spec/queries/project_search_query_spec.rb
@@ -5,14 +5,20 @@ require "rails_helper"
 RSpec.describe ProjectSearchQuery do
   subject(:search) { described_class.new(term) }
 
-  let!(:falco) { create(:project, platform: "Cargo", language: "Rust", name: "falco") }
+  let!(:falco) { create(:project, platform: "Cargo", language: "Rust", name: "falco", description: "bird brain") }
   let!(:slippy) { create(:project, platform: "Go", language: "Go", name: "slippy", licenses: "WTFPL") }
   let!(:slimy) { create(:project, platform: "Rubygems", language: "Ruby", name: "slimy", keywords_array: %w[laser beam]) }
 
   describe "#results" do
-    let(:term) { "sli" }
+    context "with unmatching term" do
+      let(:term) { "fhqwhgads" }
 
-    context "with term exact match" do
+      it "finds by name" do
+        expect(search.results).to be_empty
+      end
+    end
+
+    context "with term exact match name" do
       let(:term) { "slimy" }
 
       it "finds by name" do
@@ -20,7 +26,15 @@ RSpec.describe ProjectSearchQuery do
       end
     end
 
-    context "with term prefix match" do
+    context "with term exact match description" do
+      let(:term) { "brain" }
+
+      it "finds by name" do
+        expect(search.results).to contain_exactly(falco)
+      end
+    end
+
+    context "with term prefix match name" do
       let(:term) { "sli" }
 
       it "finds projects with partial word" do
@@ -28,35 +42,39 @@ RSpec.describe ProjectSearchQuery do
       end
     end
 
-    context "with platform" do
-      subject(:search) { described_class.new(term, platforms: ["Go"]) }
+    describe "filtering" do
+      let(:term) { "sli" }
 
-      it "limits results to any given platform" do
-        expect(search.results).to contain_exactly(slippy)
+      context "by platform" do
+        subject(:search) { described_class.new(term, platforms: ["Go"]) }
+
+        it "limits results to any given platform" do
+          expect(search.results).to contain_exactly(slippy)
+        end
       end
-    end
 
-    context "with language" do
-      subject(:search) { described_class.new(term, languages: %w[Ruby Rust]) }
+      context "by language" do
+        subject(:search) { described_class.new(term, languages: %w[Ruby Rust]) }
 
-      it "filters out projects not of any of the given langauges" do
-        expect(search.results).to contain_exactly(slimy)
+        it "filters out projects not of any of the given langauges" do
+          expect(search.results).to contain_exactly(slimy)
+        end
       end
-    end
 
-    context "with licenses" do
-      subject(:search) { described_class.new(term, licenses: ["WTFPL"]) }
+      context "by licenses" do
+        subject(:search) { described_class.new(term, licenses: ["WTFPL"]) }
 
-      it "filters out projects without given license" do
-        expect(search.results).to contain_exactly(slippy)
+        it "filters out projects without given license" do
+          expect(search.results).to contain_exactly(slippy)
+        end
       end
-    end
 
-    context "with keyword" do
-      subject(:search) { described_class.new(term, keywords: ["laser"]) }
+      context "by keyword" do
+        subject(:search) { described_class.new(term, keywords: ["laser"]) }
 
-      it "filters out projects without given keyword" do
-        expect(search.results).to contain_exactly(slimy)
+        it "filters out projects without given keyword" do
+          expect(search.results).to contain_exactly(slimy)
+        end
       end
     end
   end
