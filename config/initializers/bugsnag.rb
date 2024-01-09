862cd8e07a163c1145831d656273ae5b478ec1b9
Tieg Zaharia <tieg@tidelift.com>
2024-01-09 12:45:37 -0700

    bugsnag 6.26.0 ~> 6.26.1 and remove interrim fix

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -13,16 +13,5 @@ if Rails.env.production?
     elsif ENV["REVISION_ID"]
       config.app_version = ENV["REVISION_ID"]
     end
-    config.add_on_breadcrumb(proc do |breadcrumb|
-      if breadcrumb.metadata[:path].present?
-        cleaned_path = begin
-          Bugsnag.cleaner.clean_url(breadcrumb.metadata[:path])
-        rescue StandardError => e
-          Rails.logger.info "[BUGSNAG FILTERING] error='#{e.class}' message='#{e.message}'"
-          nil
-        end
-        breadcrumb.metadata[:path] = cleaned_path if cleaned_path
-      end
-    end)
   end
 end
