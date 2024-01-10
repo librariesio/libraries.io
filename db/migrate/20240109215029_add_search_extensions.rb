06837f2473c22aba791c00c199dc1c36f9dec1cf
Madeline Cowie <madeline@cowie.me>
2024-01-10 16:30:44 -0600

    Add trigram extension

---
+++ db/migrate/20240109215029_add_search_extensions.rb
@@ -0,0 +1,13 @@
+class AddSearchExtensions < ActiveRecord::Migration[7.0]
+  def up
+    safety_assured do
+      execute "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
+    end
+  end
+
+  def down
+    safety_assured do
+      execute "DROP EXTENSION IF EXISTS pg_trgm;"
+    end
+  end
+end
