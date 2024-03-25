71337edc13dba4da41442938e68815c6ecaf14e5
Mike Young <mike.young@tidelift.com>
2024-03-25 11:22:08 -0400

    add spec for github api call

---
+++ spec/models/repository_host/github_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe RepositoryHost::Github do
+  let(:repository) { build(:repository, host_type: "GitLab", full_name: "vuejs/vue") }
+  let(:repository_host) { described_class.new(repository) }
+  let(:api_token) { "TEST_TOKEN" }
+
+  it "can fetch repository data" do
+    VCR.insert_cassette("github/vue") do
+      repository_data = described_class.fetch_repo(repository.id_or_name, api_token)
+      expect(repository_data).not_to be_nil
+    end
+  end
+end
