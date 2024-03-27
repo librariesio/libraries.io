5cf9e95d2537e9bd8c40d9dbef85363f66bbed18
Mike Young <mike.young@tidelift.com>
2024-03-27 15:01:34 -0400

    add specs to check mapping of api values

--- spec/models/repository_host/github_spec.rb
+++ spec/models/repository_host/github_spec.rb
@@ -13,4 +13,30 @@ describe RepositoryHost::Github do
       expect(repository_data).not_to be_nil
     end
   end
+
+  it "maps data" do
+    VCR.use_cassette("github/vue") do
+      repository_data = repository_host.class.fetch_repo(repository.id_or_name, api_token)
+
+      expect(repository_data.archived).to be false
+      expect(repository_data.default_branch).to eql("main")
+      expect(repository_data.description).to eql("This is the repo for Vue 2. For Vue 3, go to https://github.com/vuejs/core")
+      expect(repository_data.fork).to be false
+      expect(repository_data.full_name).to eql("vuejs/vue")
+      expect(repository_data.has_issues).to be true
+      expect(repository_data.has_wiki).to be true
+      expect(repository_data.homepage).to eql("http://v2.vuejs.org")
+      expect(repository_data.host_type).to eql("GitHub")
+      expect(repository_data.keywords).to match_array(%w[framework frontend javascript vue])
+      expect(repository_data.language).to eql("TypeScript")
+      expect(repository_data.license).to eql("mit")
+      expect(repository_data.name).to eql("vue")
+      expect(repository_data.owner).not_to be_nil
+      expect(repository_data.parent).to be_nil
+      expect(repository_data.is_private).to be false
+      expect(repository_data.repository_uuid).to eql("11730342")
+      expect(repository_data.scm).to eql("git")
+      expect(repository_data.repository_size).to eql(32405)
+    end
+  end
 end
--- spec/models/repository_host/gitlab_spec.rb
+++ spec/models/repository_host/gitlab_spec.rb
@@ -13,4 +13,30 @@ describe RepositoryHost::Gitlab do
       expect(repository_data).not_to be_nil
     end
   end
+
+  it "maps data" do
+    VCR.use_cassette("gitlab/ase") do
+      repository_data = repository_host.class.fetch_repo(repository.id_or_name, api_token)
+
+      expect(repository_data.archived).to be false
+      expect(repository_data.default_branch).to eql("master")
+      expect(repository_data.description).to eql("[Atomic Simulation Environment](https://wiki.fysik.dtu.dk/ase/): A Python library for working with atoms")
+      expect(repository_data.fork).to be false
+      expect(repository_data.full_name).to eql("ase/ase")
+      expect(repository_data.has_issues).to be true
+      expect(repository_data.has_wiki).to be false
+      expect(repository_data.homepage).to eql("https://gitlab.com/ase/ase")
+      expect(repository_data.host_type).to eql("GitLab")
+      expect(repository_data.keywords).to match_array(["Atomistic simulations", "chemistry", "materials", "physics"])
+      expect(repository_data.language).to be_nil # not supported by our API calls at the moment
+      expect(repository_data.license).to eql("lgpl-2.1")
+      expect(repository_data.name).to eql("ase")
+      expect(repository_data.owner).not_to be_nil
+      expect(repository_data.parent).to match(hash_including(:full_name))
+      expect(repository_data.is_private).to be false
+      expect(repository_data.repository_uuid).to eql("470007")
+      expect(repository_data.scm).to eql("git")
+      expect(repository_data.repository_size).to eql(0) # size isn't availabe in the API
+    end
+  end
 end
