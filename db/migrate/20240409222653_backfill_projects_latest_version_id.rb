c16c1e54cfb7ebb2559cc83ff57cc8500a5b7f01
Tieg Zaharia <tieg@tidelift.com>
2024-04-10 11:23:06 -0700

    Migration: backfill Projects#latest_version_id column

---
+++ db/migrate/20240409222653_backfill_projects_latest_version_id.rb
@@ -0,0 +1,36 @@
+class BackfillProjectsLatestVersionId < ActiveRecord::Migration[7.0]
+  def up
+    # This migration has already been run manually in production.
+    return if Rails.env.production?
+
+    Project
+      .in_batches(of: 10_000) do |projects|
+        sql = <<~SQL
+          WITH rows_to_update AS (
+            SELECT p.id as project_id, latest_version.id as latest_version_id
+            FROM projects p
+            LEFT JOIN LATERAL (
+              SELECT versions.*
+              FROM versions 
+              WHERE versions.project_id = p.id 
+              ORDER BY published_at DESC NULLS LAST, versions.created_at DESC 
+              LIMIT 1
+            ) as latest_version ON true
+            WHERE p.id IN (#{projects.map(&:id).join(",")})
+          )
+          UPDATE projects
+          SET latest_version_id = rows_to_update.latest_version_id
+          FROM rows_to_update
+          WHERE projects.id = rows_to_update.project_id
+        SQL
+      
+        ActiveRecord::Base.connection.execute(sql)
+        print "."
+      end
+    end
+  end
+
+  def down
+    # No-op
+  end
+end
