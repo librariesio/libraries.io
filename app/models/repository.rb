dcc64ef9ec2597145f0c32508d737ced341a8dad
Tieg Zaharia <tieg@tidelift.com>
2024-04-17 14:41:48 -0700

    Don't need to download metadata when the async job already does it

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -356,7 +356,6 @@ class Repository < ApplicationRecord
     repository = Repository.where(host_type: "GitHub").find_by_uuid(uuid)
     user = Identity.where("provider ILIKE ?", "github%").where(uid: sender_id).first.try(:user)
     if user.present? && repository.present?
-      repository.download_metadata(user.token)
       repository.update_all_info_async(user.token)
     end
   end
