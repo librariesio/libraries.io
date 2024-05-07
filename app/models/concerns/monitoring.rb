91a914c917bf4ba2a35db4c2450c0a593f6de9f7
Tieg Zaharia <tieg@tidelift.com>
2024-05-07 08:22:20 -0700

    Fix an ambiguous column ref after the repo deps refactor. (#3384)

--- app/models/concerns/monitoring.rb
+++ app/models/concerns/monitoring.rb
@@ -40,7 +40,7 @@ module Monitoring
   end
 
   def your_dependent_repos(project)
-    ids = really_all_dependencies.where(project_id: project.id).pluck(:repository_id).uniq
+    ids = really_all_dependencies.where(project_id: project.id).pluck("projects.repository_id").uniq
     all_repositories.where(id: ids).order("fork ASC, pushed_at DESC, rank DESC NULLS LAST")
   end
 end
