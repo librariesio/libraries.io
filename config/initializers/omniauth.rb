c206205adc29db7475ea35eec8230e92233b9538
Mike Young <mike.young@tidelift.com>
2024-07-19 13:55:43 -0400

    add public_repo to the scopes for our github public access tier

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -16,7 +16,7 @@ end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Rails.configuration.github_key, Rails.configuration.github_secret, scope: "user:email"
-  provider :github_public, Rails.configuration.github_public_key, Rails.configuration.github_public_secret, scope: "user:email,write:repo_hook,read:org", request_path: "/auth/github_public", callback_path: "/auth/github_public/callback"
+  provider :github_public, Rails.configuration.github_public_key, Rails.configuration.github_public_secret, scope: "user:email,write:repo_hook,read:org,public_repo", request_path: "/auth/github_public", callback_path: "/auth/github_public/callback"
   provider :github_private, Rails.configuration.github_private_key, Rails.configuration.github_private_secret, scope: "user:email,repo,read:org", request_path: "/auth/github_private", callback_path: "/auth/github_private/callback"
   provider :gitlab, Rails.configuration.gitlab_application_id, Rails.configuration.gitlab_secret, scope: "read_user"
   provider :bitbucket, Rails.configuration.bitbucket_application_id, Rails.configuration.bitbucket_secret
