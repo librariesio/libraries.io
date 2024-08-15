ad316ef63c853a2def7240a64c9e9c889b8e8c66
Tieg Zaharia <tieg@tidelift.com>
2024-08-15 11:23:54 -0700

    Migration: add audits table, from audited gem.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_22_132336) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_15_181932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
@@ -26,6 +26,28 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_22_132336) do
     t.boolean "is_internal", default: false, null: false
     t.index ["access_token"], name: "index_api_keys_on_access_token"
     t.index ["user_id"], name: "index_api_keys_on_user_id"
+  end
+
+  create_table "audits", force: :cascade do |t|
+    t.integer "auditable_id"
+    t.string "auditable_type"
+    t.integer "associated_id"
+    t.string "associated_type"
+    t.integer "user_id"
+    t.string "user_type"
+    t.string "username"
+    t.string "action"
+    t.text "audited_changes"
+    t.integer "version", default: 0
+    t.string "comment"
+    t.string "remote_address"
+    t.string "request_uuid"
+    t.datetime "created_at"
+    t.index ["associated_type", "associated_id"], name: "associated_index"
+    t.index ["auditable_type", "auditable_id", "version"], name: "auditable_index"
+    t.index ["created_at"], name: "index_audits_on_created_at"
+    t.index ["request_uuid"], name: "index_audits_on_request_uuid"
+    t.index ["user_id", "user_type"], name: "user_index"
   end
 
   create_table "auth_tokens", id: :serial, force: :cascade do |t|
