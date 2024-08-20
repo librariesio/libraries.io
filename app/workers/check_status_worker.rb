8734d322b0640a552d18ece3203fb9ab4a464b70
Tieg Zaharia <tieg@tidelift.com>
2024-08-20 09:01:52 -0700

    CheckStatusWorker: better lock, and retry 429s. (#3447)
    * CheckStatusWorker: better lock, and retry 429s.

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -2,9 +2,13 @@
 
 class CheckStatusWorker
   include Sidekiq::Worker
-  sidekiq_options queue: :status, lock: :until_executed
+  sidekiq_options queue: :status, lock: :until_and_while_executing, lock_ttl: 10.minutes.to_i
 
   def perform(project_id, _ignored = nil)
     Project.find_by_id(project_id).try(:check_status)
+  rescue Project::CheckStatusRateLimited
+    # Don't give up when we are rate-limited: we would get 429'ed when we are checking many statuses at once,
+    # so detect these and retry them within the next 10-60 minutes.
+    CheckStatusWorker.perform_in(rand(10..59))
   end
 end
