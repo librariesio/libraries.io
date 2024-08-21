98bea44e0d6e0a4025626f3cb08d312e6bed873a
Tieg Zaharia <tieg@tidelift.com>
2024-08-20 21:31:34 -0700

    Remove _ignored param now

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -4,11 +4,11 @@ class CheckStatusWorker
   include Sidekiq::Worker
   sidekiq_options queue: :status, lock: :until_and_while_executing, lock_ttl: 10.minutes.to_i
 
-  def perform(project_id, _ignored = nil)
+  def perform(project_id)
     Project.find_by_id(project_id).try(:check_status)
   rescue Project::CheckStatusRateLimited
     # Don't give up when we are rate-limited: we would get 429'ed when we are checking many statuses at once,
     # so detect these and retry them within the next 10-60 minutes.
-    CheckStatusWorker.perform_in(rand(10..59), project_id, _ignored)
+    CheckStatusWorker.perform_in(rand(10..59), project_id)
   end
 end
