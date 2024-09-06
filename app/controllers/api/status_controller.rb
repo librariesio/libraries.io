b51f69594e608770b2ecb17764e19c32bc993f4b
Havoc Pennington <hp@pobox.com>
2024-09-06 12:36:46 -0400

    StatusController: add a Oj.dump and render traces
    This should better label the trace in datadog
    
    Also remove "use_as_json" because the Oj docs says
    that's ignored with `mode: :rails`

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -5,6 +5,12 @@ class Api::StatusController < Api::BulkProjectController
 
   def check
     serializer = OptimizedProjectSerializer.new(projects, project_names, internal_key: internal_api_key?)
-    render json: Oj.dump(serializer.serialize, use_as_json: true, mode: :rails)
+    serialized = serializer.serialize
+    dumped = Datadog::Tracing.trace("status_check_oj_dump") do |_span, _trace|
+      Oj.dump(serialized, mode: :rails)
+    end
+    Datadog::Tracing.trace("status_check_render_dumped") do |_span, _trace|
+      render json: dumped
+    end
   end
 end
