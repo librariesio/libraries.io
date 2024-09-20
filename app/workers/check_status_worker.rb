d81bdee1de1913f12546410a2d4ce5b9bbb63a61
Tieg Zaharia <tieg@tidelift.com>
2024-09-20 15:32:20 -0700

    Fix missing arg in CheckStatusWorker (#3478)

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -13,6 +13,6 @@ class CheckStatusWorker
     project.update_column(:status_checked_at, nil)
     # By retrying in 5 minutes, when we have a big queue of CheckStatusWorker, we'll
     # work it down gradually by retrying things 5 minutes at a time.
-    CheckStatusWorker.perform_in(5.minutes)
+    CheckStatusWorker.perform_in(5.minutes, project.id)
   end
 end
