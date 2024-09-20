6440b368afb54191a7b92554fa0286317b883208
Tieg Zaharia <tieg@tidelift.com>
2024-09-20 16:09:52 -0700

    PackageManager::Conda: treat "" requirement as "*" when fetching dependencies  (#3479)
    * Conda: treat nil requirements as "*" wildcard
    
    * Add fixture

--- spec/models/package_manager/conda_spec.rb
+++ spec/models/package_manager/conda_spec.rb
@@ -24,4 +24,29 @@ describe PackageManager::Conda do
       expect(described_class.install_instructions(project)).to eq("conda install -c anaconda foo")
     end
   end
+
+  describe ".dependencies" do
+    context "when there are blank dependencies" do
+      it "replaces blanks with '*' wildcards" do
+        dependencies = VCR.use_cassette("conda/shap") do
+          described_class.dependencies("shap", "0.46.0", nil)
+        end
+
+        expect(dependencies.pluck(:project_name, :requirements)).to match_array([
+          ["cloudpickle", "*"],
+          ["libgcc-ng", ">=11.2.0"],
+          ["libstdcxx-ng", ">=11.2.0"],
+          ["numba", "*"],
+          ["numpy", ">=1.21.6,<2.0a0"],
+          ["packaging", ">20.9"],
+          ["pandas", "*"],
+          ["python", ">=3.10,<3.11.0a0"],
+          ["scikit-learn", "*"],
+          ["scipy", "*"],
+          ["slicer", "0.0.8"],
+          ["tqdm", ">=4.27.0"],
+        ])
+      end
+    end
+  end
 end
