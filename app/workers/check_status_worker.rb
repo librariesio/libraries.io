b726b52655d5260dfdaa823cbf88ef921be533ae
Tieg Zaharia <tieg@tidelift.com>
2024-09-27 15:31:59 -0700

    Project#check_status: change the strategy of rate-limiting ourselves (#3482)
    * Specs, tweaks, etc.
    
    * Some fixes
    
    * Add a spec for blank retry-after

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -7,12 +7,12 @@ class CheckStatusWorker
   def perform(project_id)
     project = Project.find_by_id(project_id)
     project.try(:check_status)
-  rescue Project::CheckStatusInternallyRateLimited, Project::CheckStatusExternallyRateLimited
+  rescue Project::CheckStatusInternallyRateLimited, Project::CheckStatusExternallyRateLimited => e
     # status_check_at is eagerly updated in Project#check_status, so reset it to nil
     # so we can tell if it was throttled.
     project.update_column(:status_checked_at, nil)
-    # By retrying in 5 minutes, when we have a big queue of CheckStatusWorker, we'll
-    # work it down gradually by retrying things 5 minutes at a time.
-    CheckStatusWorker.perform_in(5.minutes, project.id)
+    if e.retry_after_seconds
+      CheckStatusWorker.perform_in(e.retry_after_seconds.seconds.from_now, project.id)
+    end
   end
 end
