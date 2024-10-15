2b65157724990705bcc79b0df0a7a907cd72910f
Tieg Zaharia <tieg@tidelift.com>
2024-10-15 08:33:09 -0700

    Handle ActiveRecord::RecordNotUnique when creating projects too

--- app/models/package_manager/base.rb
+++ app/models/package_manager/base.rb
@@ -100,8 +100,8 @@ module PackageManager
 
       begin
         db_project.save!
-      rescue ActiveRecord::RecordInvalid => e
-        raise e unless e.message =~ /Name has already been taken/
+      rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid => e
+        raise e if e == ActiveRecord::RecordInvalid && e.message !~ /Name has already been taken/
 
         # Probably a race condition with multiple versions of a new project being updated.
         db_project = Project.find_by(platform: db_platform, name: mapped_project[:name])
--- spec/models/package_manager/pypi_spec.rb
+++ spec/models/package_manager/pypi_spec.rb
@@ -463,6 +463,16 @@ describe PackageManager::Pypi do
 
         expect(db_project.reload.homepage).to eq(nil)
       end
+
+      context "that was created by another process in between lookup and save (a race condition)" do
+        it "overwrites the homepage" do
+          allow(Project).to receive(:find_or_initialize_by).and_return(Project.new(name: project_name, platform: "Pypi"))
+
+          described_class.update(project_name)
+
+          expect(db_project.reload.homepage).to eq("https://www.libraries.io/package_name/home")
+        end
+      end
     end
 
     it "adds the versions with correct statuses" do
