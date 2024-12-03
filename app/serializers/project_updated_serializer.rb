1f4a8b835abded834f70845291c1564358f4c9fe
Havoc Pennington <hp@pobox.com>
2024-12-03 11:52:06 -0500

    ProjectUpdatedSerializer: send fields that are needed to search for the Project
    We want to send only "cheap" fields (in particular: avoid joining to Repository
    or Version), but include fields that are useful to search against in order to
    discover relevant projects.
    
    We throw in the release numbers just so we have a sample release of the
    project if we need it.
    
    Some of these are in here in case they are useful to sort search results.

--- app/serializers/project_updated_serializer.rb
+++ app/serializers/project_updated_serializer.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 class ProjectUpdatedSerializer < ActiveModel::Serializer
+  # Don't add anything here that requires a join.
+  #  - join to Version will be too slow for high-release-count projects
+  #  - join to Repository assumes Project.repository_id is correct
   attributes %i[
+    created_at
+    dependents_count
+    homepage
+    keywords_array
+    latest_release_number
+    latest_release_published_at
+    latest_stable_release_number
     name
     platform
+    repository_url
     updated_at
+    versions_count
   ]
 end
