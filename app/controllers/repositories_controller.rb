24b28be3e5a97377730d6d1261800677b9043f6b
Jeremy Katz <jeremy@tidelift.com>
2024-12-05 20:01:46 +0000

    Move repository pages to just redirect to the repo host
    These aren't linked anywhere anymore so let's just redirect
    to the repo host if someone somehow gets to one

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,60 +1,43 @@
 # frozen_string_literal: true
 
 class RepositoriesController < ApplicationController
-  before_action :ensure_logged_in, only: [:sync]
-
   def show
-    load_repo
-    @contributors = @repository.contributors.order("count DESC").visible.limit(20).select(:host_type, :name, :login, :uuid)
-    @projects = @repository.projects.visible.limit(20).includes(:versions)
-    @color = @repository.color
-    @forks = @repository.forked_repositories.host(@repository.host_type).interesting.limit(5)
+    redirect_to redirect_url, status: :moved_permanently
   end
 
   def sourcerank
-    load_repo
+    redirect_to redirect_url, status: :moved_permanently
   end
 
   def tags
-    load_repo
-    @tags = @repository.tags.published.order("published_at DESC, name DESC").paginate(page: page_number)
+    redirect_to redirect_url, status: :moved_permanently
   end
 
   def contributors
-    load_repo
-    scope = @repository.contributions.where("count > 0").joins(:repository_user)
-    visible_scope = scope.where("repository_users.hidden = ?", false).order("count DESC")
-    @total = scope.sum(:count)
-    @top_count = visible_scope.first.try(:count)
-    @contributions = visible_scope.paginate(page: page_number)
-    @any_hidden = scope.count > @contributions.total_entries
+    redirect_to redirect_url, status: :moved_permanently
   end
 
   def forks
-    load_repo
-    @forks = @repository.forked_repositories.host(@repository.host_type).maintained.order("stargazers_count DESC, rank DESC NULLS LAST").paginate(page: page_number)
+    redirect_to redirect_url, status: :moved_permanently
   end
 
   def dependencies
-    load_repo
-    @dependencies = @repository.projects_dependencies
-    render layout: false
-  end
-
-  def sync
-    load_repo
-    if @repository.recently_synced?
-      flash[:error] = "Repository has already been synced recently"
-    else
-      @repository.manual_sync(current_user.token)
-      flash[:notice] = "Repository has been queued to be resynced"
-    end
-    redirect_back fallback_location: repository_path(@repository.to_param)
+    redirect_to redirect_url, status: :moved_permanently
   end
 
   private
 
-  def allowed_sorts
-    %w[rank stargazers_count contributions_count created_at pushed_at subscribers_count open_issues_count forks_count size]
+  def redirect_url
+    full_name = [params[:owner], params[:name]].join("/")
+    case params[:host_type].try(:downcase)
+    when "github"
+      "https://github.com/#{full_name.downcase}"
+    when "gitlab"
+      "https://gitlab.com/#{full_name.downcase}"
+    when "bitbucket"
+      "https://bitbucket.com/#{full_name.downcase}"
+    else
+      raise ActiveRecord::RecordNotFound
+    end
   end
 end
