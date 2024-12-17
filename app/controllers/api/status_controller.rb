0ea353b9b938744d0a106069d54f21a4c1e9af81
Tieg Zaharia <tieg@tidelift.com>
2024-12-16 18:58:52 -0700

    Include list of project ids when tracing Api::Status#check rendering (#3531)

--- app/controllers/api/status_controller.rb
+++ app/controllers/api/status_controller.rb
@@ -6,10 +6,12 @@ class Api::StatusController < Api::BulkProjectController
   def check
     serializer = OptimizedProjectSerializer.new(projects, project_names, internal_key: internal_api_key?)
     serialized = serializer.serialize
-    dumped = Datadog::Tracing.trace("status_check_oj_dump") do |_span, _trace|
+    dumped = Datadog::Tracing.trace("status_check_oj_dump") do |_span, trace|
+      trace&.set_tag("project_ids", projects.map(&:id))
       Oj.dump(serialized, mode: :rails)
     end
-    Datadog::Tracing.trace("status_check_render_dumped") do |_span, _trace|
+    Datadog::Tracing.trace("status_check_render_dumped") do |_span, trace|
+      trace&.set_tag("project_ids", projects.map(&:id))
       render json: dumped
     end
   end
