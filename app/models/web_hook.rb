dc27d4dc511a01e77b76a1a31f5a81447769c43b
Havoc Pennington <hp@pobox.com>
2025-01-04 11:59:50 -0500

    WebHook: timeout from 5s to 10s

--- app/models/web_hook.rb
+++ app/models/web_hook.rb
@@ -97,7 +97,7 @@ class WebHook < ApplicationRecord
 
     Typhoeus::Request.new(url,
                           method: :post,
-                          timeout_ms: 5000,
+                          timeout_ms: 10000,
                           body: body,
                           headers: {
                             "Content-Type" => "application/json",
