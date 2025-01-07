fdef392eedab7617202b4b3358bbab5fd21d9750
Tieg Zaharia <tieg@tidelift.com>
2025-01-07 10:21:11 -0700

    Hackage: use "Category" words from HTML instead of ToC content for keywords (#3547)
    * Hackage: use "Category" words instead of ToC content for keywords
    
    * Lint

--- app/models/package_manager/hackage.rb
+++ app/models/package_manager/hackage.rb
@@ -39,7 +39,11 @@ module PackageManager
     def self.mapping(raw_project)
       MappingBuilder.build_hash(
         name: raw_project[:name],
-        keywords_array: Array(raw_project[:page].css("#content div:first a")[1..].map(&:text)),
+        keywords_array: Array(
+          raw_project[:page].css("#content table.properties tr")
+            .find { |tr| tr.css("th")&.text&.strip == "Category" }
+            &.css("a")&.map(&:text)&.reject(&:blank?)&.map(&:strip)
+        ),
         description: description(raw_project[:page]),
         licenses: find_attribute(raw_project[:page], "License"),
         homepage: find_attribute(raw_project[:page], "Home page"),
