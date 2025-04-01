29069f95900844a44638da502d4739488f25d338
Mike Young <mike.young@tidelift.com>
2025-04-01 10:04:37 -0400

    cache languages and platforms for explore page (#3557)

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -2,11 +2,14 @@
 
 class ExploreController < ApplicationController
   def index
-    @platforms = Project.maintained.group(:platform).order("count_id DESC").limit(28).count("id").keys
-    @languages = Project.maintained.where.not(language: nil).group(:language).order("count_id DESC").limit(21).count("id").keys
+    @platforms = Rails.cache.fetch("explore:platforms", expires_in: 1.day, race_condition_ttl: 2.minutes) do
+      Project.maintained.group(:platform).order("count_id DESC").limit(28).count("id").keys
+    end
+    @languages = Rails.cache.fetch("explore:languages", expires_in: 1.day, race_condition_ttl: 2.minutes) do
+      Project.maintained.where.not(language: nil).group(:language).order("count_id DESC").limit(21).count("id").keys
+    end
 
-    project_scope = Project.includes(:repository).maintained.with_description
-
+    project_scope = Project.maintained.with_description
     @new_projects = project_scope.order("projects.created_at desc").limit(10)
   end
 end
