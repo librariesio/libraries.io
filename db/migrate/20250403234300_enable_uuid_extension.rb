edb01d56db15c7cea761f133a6144dec8099b496
Havoc Pennington <havoc.pennington@sonarsource.com>
2025-04-07 13:41:41 -0400

    Linter fixes

--- db/migrate/20250403234300_enable_uuid_extension.rb
+++ db/migrate/20250403234300_enable_uuid_extension.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class EnableUuidExtension < ActiveRecord::Migration[7.1]
   def change
-    enable_extension 'uuid-ossp'
-    enable_extension 'pgcrypto'
+    enable_extension "uuid-ossp"
+    enable_extension "pgcrypto"
   end
 end
