6d741e751ca5f214af8fc55a0a60f5e3aac81b9e
Jeremy Katz <jeremy.katz@sonarsource.com>
2026-04-02 12:38:53 -0400

    PackageManagerDownloadWorker: call GC.compact after each job
    Large package syncs allocate many AR objects (Version, Dependency) that are
    processed in batches and freed, but Ruby does not eagerly return heap pages to
    the OS after GC. Calling GC.compact after update() defragments the heap and
    allows more pages to be returned, reducing the RSS ratchet effect observed when
    many large packages are processed sequentially.

--- app/workers/package_manager_download_worker.rb
+++ app/workers/package_manager_download_worker.rb
@@ -77,6 +77,7 @@ class PackageManagerDownloadWorker
 
     Rails.logger.info("Package update for platform=#{key} name=#{name} version=#{version} source=#{source}")
     project = package_manager.update(name, sync_version: sync_version, force_sync_dependencies: force_sync_dependencies, source: source)
+    GC.compact # defragment heap after potentially large AR object graph; helps OS reclaim pages
 
     # Raise/log if version was requested but not found
     if version.present? && project && !project&.versions&.exists?(number: version)
